38ed980bacdb376d67bcb946aba0a328aee725f2
Seah Chin Ying <scy.6char@gmail.com>
2020-03-24 04:39:26 +0800

    feat: Check for required env vars

--- backend/src/core/index.ts
+++ backend/src/core/index.ts
@@ -1,8 +1,18 @@
 import { sequelizeLoader } from './sequelize'
 
+const checkRequiredEnvVars = (vars: Array<string>): boolean => {
+  vars.forEach(v => {
+    if (!process.env[v]) {
+      console.log(`${v} environment variable is not set!`)
+      throw new Error(`${v} environment variable is not set!`)
+    }
+  })
+  return true
+}
+
 const loaders = async (): Promise<void> => {
   await sequelizeLoader()
 }
 
-export { loaders }
+export { checkRequiredEnvVars, loaders }
 export * from './config'
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,14 @@
 import express from 'express'
 require('dotenv').config()
 
-import { loaders } from './core'
+import { checkRequiredEnvVars, loaders } from './core'
 
+const requiredEnvVars = ['DB_URI']
 const port = Number(process.env.PORT) || 4000
 const app: express.Application = express()
+
 const start = async (): Promise<void> => {
+  checkRequiredEnvVars(requiredEnvVars)
   await loaders()
   app.listen(port, () => console.log(`Listening on port ${port}!`))
 }
