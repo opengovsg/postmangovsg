7b2e6fe0c10f10224a349e7075820a0f58ad7304
Ian Chen <ian-@gmx.com>
2020-03-25 23:29:47 +0800

    chore: tsconfig root dir change, copy assets, rimraf build

--- backend/src/core/config.ts
+++ backend/src/core/config.ts
@@ -3,11 +3,11 @@ import path from 'path'
 
 const databaseUri: string = process.env.DB_URI as string
 
-const IS_PROD: boolean = process.env.NODE_ENV !== 'development'
+const IS_PROD: boolean = process.env.NODE_ENV === 'production'
 const SEQUELIZE_POOL_MAX_CONNECTIONS = 150
 const SEQUELIZE_POOL_ACQUIRE_IN_MILLISECONDS = 600000
 
-const rdsCa = fs.readFileSync(path.join(__dirname, '../db-ca.pem'))
+const rdsCa = IS_PROD && fs.readFileSync(path.join(__dirname, '../db-ca.pem'))
 
 export default {
   IS_PROD,
