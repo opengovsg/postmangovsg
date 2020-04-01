bf187c958e3ca7cfe83d93c95dd591efce7b84da
Ian Chen <ian-@gmx.com>
2020-04-02 01:39:13 +0800

    chore: Remove material ui, use scss

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,13 @@
 import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-import { ThemeProvider } from '@material-ui/core/styles'
-
-// Material UI custom theme
-import theme from './App.style'
 
 import Landing from './components/landing'
 
 const App = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <Router>
-        <Route exact path="/" component={Landing}></Route>
-      </Router>
-    </ThemeProvider>
+    <Router>
+      <Route exact path="/" component={Landing}></Route>
+    </Router>
   )
 }
 
