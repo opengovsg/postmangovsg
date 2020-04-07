0108ecd81edba4b6d413d191a0c8a05811671448
Shao Wei <lshaowei18@gmail.com>
2020-04-07 15:53:47 +0800

    Add module name mappers

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -2,6 +2,11 @@ module.exports = {
   "roots": [
     "<rootDir>/src"
   ],
+  'moduleNameMapper': {
+    '@core/(.*)': '<rootDir>/src/core/$1',
+    '@sms/(.*)': '<rootDir>/src/sms/$1',
+    '@email/(.*)': '<rootDir>/src/email/$1'
+  },
   "testMatch": [
     "**/__tests__/**/*.+(ts|tsx|js)",
     "**/?(*.)+(spec|test).+(ts|tsx|js)"
