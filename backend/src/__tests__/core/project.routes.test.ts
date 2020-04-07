8987aee9d866309358ffd9cdb1da54a9615b8860
Shao Wei <lshaowei18@gmail.com>
2020-04-07 11:17:19 +0800

    Remove done from test

--- backend/src/__tests__/core/project.routes.test.ts
+++ backend/src/__tests__/core/project.routes.test.ts
@@ -3,9 +3,8 @@ import request from 'supertest'
 
 
 describe('GET /projects', () => {
-  test('Should respond with a 200', async (done) => {
+  test('Should respond with a 200', async () => {
     const response = await(request(app).get('/v1/projects'))
     expect(response.status).toBe(200)
-    done()
   })
 })
