cfdc90401166d7026870f0d43a2bf847d1823f4b
Shao Wei <lshaowei18@gmail.com>
2020-04-08 16:49:09 +0800

    Add middleware to authenticate cookie

---
+++ backend/src/core/middlewares/is-authenticated.middleware.ts
@@ -0,0 +1,9 @@
+import { Request, Response, NextFunction } from 'express'
+
+
+export const isCookieAuthenticated = async (req: Request, res: Response, next: NextFunction): Promise<Response | void> => {
+  if (!!req.session?.user?.id) {
+    return next()
+  }
+  return res.sendStatus(401)
+}
