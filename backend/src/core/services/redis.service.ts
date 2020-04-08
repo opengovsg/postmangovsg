c7b23efba47219aa9840f35a011742b16ed64472
Shao Wei <lshaowei18@gmail.com>
2020-04-08 14:36:18 +0800

    Clean up plus check for otpClient

--- backend/src/core/services/redis.service.ts
+++ backend/src/core/services/redis.service.ts
@@ -2,17 +2,15 @@ import redis from 'redis'
 import config from '@core/config'
 import logger from '@core/logger'
 
+if (!config.redisOtpUri) {
+  throw new Error('otpClient: redisOtpUri not found')
+}
 
-const createOtpClient = () : redis.RedisClient => {
-  return redis.createClient({ url: config.redisOtpUri })
+export const otpClient = 
+  redis.createClient({ url: config.redisOtpUri })
     .on('connect', () => {
       logger.info('otpClient: Connected')
     })
     .on('error', (err: Error) => {
       logger.error(String(err))
     })
-}
-
-const otpClient : redis.RedisClient = createOtpClient()
-
-export { otpClient }
