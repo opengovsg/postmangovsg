aa6feb9c5a819e36e5982a49b5d95e2fc41a2df4
Seah Chin Ying <scy.6char@gmail.com>
2020-04-12 22:38:22 +0800

    chore: stub sequelize hook for template params

--- backend/src/sms/models/sms-template.ts
+++ backend/src/sms/models/sms-template.ts
@@ -1,4 +1,4 @@
-import { Column, DataType, ForeignKey, Model, Table, BelongsTo } from 'sequelize-typescript'
+import { BeforeCreate, BeforeUpdate, BeforeUpsert, BelongsTo, Column, DataType, ForeignKey, Model, Table } from 'sequelize-typescript'
 import { Campaign } from '@core/models/campaign'
 
 @Table({ tableName: 'sms_templates' , underscored: true, timestamps: true })
@@ -17,4 +17,19 @@ export class SmsTemplate extends Model<SmsTemplate> {
     type: DataType.TEXT,
   })
   body?: string
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: true
+  })
+  params?: Array<String>
+
+  @BeforeUpdate
+  @BeforeCreate
+  @BeforeUpsert
+  static generateParams(instance: SmsTemplate) {
+    // stubbed
+    instance.params = ['name', 'event']
+  }
+
 }
