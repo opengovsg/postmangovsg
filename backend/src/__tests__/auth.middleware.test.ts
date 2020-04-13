71357dcc015aa5cbce6db78445907a7846676118
Shao Wei <lshaowei18@gmail.com>
2020-04-13 09:09:03 +0800

    Rewrite test to use Jest mocks

--- backend/src/__tests__/auth.middleware.test.ts
+++ backend/src/__tests__/auth.middleware.test.ts
@@ -1,14 +1,22 @@
-const httpMocks = require('node-mocks-http')
-
 import { getOtp } from '@core/middlewares'
 
+const mockRequest = () => {
+  let req : any
+  return req
+}
+
+const mockResponse = () => {
+  let res : any = {}
+  res.status = jest.fn().mockReturnValue(res)
+  res.json = jest.fn().mockReturnValue(res)
+  res.sendStatus = jest.fn().mockReturnValue(res)
+  return res
+}
 describe ('getOtp', () => {
-  test("getOtp should return 200", () => {
-    const request = httpMocks.createRequest({
-      method: "POST",
-    })
-    const response = httpMocks.createResponse()
-    getOtp(request, response)
-    expect(response.statusCode).toBe(200)
+  test("getOtp should return 200", async () => {
+    const req = mockRequest()
+    const res = mockResponse()
+    await getOtp(req, res)
+    expect(res.sendStatus).toBeCalledWith(200)
   })
 })
