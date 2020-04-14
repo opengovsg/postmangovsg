021b68c02f0f72f837b9b8c6947db948eb71c199
Seah Chin Ying <scy.6char@gmail.com>
2020-04-15 00:31:39 +0800

    chore: Add interface for s3Object

--- backend/src/core/models/campaign.ts
+++ backend/src/core/models/campaign.ts
@@ -1,5 +1,6 @@
 import { BelongsTo, Column, DataType, ForeignKey, Model, Table } from 'sequelize-typescript'
 import { ChannelType } from '@core/constants'
+import { CampaignS3ObjectInterface } from '@core/interfaces'
 import { Credential } from './credential'
 import { User } from './user'
 
@@ -32,7 +33,7 @@ export class Campaign extends Model<Campaign> {
   credential?: Credential
 
   @Column(DataType.JSON)
-  s3Object?: object
+  s3Object?: CampaignS3ObjectInterface
 
   @Column({
     type: DataType.BOOLEAN,
