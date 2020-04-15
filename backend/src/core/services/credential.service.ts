9d655454a2556b41429c0bd790b70204fc931433
Shao Wei <lshaowei18@gmail.com>
2020-04-16 04:38:42 +0800

    feature: retrieve twilio credentials from secrets manager

--- backend/src/core/services/credential.service.ts
+++ backend/src/core/services/credential.service.ts
@@ -2,6 +2,8 @@ import AWS from 'aws-sdk'
 import { Credential, Campaign } from '@core/models'
 import logger from '@core/logger'
 import config from '@core/config'
+import { TwilioCredentials } from '@sms/interfaces'
+import { get } from 'lodash'
 
 const secretsManager = new AWS.SecretsManager({ region: config.aws.awsRegion })
 
@@ -40,10 +42,19 @@ const storeSecret = async (name: string, secret: string) => {
   logger.info('Successfully stored secret in AWS secrets manager.')
 }
 
+const getTwilioCredentials = async (name: string): Promise<TwilioCredentials> => {
+  logger.info('Getting secret from AWS secrets manager.')
+  const data = await secretsManager.getSecretValue({SecretId: name}).promise()
+  logger.info('Gotten secret from AWS secrets manager.')
+  const secretString = get(data, 'SecretString', '')
+  if (!secretString) throw new Error('Missing secret string from AWS secrets manager.')
+  return JSON.parse(secretString)
+}
 
 export const credentialService = {
   insertCredential,
   updateCampaignWithCredential,
   isExistingCredential,
-  storeSecret
+  storeSecret,
+  getTwilioCredentials,
 }
