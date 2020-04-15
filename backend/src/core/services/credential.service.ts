9f75f3578e8af8ea9a95955b732df1b06417ddcb
Shao Wei <lshaowei18@gmail.com>
2020-04-15 11:03:34 +0800

    chore: merge secrets and db service into credential

---
+++ backend/src/core/services/credential.service.ts
@@ -0,0 +1,45 @@
+import AWS from 'aws-sdk'
+import { Credential, Campaign } from '@core/models'
+import logger from '@core/logger'
+import config from '@core/config'
+
+const secretsManager = new AWS.SecretsManager({ region: config.aws.awsRegion })
+
+const insertCredential = async (hash: string) => {
+  await Credential.create({
+    name: hash,
+  })
+}
+
+const updateCampaignWithCredential = async (campaignId: string, credentialName: string) => {
+  return Campaign.update({
+    credName: credentialName,
+  },{
+    where: {id: campaignId},
+    returning: false
+  })
+}
+
+const isExistingCredential = async (name: string): Promise<Boolean> => {
+  const result = await Credential.findOne({
+    where: {
+      name: name
+    },
+  })
+  if (result) return true
+  return false
+}
+
+const storeSecret = async (name: string, secret: string) => {
+  const params = {
+    Name: name,
+    SecretString: secret
+  }
+  logger.info('Storing secret in AWS secrets manager.')
+  await secretsManager.createSecret(params).promise()
+  logger.info('Successfully stored secret in AWS secrets manager.')
+}
+
+
+export const dbService = { insertCredential, updateCampaignWithCredential, isExistingCredential }
+export const secretsService = {storeSecret}
