41197d318b13e465cc48418a58debe621cacc3af
Azima M I <miazima@gmail.com>
2020-04-16 11:44:03 +0800

    feat: change wording of validate credentials

--- frontend/src/components/dashboard/create/sms/SMSCredentials.tsx
+++ frontend/src/components/dashboard/create/sms/SMSCredentials.tsx
@@ -18,54 +18,66 @@ const SMSCredentials = ({ hasCredentials, onNext }: { hasCredentials: boolean; o
   return (
     <>
       <sub>Step 3</sub>
-      <h2>Insert your SMS credentials</h2>
+      {
+        hasCredentials
+          ? <h2>Your credentials have been validated successfully!</h2>
+          : (
+            <>
+              <h2>Insert your SMS credentials</h2>
 
-      <h5>Account SID</h5>
-      <TextInput
-        placeholder="Enter Account SID"
-        value={accountSid}
-        onChange={(e: React.ChangeEvent<HTMLInputElement>) => setAccountSid(e.target.value)}
-      />
+              <h5>Account SID</h5>
+              <TextInput
+                placeholder="Enter Account SID"
+                value={accountSid}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setAccountSid(e.target.value)}
+              />
 
-      <h5>API Key</h5>
-      <TextInput
-        placeholder="Enter API Key"
-        value={apiKey}
-        onChange={(e: React.ChangeEvent<HTMLInputElement>) => setApiKey(e.target.value)}
-      />
+              <h5>API Key</h5>
+              <TextInput
+                placeholder="Enter API Key"
+                value={apiKey}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setApiKey(e.target.value)}
+              />
 
-      <h5>API Secret</h5>
-      <TextInput
-        placeholder="Enter API Secret"
-        value={apiSecret}
-        onChange={(e: React.ChangeEvent<HTMLInputElement>) => setApiSecret(e.target.value)}
-      />
+              <h5>API Secret</h5>
+              <TextInput
+                placeholder="Enter API Secret"
+                value={apiSecret}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setApiSecret(e.target.value)}
+              />
 
-      <h5>Messaging Service ID</h5>
-      <TextInput
-        placeholder="Enter Messaging Service ID"
-        value={messagingServiceSid}
-        onChange={(e: React.ChangeEvent<HTMLInputElement>) => setMessagingServiceSid(e.target.value)}
-      />
+              <h5>Messaging Service ID</h5>
+              <TextInput
+                placeholder="Enter Messaging Service ID"
+                value={messagingServiceSid}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setMessagingServiceSid(e.target.value)}
+              />
 
-      <div className="separator"></div>
+              <div className="separator"></div>
 
-      <h2>We encourage you to do a test send</h2>
-      <p>To ensure your credentials are working perfectly, you may send a test SMS to double check for accuracy.</p>
-      <TextInputWithButton
-        type="tel"
-        value={mobileNumber}
-        onChange={(e: React.ChangeEvent<HTMLInputElement>) => setMobileNumber(e.target.value)}
-        onClick={() => testCredentials}
-        disabled={isDisabled()}
-      >
-        Validate credential
-      </TextInputWithButton>
+              <h2>Validate your credentials by doing a test send</h2>
+              <p>
+                To ensure your credentials are working perfectly,
+                please send a test SMS to an available phone numnber
+                to receive a preview of your message.
+              </p>
+              <TextInputWithButton
+                type="tel"
+                value={mobileNumber}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setMobileNumber(e.target.value)}
+                onClick={() => testCredentials}
+                disabled={isDisabled()}
+              >
+                Validate credential
+              </TextInputWithButton>
+            </>
+          )
+      }
 
       <div className="separator"></div>
 
       <div className="progress-button">
-        <PrimaryButton disabled={isDisabled()} onClick={onNext}>Send Message →</PrimaryButton>
+        <PrimaryButton disabled={!hasCredentials} onClick={onNext}>Send Message →</PrimaryButton>
       </div>
     </>
   )
