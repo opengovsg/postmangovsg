48d03265780ebbd827a10d541f092637c3dfd34b
Shao Wei <lshaowei18@gmail.com>
2020-04-16 14:46:38 +0800

    chore:  try catch for send mail

--- backend/src/email/middlewares/email.middleware.ts
+++ backend/src/email/middlewares/email.middleware.ts
@@ -3,15 +3,22 @@ import { Campaign } from '@core/models'
 import { ChannelType } from '@core/constants'
 import { mailClient } from '@core/services'
 import { MailToSend } from '@core/interfaces'
+import logger from '@core/logger'
 
 const sendEmail = async (email: string): Promise<boolean> => {
   // TODO: replace with hydrated email
   const mail: MailToSend = {
     recipients: [email],
     subject: 'Test Message',
-    body: 'Test message from postman.'
+    body: 'Test message from postman.',
   }
-  return await mailClient.sendMail(mail)
+  try {
+    return await mailClient.sendMail(mail)
+  } catch (e) {
+    logger.error(`Error while sending test email. error=${e}`)
+    return false
+  }
+  
 
 }
 
