95daffeb6b244a6d280d5ed5459b7ec067a3c4c6
Shao Wei <lshaowei18@gmail.com>
2020-04-17 12:04:13 +0800

    feat: Middleware to authenticate api key

--- backend/src/core/middlewares/is-authenticated.middleware.ts
+++ backend/src/core/middlewares/is-authenticated.middleware.ts
@@ -1,5 +1,13 @@
 import { Request, Response, NextFunction } from 'express'
+import { get } from 'lodash'
+import { hashService } from '@core/services'
 
+// SWTODO: Move to env var
+const API_KEY_SALT_V1 = '$2b$10$o5JEFyRcL9WHQFqmu8W3mO'
+// SWTODO: store this in DB
+const API_KEY_HASH  = '$2b$10$o5JEFyRcL9WHQFqmu8W3mOfw50tXCTj4WltIg0y3JycUVdqlcUs02'
+// SWTODO: Move this to config but not env var 
+const API_KEY_VERSION = 'v1'
 
 export const isCookieAuthenticated = async (req: Request, res: Response, next: NextFunction): Promise<Response | void> => {
   if (req.session?.user?.id) {
@@ -8,6 +16,18 @@ export const isCookieAuthenticated = async (req: Request, res: Response, next: N
   return res.sendStatus(401)
 }
 
+export const isApiKeyAuthenticated = async (req: Request, res: Response, next: NextFunction): Promise<Response | void> => {
+  const headerKey = `ApiKey-${API_KEY_VERSION}`
+  const authHeader = get(req, 'headers.authorization', '')
+  
+  const [header, apiKey] = authHeader.split(' ')
+
+  const hash = await hashService.specifySalt(apiKey, API_KEY_SALT_V1)
+
+  if (headerKey !== header || hash !== API_KEY_HASH) return res.sendStatus(401)
+
+  return next()
+}
 export const logout = async (req: Request, res: Response, next: NextFunction): Promise<Response | void> => {
   return new Promise <Response | void> ((resolve, reject) => {
    req.session?.destroy((err) => {
