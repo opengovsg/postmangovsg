dd69a8eddd4e71f84a2d67677006f4c0b8594342
Shao Wei <lshaowei18@gmail.com>
2020-04-17 14:23:48 +0800

    feat: new api-key table to store api key hash

---
+++ backend/src/core/models/api-key.ts
@@ -0,0 +1,17 @@
+import { Column, DataType, Model, Table } from 'sequelize-typescript'
+
+@Table({ tableName: 'api_key' , underscored: true, timestamps: true })
+export class ApiKey extends Model<Credential> {
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+    primaryKey: true,
+  })
+  hash!: string
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  email!: string
+}
