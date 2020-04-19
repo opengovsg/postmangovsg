d5ae70c011f1235235a3dc5c3ef69f54cdcf9b19
jeantanzj <33819199+jeantanzj@users.noreply.github.com>
2020-04-19 22:27:03 +0800

    chore: add default axios config (#66)

--- frontend/src/services/auth.service.ts
+++ frontend/src/services/auth.service.ts
@@ -1,34 +1,25 @@
 import axios from 'axios'
-import { POSTMAN_API_BASEURL } from 'config'
 
 async function getOtpWithEmail(email: string): Promise<void> {
-  return axios.post(`${POSTMAN_API_BASEURL}/auth/otp`, {
+  return axios.post('/auth/otp', {
     email,
-  }, {
-    withCredentials: true,
   })
 }
 
 async function loginWithOtp(email: string, otp: string): Promise<void> {
-  return axios.post(`${POSTMAN_API_BASEURL}/auth/login`, {
+  return axios.post('/auth/login', {
     email, otp,
-  }, {
-    withCredentials: true,
   })
 }
 
 async function getIsLoggedIn(): Promise<boolean> {
-  return axios.get(`${POSTMAN_API_BASEURL}/auth/login`, {
-    withCredentials: true,
-  }).then((response) => {
+  return axios.get('/auth/login').then((response) => {
     return response.status === 200
   })
 }
 
 async function logout(): Promise<void> {
-  return axios.get(`${POSTMAN_API_BASEURL}/auth/logout`, {
-    withCredentials: true,
-  })
+  return axios.get('/auth/logout')
 }
 
 export {
