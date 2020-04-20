c6077079e928bd9ee9167c2987156a740996fae4
jeantanzj <33819199+jeantanzj@users.noreply.github.com>
2020-04-20 20:31:40 +0800

    feat: send, stop, retry campaign (#77)

--- frontend/src/components/dashboard/create/sms/SMSSend.tsx
+++ frontend/src/components/dashboard/create/sms/SMSSend.tsx
@@ -4,11 +4,12 @@ import { ModalContext } from 'contexts/modal.context'
 import ConfirmModal from 'components/dashboard/confirm-modal'
 import { InfoBlock, PrimaryButton } from 'components/common'
 import styles from '../Create.module.scss'
+import { useParams } from 'react-router-dom'
 
 const SMSSend = ({ body, numRecipients }: { body: string; numRecipients: number }) => {
 
   const modalContext = useContext(ModalContext)
-
+  const params : { id? : string }= useParams()
   return (
     <>
       <sub>Step 4</sub>
@@ -27,7 +28,7 @@ const SMSSend = ({ body, numRecipients }: { body: string; numRecipients: number 
 
       <div className="progress-button">
         <PrimaryButton onClick={() => modalContext.setModalContent(
-          <ConfirmModal></ConfirmModal>
+          <ConfirmModal campaignId={+params.id!}></ConfirmModal>
         )}>
           Send campaign now
           <i className="bx bx-send"></i>
