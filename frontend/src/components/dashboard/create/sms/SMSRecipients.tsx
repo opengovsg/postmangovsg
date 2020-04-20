faa3b24cb2b6e578926e86b2742a855a715273a2
Seah Chin Ying <scy.6char@gmail.com>
2020-04-20 22:56:09 +0800

    feat: Add upload to frontend

--- frontend/src/components/dashboard/create/sms/SMSRecipients.tsx
+++ frontend/src/components/dashboard/create/sms/SMSRecipients.tsx
@@ -1,21 +1,68 @@
 import React, { useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 import { FileInput, InfoBlock, PrimaryButton } from 'components/common'
 
+import { getPresignedUrl, completeFileUpload } from 'services/sms.service'
+import axios from 'axios'
+
 const SMSRecipients = ({ csvFilename: initialCsvFilename, numRecipients: initialNumRecipients, onNext }: { csvFilename: string; numRecipients: number; onNext: (changes: any, next?: boolean) => void }) => {
 
+  const [errorMessage, setErrorMessage] = useState(null)
   const [csvFilename, setUploadedCsvFilename] = useState(initialCsvFilename)
   const [numRecipients, setNumRecipients] = useState(initialNumRecipients)
   const [isUploading, setIsUploading] = useState(false)
 
-  function uploadFile(files: File[]) {
-    const uploadedFile = files[0].name
+  const params: { id?: string } = useParams()
+
+  async function uploadFile(files: File[]) {
     setIsUploading(true)
-    setTimeout(() => {
-      setNumRecipients(100)
-      setUploadedCsvFilename(uploadedFile)
+
+    try {
+      // user did not select a file
+      if (!files[0]) {
+        return
+      }
+
+      const uploadedFile = files[0]
+      const campaignId = +params.id!
+
+      const startUploadResponse = await getPresignedUrl({
+        campaignId,
+        mimeType: uploadedFile.type,
+      })
+      console.log('obtained s3 presigned url')
+
+      const s3AxiosInstance = axios.create({
+        withCredentials: false,
+      })
+      await s3AxiosInstance.put(startUploadResponse.presignedUrl, uploadedFile, {
+        headers: { 'Content-Type': uploadedFile.type },
+      })
+      console.log('PUT to s3 succeeded')
+
+      // POST to upload complete
+      const uploadResponse = await completeFileUpload({
+        campaignId,
+        transactionId: startUploadResponse.transactionId,
+      })
+
+      setUploadedCsvFilename(uploadedFile.name)
+      setNumRecipients(uploadResponse.num_recipients)
+
+    } catch (err) {
+      const axiosError = err.response?.data
+      if (axiosError?.statusCode === 400) {
+        setErrorMessage(axiosError?.message)
+      }
+      if (axiosError) {
+        console.error(axiosError)
+      } else {
+        console.error(err)
+      }
+    } finally {
       setIsUploading(false)
-    }, 1000)
+    }
   }
 
   return (
@@ -38,6 +85,17 @@ const SMSRecipients = ({ csvFilename: initialCsvFilename, numRecipients: initial
         </InfoBlock>
       }
       <FileInput isProcessing={isUploading} onFileSelected={uploadFile} />
+
+      {errorMessage
+      ?
+      <div>
+        Error here: {errorMessage}
+      </div>
+      :
+      <></>
+      }
+
+
       <div className="progress-button">
         <PrimaryButton disabled={!numRecipients || !csvFilename} onClick={() => onNext({ csvFilename, numRecipients })}>Insert Credentials →</PrimaryButton>
       </div>
