ce03b35c8bed910f2f302bdca9f7777df8a4f2c2
Shao Wei <lshaowei18@gmail.com>
2020-04-21 16:56:57 +0800

    chore: Change db script to update users table

--- backend/scripts/database/insert-api-hash.sql
+++ backend/scripts/database/insert-api-hash.sql
@@ -1,5 +1,6 @@
 -- Copy and paste this into the sql query.
 -- Make sure to change the values ie: 'HASH', 'EMAIL'
 
-INSERT INTO api_keys("hash", "email", "created_at", "updated_at")
-VALUES('HASH', 'EMAIL', clock_timestamp(), clock_timestamp())
+UPDATE "users"
+SET "api_key" = 'HASH', "updated_at" = clock_timestamp()
+WHERE "email" = 'EMAIL';
