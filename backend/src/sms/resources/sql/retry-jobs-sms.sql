780ac0087775c0beab7e10dd220d8f348694a4bb
Jean <33819199+jeantanzj@users.noreply.github.com>
2020-04-22 11:05:47 +0800

    chore: [backend] remove worker-related files

--- backend/src/sms/resources/sql/retry-jobs-sms.sql
+++
@@ -1,8 +0,0 @@
--- Called by the core.retry_jobs() function
-CREATE OR REPLACE FUNCTION retry_jobs_sms(selected_campaign_id int) RETURNS VOID
-LANGUAGE plpgsql
-AS $$
-BEGIN
-    UPDATE sms_messages SET dequeued_at = NULL WHERE campaign_id = selected_campaign_id;
-END $$
-
