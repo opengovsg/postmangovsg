296066f62dd6f6ad4cdbd0c459ec797952e658f2
Jean <33819199+jeantanzj@users.noreply.github.com>
2020-04-23 19:50:17 +0800

    chore: [worker] remove sequelize loader

--- worker/src/core/loaders/index.ts
+++ worker/src/core/loaders/index.ts
@@ -1,9 +1,7 @@
 import scriptLoader from './script.loader'
 import messageWorkerLoader from './message-worker.loader'
-import SequelizeLoader from './sequelize.loader'
 const loaders = async (): Promise<void> => {
-  await SequelizeLoader.load()
-  await scriptLoader({ sequelize: SequelizeLoader.sequelize })
+  await scriptLoader()
   await messageWorkerLoader()
 }
 
--- worker/src/core/loaders/script.loader.ts
+++ worker/src/core/loaders/script.loader.ts
@@ -1,39 +1,44 @@
 import fs from 'fs'
-import { Sequelize } from 'sequelize/types'
+import { Sequelize } from 'sequelize-typescript'
 import logger from '@core/logger'
+import config from '@core/config'
 import { sqlFilePaths } from '@core/resources/sql'
 import { sqlFilePaths as emailSqlFilePaths } from '@email/resources/sql'
 import { sqlFilePaths as smsSqlFilePaths } from '@sms/resources/sql'
 
-const scriptLoader = async ({ sequelize }: {sequelize: Sequelize | undefined}): Promise<void> => {
-  if(sequelize){
-    const scriptsFilePaths = [
-      ...sqlFilePaths,
-      ...emailSqlFilePaths,
-      ...smsSqlFilePaths,
-    ]
-    const scripts = scriptsFilePaths.map((filePath: string) => {
-      return new Promise((resolve, reject) => {
-        fs.readFile(filePath, 'utf8', (err, data) => {
-          if (err) reject(err)
-          resolve(data)
-        })
-      }).then((data) => {
-        return sequelize.query(data as string)
+const scriptLoader = async (): Promise<void> => {
+  const dialectOptions = config.IS_PROD ? { ...config.database.dialectOptions } : {}
+  const sequelize = new Sequelize(config.database.databaseUri, {
+    dialect: 'postgres',
+    logging: false,
+    pool: config.database.poolOptions,
+    ...dialectOptions,
+  })
+  await sequelize.sync()
+  
+  const scriptsFilePaths = [
+    ...sqlFilePaths,
+    ...emailSqlFilePaths,
+    ...smsSqlFilePaths,
+  ]
+  const scripts = scriptsFilePaths.map((filePath: string) => {
+    return new Promise((resolve, reject) => {
+      fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) reject(err)
+        resolve(data)
       })
+    }).then((data) => {
+      return sequelize.query(data as string)
     })
-    return Promise.all(scripts)
-      .then(() => {
-        logger.info('Sql scripts loaded')
-      })
-      .catch(err => {
-        logger.error(`Could not load sql scripts from ${scriptsFilePaths} \t ${err}`)
-        process.exit(1)
-      })
-  } else{
-    logger.error('Sequelize connection was not instantiated')
-  }
- 
+  })
+  return Promise.all(scripts)
+    .then(() => {
+      logger.info('Sql scripts loaded')
+    })
+    .catch(err => {
+      logger.error(`Could not load sql scripts from ${scriptsFilePaths} \t ${err}`)
+      process.exit(1)
+    })
 }
 
 export default scriptLoader
