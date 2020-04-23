866447dfc3a56a9aa81be35457b54ff8314462fd
Ian Chen <ian-@gmx.com>
2020-04-23 15:14:34 +0800

    chore: Increase otp expiry and refactor otp config variables

--- backend/src/core/middlewares/auth.middleware.ts
+++ backend/src/core/middlewares/auth.middleware.ts
@@ -5,10 +5,9 @@ import { User } from '@core/models'
 import { HashedOtp, VerifyOtpInput } from '@core/interfaces'
 import logger from '@core/logger'
 import { otpClient, mailClient, hashService } from '@core/services'
+import config from '@core/config'
 
-const RETRIES = 4 // Number of attempts to enter otp
-const EXPIRY_IN_SECONDS = 300 //expires after 5 minutes
-const WAIT_IN_SECONDS = 30 // Number of seconds to wait before resending otp
+const { retries: OTP_RETRIES, expiry: OTP_EXPIRY, resendTimeout: OTP_RESEND_TIMEOUT } = config.otp
 
 const generateOtp = (): string => {
   return authenticator.generate(authenticator.generateSecret())
@@ -16,7 +15,7 @@ const generateOtp = (): string => {
 
 const saveHashedOtp = (email: string, hashedOtp: HashedOtp): Promise<boolean> => {
   return new Promise((resolve, reject) => {
-    otpClient.set(email, JSON.stringify(hashedOtp), 'EX', EXPIRY_IN_SECONDS, (error) => {
+    otpClient.set(email, JSON.stringify(hashedOtp), 'EX', OTP_EXPIRY, (error) => {
       if (error) {
         logger.error(`Failed to save hashed otp: ${error}`)
         reject(error)
@@ -54,19 +53,19 @@ const deleteHashedOtp = async (email: string): Promise<boolean> => {
 }
 
 const hasWaitTimeElapsed = async (email: string): Promise<void> => {
-  const existingHash: HashedOtp | null =  await getHashedOtp(email).catch(() => {
+  const existingHash: HashedOtp | null = await getHashedOtp(email).catch(() => {
     // If there is no hash, just proceed
     return null
   })
 
   // Check that at least WAIT_IN_SECONDS has elapsed before allowing the resend of a new otp
-  if(existingHash){
-    const remainingTime = Math.ceil ( (existingHash.createdAt + (WAIT_IN_SECONDS*1000) - (new Date()).getTime()) / 1000 )
-    if(remainingTime > 0) throw new Error(`Wait for ${remainingTime} seconds before requesting for a new otp`)
+  if (existingHash) {
+    const remainingTime = Math.ceil((existingHash.createdAt + (OTP_RESEND_TIMEOUT * 1000) - (new Date()).getTime()) / 1000)
+    if (remainingTime > 0) throw new Error(`Wait for ${remainingTime} seconds before requesting for a new otp`)
   }
 }
 
-const isOtpVerified = async (input: VerifyOtpInput ): Promise<boolean> => {
+const isOtpVerified = async (input: VerifyOtpInput): Promise<boolean> => {
   try {
     const hashedOtp: HashedOtp = await getHashedOtp(input.email)
     const authorized: boolean = await bcrypt.compare(input.otp, hashedOtp.hash)
@@ -105,7 +104,7 @@ const sendOtp = (recipient: string, otp: string): Promise<string | void> => {
   return mailClient.sendMail({
     recipients: [recipient],
     subject: 'One-Time Password (OTP) for Postman.gov.sg',
-    body: `Your OTP is <b>${otp}</b>. It will expire in ${Math.floor(EXPIRY_IN_SECONDS / 60)} minutes.
+    body: `Your OTP is <b>${otp}</b>. It will expire in ${Math.floor(OTP_EXPIRY / 60)} minutes.
     Please use this to login to your Postman.gov.sg account. <p>If your OTP does not work, please request for a new OTP.</p>`,
   })
 }
@@ -123,14 +122,14 @@ const getOtp = async (req: Request, res: Response): Promise<Response> => {
   try {
     const otp = generateOtp()
     const hashValue = await hashService.randomSalt(otp)
-    const hashedOtp: HashedOtp = { hash: hashValue, retries: RETRIES, createdAt: Date.now() }
+    const hashedOtp: HashedOtp = { hash: hashValue, retries: OTP_RETRIES, createdAt: Date.now() }
     await saveHashedOtp(email, hashedOtp)
     await sendOtp(email, otp)
   } catch (e) {
     logger.error(`Error sending OTP: ${e}. email=${email}`)
     return res.sendStatus(500)
   }
-  
+
   return res.sendStatus(200)
 }
 
