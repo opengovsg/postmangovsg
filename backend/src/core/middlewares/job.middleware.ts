4f26f4a8e7f053a1bfc51e26628de25e6b61f9c8
Jean <33819199+jeantanzj@users.noreply.github.com>
2020-04-27 18:06:10 +0800

    chore: distribute rate evenly

--- backend/src/core/middlewares/job.middleware.ts
+++ backend/src/core/middlewares/job.middleware.ts
@@ -6,18 +6,19 @@ import config from '@core/config'
 const sendCampaign = async (req: Request, res: Response, next: NextFunction): Promise<Response | void> => {
   try{
     const { campaignId } = req.params
-    const rate = +req.body.rate
+    let rate = +req.body.rate
     const canSend = await Campaign.findOne({ where: { id: campaignId, valid: true, credName: { [Op.ne]: null } } })
     if(canSend){
       // Split jobs if the supplied send rate is higher than the rate 1 worker can support
-      // As we want to minimize the number of workers being used, the rate is not distributed across workers.
-      // Instead, each worker will operate at its max capacity. Eg: rate=400, maxRate=150 will be split into 150,150,100
+      // The rate is distributed evenly across workers.
       const jobs = []
       const numJobs = Math.ceil(rate / config.maxRatePerJob)
-      for(let i=0; i<numJobs; i++) {
-        const sendRate = Math.min(config.maxRatePerJob, rate - i*config.maxRatePerJob) 
-        jobs.push(createJob({ campaignId: +campaignId, rate: sendRate }))
+      const sendRate = Math.ceil(rate/numJobs)
+      for (let i = 0; i < numJobs; i++){
+        jobs.push(createJob({ campaignId: +campaignId, rate: Math.min(sendRate, rate) }))
+        rate-=sendRate
       }
+    
       const jobIds = await Promise.all(jobs)
       return res.status(200).json({ 'campaign_id': campaignId, 'job_id': jobIds })
     }
