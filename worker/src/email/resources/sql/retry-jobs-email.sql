c9451aec93b206df543c62c892a2c96c935f7158
jeantanzj <33819199+jeantanzj@users.noreply.github.com>
2020-04-27 16:32:09 +0800

    chore: set dequeued_at to null only when message_id is null (#138)

--- worker/src/email/resources/sql/retry-jobs-email.sql
+++ worker/src/email/resources/sql/retry-jobs-email.sql
@@ -3,6 +3,6 @@ CREATE OR REPLACE FUNCTION retry_jobs_email(selected_campaign_id int) RETURNS VO
 LANGUAGE plpgsql
 AS $$
 BEGIN
-    UPDATE email_messages SET dequeued_at = NULL WHERE campaign_id = selected_campaign_id;
+    UPDATE email_messages SET dequeued_at = NULL WHERE campaign_id = selected_campaign_id AND message_id IS NULL;
 END $$
 
