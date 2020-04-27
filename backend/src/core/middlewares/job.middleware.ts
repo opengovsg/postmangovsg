a30854472f18d044f935796f6b27b4b8474ee3dd
jeantanzj <33819199+jeantanzj@users.noreply.github.com>
2020-04-27 22:59:54 +0800

    feat: split jobs according to send rate (#145)
    * feat: split jobs according to send rate
    
    * chore: distribute rate evenly

--- backend/src/core/middlewares/job.middleware.ts
+++ backend/src/core/middlewares/job.middleware.ts
@@ -2,14 +2,25 @@ import { Request, Response, NextFunction } from 'express'
 import { createJob, stopCampaign as stop, retryCampaign as retry } from '@core/services'
 import { Campaign } from '@core/models'
 import { Op } from 'sequelize'
+import config from '@core/config'
 const sendCampaign = async (req: Request, res: Response, next: NextFunction): Promise<Response | void> => {
   try{
     const { campaignId } = req.params
-    const { rate } = req.body
+    let rate = +req.body.rate
     const canSend = await Campaign.findOne({ where: { id: campaignId, valid: true, credName: { [Op.ne]: null } } })
     if(canSend){
-      const jobId = await createJob({ campaignId: + campaignId, rate })
-      return res.status(200).json({ 'campaign_id': campaignId, 'job_id': jobId })
+      // Split jobs if the supplied send rate is higher than the rate 1 worker can support
+      // The rate is distributed evenly across workers.
+      const jobs = []
+      const numJobs = Math.ceil(rate / config.maxRatePerJob)
+      const sendRate = Math.ceil(rate/numJobs)
+      for (let i = 0; i < numJobs; i++){
+        jobs.push(createJob({ campaignId: +campaignId, rate: Math.min(sendRate, rate) }))
+        rate-=sendRate
+      }
+    
+      const jobIds = await Promise.all(jobs)
+      return res.status(200).json({ 'campaign_id': campaignId, 'job_id': jobIds })
     }
     return res.status(400).json({ message: 'Unable to send campaign due to invalid template, recipients or missing credentials.' })
   }
