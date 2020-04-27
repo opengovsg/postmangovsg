96253080b9ed7258827991edfa2c6479356629ae
Seah Chin Ying <scy.6char@gmail.com>
2020-04-27 18:01:01 +0800

    chore: log failure of log transport creation

--- backend/src/core/loaders/cloudwatch.loader.ts
+++ backend/src/core/loaders/cloudwatch.loader.ts
@@ -6,12 +6,18 @@ import { getInstanceId } from '@core/utils/ec2'
 
 const cloudwatchLoader = async (): Promise<void> => {
   const instanceId = await getInstanceId()
-  if (instanceId) {
-    logger.add(new WinstonCloudwatch({
-      logGroupName: config.aws.logGroupName,
-      logStreamName: instanceId,
-      awsRegion: config.aws.awsRegion,
-    }))
+  try {
+    if (instanceId) {
+      logger.info({ message: `Detetcted instanceId as ${instanceId}`})
+      logger.add(new WinstonCloudwatch({
+        logGroupName: config.aws.logGroupName,
+        logStreamName: instanceId,
+        awsRegion: config.aws.awsRegion,
+      }))
+    }
+  } catch (err) {
+    console.error(err)
+    console.error('Unable to add winston cloudwatch transport')
   }
 }
 
