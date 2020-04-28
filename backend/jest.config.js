2ec631d32bf55df6429717bcb328e8e6a580b4c3
Shao Wei <lshaowei18@gmail.com>
2020-04-28 11:59:21 +0800

    chore: Add @ modules to jest config

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -6,6 +6,11 @@ module.exports = {
     "**/__tests__/**/*.+(ts|tsx|js)",
     "**/?(*.)+(spec|test).+(ts|tsx|js)"
   ],
+  'moduleNameMapper': {
+    '@core/(.*)': '<rootDir>/src/core/$1',
+    '@sms/(.*)': '<rootDir>/src/sms/$1',
+    '@email/(.*)': '<rootDir>/src/email/$1'
+  },
   "transform": {
     "^.+\\.(ts|tsx)$": "ts-jest"
   },
