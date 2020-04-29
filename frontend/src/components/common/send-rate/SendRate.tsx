7dbff68f78184a71b75697abd16e6c5a549cfbad
Azima M I <miazima@gmail.com>
2020-04-29 11:34:34 +0800

    feat: implement send rate component with styling

---
+++ frontend/src/components/common/send-rate/SendRate.tsx
@@ -0,0 +1,53 @@
+import React, { Dispatch, SetStateAction, useState } from 'react'
+import cx from 'classnames'
+
+import { TextInput } from 'components/common'
+import styles from './SendRate.module.scss'
+
+const SendRate = ({ sendRate, setSendRate }: {sendRate: string; setSendRate: Dispatch<SetStateAction<string>>}) => {
+
+  const [useCustomRate, setUseCustomRate] = useState(false)
+
+  return (
+    <>
+      <div className={styles.title}>
+        <span><b>Send rate</b> <i>optional</i></span>
+        <i
+          className={cx(styles.icon, 'bx bxs-down-arrow', { [styles.rotateArrow]: useCustomRate })}
+          onClick={() => setUseCustomRate(!useCustomRate)}
+        />
+      </div>
+
+      {
+        useCustomRate &&
+        <>
+          <p>You can send messages at a rapid rate, as long as the requests do not max out Twilio's
+            REST API concurrency limit.&nbsp;
+          <a
+            href="https://guide.postman.gov.sg/poweruser.html"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.link}>
+            Learn more about send rate limits
+          </a>
+          </p>
+
+          <p>Default rate is 10 messages/ second. If you have raised your send rate with Twilio previously,
+            please enter your new send rate here. We will optimise our sending to match what Twilio has
+            configured for your account.</p>
+
+          <TextInput
+            type="tel"
+            value={sendRate}
+            maxLength="4"
+            className={styles.input}
+            onChange={(str: string) => setSendRate(str.replace(/\D/g, ''))}
+            placeholder='enter send rate'
+          />
+        </>
+      }
+    </>
+  )
+}
+
+export default SendRate
