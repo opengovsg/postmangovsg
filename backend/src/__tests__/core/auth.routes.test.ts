9f5af13054c20152faffda42ddd6fa1020cc5e90
Shao Wei <lshaowei18@gmail.com>
2020-04-29 11:31:42 +0800

    chore: initial test

---
+++ backend/src/__tests__/core/auth.routes.test.ts
@@ -0,0 +1,21 @@
+import getApp from '../../app'
+import { Application } from 'express'
+import request from 'supertest'
+jest.mock('@core/services/mail.service')
+
+let app: Application
+
+beforeAll(async() => {
+  app = await getApp()
+})
+
+describe('POST /auth/otp', () => {
+  test('Should respond with a 200 when there is email in body', async () => {
+    const response = await(request(app)
+      .post('/v1/auth/otp')
+      .send({
+        email: 'test@open.gov.sg'
+      }))
+      expect(response.status).toBe(200)
+  })
+})
