378af65650ece7fb2e6a9a03de8b3e5755a053e5
Shao Wei <lshaowei18@gmail.com>
2020-04-30 06:50:20 +0800

    chore: create jest config file

---
+++ backend/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  "roots": [
+    "<rootDir>/src"
+  ],
+  "testMatch": [
+    "**/__tests__/**/*.+(ts|tsx|js)",
+    "**/?(*.)+(spec|test).+(ts|tsx|js)"
+  ],
+  'moduleNameMapper': {
+    '@core/(.*)': '<rootDir>/src/core/$1',
+    '@sms/(.*)': '<rootDir>/src/sms/$1',
+    '@email/(.*)': '<rootDir>/src/email/$1'
+  },
+  "testEnvironment": "node",
+} 
