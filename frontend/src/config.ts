53fa54a64d5c12b015e4c4a21e236c695d8e39c9
Ian Chen <ian-@gmx.com>
2020-04-30 17:00:36 +0800

    chore: add missing hrefs, guide open in new tab (#186)

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,5 +1,6 @@
 import axios from 'axios'
 export const POSTMAN_GUIDE_URL = 'https://guide.postman.gov.sg'
+export const CONTACT_US_URL = 'https://form.gov.sg/#!/5e8db1736d789b0011743202'
 export const POSTMAN_API_BASEURL = process.env.REACT_APP_BACKEND_URL as string
 
 // axios global defaults
