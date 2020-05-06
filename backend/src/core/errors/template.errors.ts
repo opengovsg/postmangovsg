ba36ccf64aa151a9cfecd6469f9baa28a669780a
Shao Wei <lshaowei18@gmail.com>
2020-05-06 16:44:14 +0800

    chore: Improve missing template keys error message

--- backend/src/core/errors/template.errors.ts
+++ backend/src/core/errors/template.errors.ts
@@ -1,7 +1,7 @@
 export class MissingTemplateKeysError extends Error {
   public readonly missingKeys: string[]
   constructor(missingKeys: string[]) {
-    super(`Template contains keys [${missingKeys}] that are not present in uploaded recipient list.`)
+    super(`The attribute(s) { ${missingKeys} } are not present in uploaded recipient list.`)
     this.missingKeys = missingKeys
     Object.setPrototypeOf(this, new.target.prototype) // restore prototype chain
     Error.captureStackTrace(this)
