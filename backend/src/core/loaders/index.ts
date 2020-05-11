8cc47a7671a20bf477a8d7c5101e6a76d7f4dfa6
jeantanzj <33819199+jeantanzj@users.noreply.github.com>
2020-05-11 12:31:44 +0800

    refactor: restore sequelizeLoader to consistent format (#208)

--- backend/src/core/loaders/index.ts
+++ backend/src/core/loaders/index.ts
@@ -1,16 +1,16 @@
+import { Application } from 'express'
 import securityHeadersLoader from './security-headers.loader'
 import expressLoader from './express.loader'
 import swaggerLoader from './swagger.loader'
 import sessionLoader from './session.loader'
-import SequelizeLoader from './sequelize.loader'
-import { Application } from 'express'
+import sequelizeLoader from './sequelize.loader'
 import cloudwatchLoader from './cloudwatch.loader'
 
 
 const loaders = async ({ app }: { app: Application }): Promise<void> => {
   securityHeadersLoader({ app })
   await cloudwatchLoader()
-  await SequelizeLoader.load()
+  await sequelizeLoader()
   await sessionLoader({ app })
   await expressLoader({ app })
   await swaggerLoader({ app })
--- backend/src/core/services/job.service.ts
+++ backend/src/core/services/job.service.ts
@@ -1,8 +1,8 @@
-import SequelizeLoader from '@core/loaders/sequelize.loader'
+import { Campaign } from '@core/models'
 import { QueryTypes } from 'sequelize'
 import get from 'lodash/get'
 const createJob = async ({ campaignId, rate }: {campaignId: number; rate: number}): Promise<number | undefined> => {
-  const job = await SequelizeLoader.sequelize?.query('SELECT insert_job(:campaignId, :rate);',
+  const job = await Campaign.sequelize?.query('SELECT insert_job(:campaignId, :rate);',
     {
       replacements: { campaignId, rate }, type: QueryTypes.SELECT,
     })
@@ -10,13 +10,13 @@ const createJob = async ({ campaignId, rate }: {campaignId: number; rate: number
   return jobId ? Number(jobId) : undefined
 }
 const stopCampaign = (campaignId: number): Promise<any> | undefined => {
-  return SequelizeLoader.sequelize?.query('SELECT stop_jobs(:campaignId);',
+  return Campaign.sequelize?.query('SELECT stop_jobs(:campaignId);',
     {
       replacements: { campaignId }, type: QueryTypes.SELECT,
     })
 }
 const retryCampaign = (campaignId: number): Promise<any> | undefined  => {
-  return SequelizeLoader.sequelize?.query('SELECT retry_jobs(:campaignId);',
+  return Campaign.sequelize?.query('SELECT retry_jobs(:campaignId);',
     {
       replacements: { campaignId }, type: QueryTypes.SELECT,
     })
