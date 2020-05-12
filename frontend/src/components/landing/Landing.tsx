2bae6cc28201071d051418748b6116d3591ece17
Shao Wei <lshaowei18@gmail.com>
2020-05-12 10:10:52 +0800

    feat: how it works

--- frontend/src/components/landing/Landing.tsx
+++ frontend/src/components/landing/Landing.tsx
@@ -13,7 +13,11 @@ import appLogo from 'assets/img/brand/app-logo.svg'
 import companyLogo from 'assets/img/brand/company-logo.svg'
 import arrowRight from 'assets/img/arrow-right.svg'
 import ogpLogo from 'assets/img/ogp-logo.svg'
-import mohAgency from 'assets/img/moh.png'
+import mohAgency from 'assets/img/landing/moh.png'
+import chooseChannelImg from 'assets/img/landing/choose-channel.svg'
+import createMessageImg from 'assets/img/landing/create-message.svg'
+import uploadContactsImg from 'assets/img/landing/upload-contacts.svg'
+import sendMessageImg from 'assets/img/landing/send-message.svg'
 
 const Landing = () => {
   const authContext = useContext(AuthContext)
@@ -26,6 +30,10 @@ const Landing = () => {
 
   function directToSignIn() {
     window.location.href = '/signin'
+  }
+
+  function directToGuide() {
+    window.location.href = POSTMAN_GUIDE_URL
   }
 
   return (
@@ -60,6 +68,40 @@ const Landing = () => {
             </div>
           </div>
       </div >
+      <div className={styles.howItWorks}>
+        <h1>How it works</h1>
+        <h2>Send mass messages to citizens with minimum setup required</h2>
+
+        <div className={styles.features}>
+          <div className={styles.feature}>
+            <img src={chooseChannelImg} alt="Choose channel"/>
+            <h3>Choose Channel</h3>
+            <span className={styles.featureText}>Select either SMS or email and name the campaign</span>
+          </div>
+          
+          <div className={styles.feature}>
+            <img src={createMessageImg} alt="Create message"/>
+            <h3>Create Message</h3>
+            <span className={styles.featureText}>Personalise messages using attributes</span>
+          </div>
+
+          <div className={styles.feature}>
+            <img src={uploadContactsImg} alt="Upload contacts"/>
+            <h3>Upload Contacts</h3>
+            <span className={styles.featureText}>Upload recipients list in CSV format</span>
+          </div>
+
+          <div className={styles.feature}>
+            <img src={sendMessageImg} alt="Send message"/>
+            <h3>Send Message</h3>
+            <span className={styles.featureText}>Do a test send before mass sendout</span>
+          </div>
+        </div>
+
+        <PrimaryButton className={styles.learnMoreBtn} onClick={directToGuide}>
+          Learn more <img className={styles.arrowRight} src={arrowRight} alt="Right arrow"/>
+        </PrimaryButton>
+      </div>
       <div className={styles.bottomContainer}>
         <div className={styles.bottomContent}>
           <img className={styles.companyLogo} src={companyLogo} alt="OGP"></img>
