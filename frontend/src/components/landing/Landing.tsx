3f68bcd01b331bd83ea28d14012f9b89d5bb8e47
Shao Wei <lshaowei18@gmail.com>
2020-05-12 16:28:30 +0800

    feat: why use postman section

--- frontend/src/components/landing/Landing.tsx
+++ frontend/src/components/landing/Landing.tsx
@@ -14,10 +14,17 @@ import companyLogo from 'assets/img/brand/company-logo.svg'
 import arrowRight from 'assets/img/arrow-right.svg'
 import ogpLogo from 'assets/img/ogp-logo.svg'
 import mohAgency from 'assets/img/landing/moh.png'
+
+// how it works
 import chooseChannelImg from 'assets/img/landing/choose-channel.svg'
 import createMessageImg from 'assets/img/landing/create-message.svg'
 import uploadContactsImg from 'assets/img/landing/upload-contacts.svg'
 import sendMessageImg from 'assets/img/landing/send-message.svg'
+
+import reliableImg from 'assets/img/landing/WhyUsePostman-1.svg'
+import reattemptImg from 'assets/img/landing/WhyUsePostman-2.svg'
+import trackImg from 'assets/img/landing/WhyUsePostman-3.svg'
+import faceImg from 'assets/img/landing/WhyUsePostman-face.png'
 
 const Landing = () => {
   const authContext = useContext(AuthContext)
@@ -32,6 +39,7 @@ const Landing = () => {
     window.location.href = '/signin'
   }
 
+  // how it works
   function directToGuide() {
     window.location.href = POSTMAN_GUIDE_URL
   }
@@ -68,6 +76,7 @@ const Landing = () => {
             </div>
           </div>
       </div >
+
       <div className={styles.howItWorks}>
         <h1>How it works</h1>
         <h2>Send mass messages to citizens with minimum setup required</h2>
@@ -101,6 +110,44 @@ const Landing = () => {
         <PrimaryButton className={styles.learnMoreBtn} onClick={directToGuide}>
           Learn more <img className={styles.arrowRight} src={arrowRight} alt="Right arrow"/>
         </PrimaryButton>
+      </div>
+
+      <div className={styles.whyUsePostman}>
+        <div className={styles.innerContainer}>
+          <h1>Why use Postman</h1>
+          <div className={styles.reason}>
+            <img src={reliableImg} alt="Reliable"/>
+            <div className={styles.textContainer}>
+              <h3>Reliable</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore</p>
+            </div>
+          </div>
+
+          <div className={styles.reason}>
+            <div className={styles.textContainer}>
+              <h3>Reattempt delivery</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore</p>
+            </div>
+            <img src={reattemptImg} alt="Reattempt delivery"/>
+          </div>
+
+          <div className={styles.reason}>
+            <img src={trackImg} alt="Track engagement"/>
+            <div className={styles.textContainer}>
+              <h3>Track engagement</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore</p>
+            </div>
+          </div>
+
+          <div className={styles.testimonial}>
+            <img src={faceImg} alt="User's face"/>
+            <div className={styles.textContainer}>
+              <h3>Lorem ipsum dolor sit amet</h3>
+              <p className={styles.longText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tellus id interdum velit laoreet id donec ultrices. Amet cursus sit amet dictum sit amet justo donec enim. Mi proin sed libero enim. Aenean et tortor at risus viverra adipiscing at in. Lectus vestibulum mattis ullamcorper velit.</p>
+              <span>Lorem ipsum dolor, Agency A</span>
+            </div>
+          </div>          
+        </div>
       </div>
       <div className={styles.bottomContainer}>
         <div className={styles.bottomContent}>
