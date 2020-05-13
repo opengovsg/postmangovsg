682bf83908aa7a49d4d53172be6d9b6fb7cf6931
jeantanzj <33819199+jeantanzj@users.noreply.github.com>
2020-05-13 09:43:50 +0800

    chore: remove api key script (#224)

--- backend/bin/api-key/insert-api-hash.sql
+++
@@ -1,6 +0,0 @@
--- Copy and paste this into the sql query.
--- Make sure to change the values ie: 'HASH', 'EMAIL'
-
-UPDATE "users"
-SET "api_key" = 'HASH', "updated_at" = clock_timestamp()
-WHERE "email" = 'EMAIL';
