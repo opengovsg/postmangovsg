db2e5fba6cbc78370739279f1d9f04ea939027ec
Daryl Chan <dckj95@gmail.com>
2020-05-14 19:37:48 +0800

    fix: Implement wrapper to hide overflow when modal is open (5)

---
+++ frontend/src/components/common/body-wrapper/BodyWrapper.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import cx from 'classnames'
+
+import styles from './BodyWrapper.module.scss'
+
+interface BodyWrapperProps {
+  /**
+   * Hides any body content that overflows the viewport if set to `true`.
+   * This prevents an external scrollbar from appearing when the modal is open.
+   */
+  wrap?: boolean;
+}
+
+const BodyWrapper: React.FunctionComponent<BodyWrapperProps> = ({
+  wrap,
+  children,
+}) => <div className={cx({ [styles.bodyWrapper]: wrap })}>{children}</div>
+
+export default BodyWrapper
