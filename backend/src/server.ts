41db190ff018556721c4a7f13198ea156e9fdbea
Jean <33819199+jeantanzj@users.noreply.github.com>
2020-05-16 00:08:02 +0800

    chore: refactor check for required env vars

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,20 +1,8 @@
 require('dotenv').config()
 require('module-alias/register')
 
-const checkRequiredEnvVars = (vars: Array<string>): boolean => {
-  vars.forEach(v => {
-    if (!process.env[v]) {
-      // do not use winston logger here since we may require certain env vars for logger in the future
-      // eslint-disable-next-line no-console
-      console.log(`${v} environment variable is not set!`)
-      throw new Error(`${v} environment variable is not set!`)
-    }
-  })
-  return true
-}
-
 /** SENSITIVE CONFIGURATION **/
-const requiredEnvVars = [
+const missingEnvVars = [
   // Database settings
   'DB_URI',
   // Cache settings
@@ -28,9 +16,14 @@ const requiredEnvVars = [
   'JWT_SECRET',
   // For API Key hashing 
   'API_KEY_SALT_V1',
-]
+].reduce(function (acc: string[], name: string){
+  if (process.env[name] === undefined) acc.push(name)
+  return acc
+},[])
 
-checkRequiredEnvVars(requiredEnvVars)
+if (missingEnvVars.length>0) {
+  throw new Error(`Missing required environment variables: ${missingEnvVars}`)
+}
 
 /** Load the app after we make sure that the sensitive env vars exist */
 import 'source-map-support/register'
