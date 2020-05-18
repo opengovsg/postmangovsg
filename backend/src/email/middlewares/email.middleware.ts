67d946179bfef27fe95817d2db6fd2026ce3a0c0
Jean <33819199+jeantanzj@users.noreply.github.com>
2020-05-18 19:22:33 +0800

    refactor: get campaign message for sending test message

--- backend/src/email/middlewares/email.middleware.ts
+++ backend/src/email/middlewares/email.middleware.ts
@@ -1,6 +1,12 @@
 import { Request, Response, NextFunction } from 'express'
 import { EmailService } from '@email/services'
 
+/**
+ * Checks if the campaign id supplied is indeed a campaign of the 'Email' type, and belongs to the user
+ * @param req 
+ * @param res 
+ * @param next 
+ */
 const isEmailCampaignOwnedByUser = async (req: Request, res: Response, next: NextFunction): Promise<Response | void> => {
   const { campaignId } = req.params
   const userId = req.session?.user?.id
@@ -12,19 +18,29 @@ const isEmailCampaignOwnedByUser = async (req: Request, res: Response, next: Nex
   }
 }
 
-// Sends a test email
+/**
+ * Sends a test message. If the test message succeeds, store the credentials 
+ * @param req 
+ * @param res 
+ */
 const validateAndStoreCredentials = async (req: Request, res: Response): Promise<Response | void> => {
   try {
     const { campaignId } = req.params
     const { recipient } = req.body
     await EmailService.sendCampaignMessage(+campaignId, recipient)
-    await EmailService.updateCredentials(+campaignId)
+    await EmailService.setCampaignCredential(+campaignId)
   } catch (err) {
     return res.status(400).json({ message: `${err.message}` })
   }
   return res.json({ message: 'OK' })
-
 }
+
+/**
+ * Gets details of a campaign and the number of recipients that have been uploaded for this campaign
+ * @param req 
+ * @param res 
+ * @param next 
+ */
 const getCampaignDetails = async (req: Request, res: Response, next: NextFunction): Promise<Response | void> => {
   try {
     const { campaignId } = req.params
@@ -35,13 +51,19 @@ const getCampaignDetails = async (req: Request, res: Response, next: NextFunctio
   }
 }
 
+/**
+ * Retrieves a message for this campaign
+ * @param req 
+ * @param res 
+ * @param next 
+ */
 const previewFirstMessage = async (req: Request, res: Response, next: NextFunction): Promise<Response | void> => {
-  try{
+  try {
     const { campaignId } = req.params
     return res.json({
       preview: await EmailService.getHydratedMessage(+campaignId),
     })
-  } catch(err){
+  } catch (err){
     return next(err)
   }
 }
