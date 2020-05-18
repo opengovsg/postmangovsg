5691143fafb17d989814150a7f92931e26054d33
Jean <33819199+jeantanzj@users.noreply.github.com>
2020-05-19 03:46:09 +0800

    Revert "refactor: configure template client with function for validating recipient"
    This reverts commit c24b0f276a2b452024bb0de9f34bc022f4232013.

--- backend/src/email/middlewares/email-template.middleware.ts
+++ backend/src/email/middlewares/email-template.middleware.ts
@@ -109,7 +109,9 @@ const uploadCompleteHandler = async (req: Request, res: Response, next: NextFunc
         templateBody: emailTemplate.body as string,
         templateParams: emailTemplate.params as string[],
       })
-        
+  
+      if (EmailTemplateService.hasInvalidEmailRecipient(records)) throw new InvalidRecipientError()
+      
       const recipientCount: number = records.length
        
       // START populate template
