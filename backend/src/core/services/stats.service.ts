24a691b9476c9a976e8f3cb4277a34367a256adf
Ian Chen <ian-@gmx.com>
2020-05-19 22:31:04 +0800

    fix: merge stats from archive and ops table, fixed paused progress bar

--- backend/src/core/services/stats.service.ts
+++ backend/src/core/services/stats.service.ts
@@ -1,36 +1,19 @@
 import { fn, col, cast } from 'sequelize'
-import { Statistic } from '@core/models'
-
-/**
- * Helper method to get the number of errored messages, sent messages, and messages that remain unsent from ops table. 
- * @param model 
- * @param campaignId 
- */
-const getStatsFromTable = async (model: any, campaignId: number): Promise<{ error: number; unsent: number; sent: number }> => {
-  const [data] = await model.findAll({
-    raw: true,
-    /* eslint-disable-next-line @typescript-eslint/camelcase */
-    where: { campaign_id: campaignId },
-    attributes:
-      [
-        [fn('count', col('error_code')), 'error'],
-        [fn('count', col('message_id')), 'sent'],
-        [fn('sum', cast({ 'sent_at': null }, 'int')), 'unsent'],
-      ],
-  })
-  const res = { error: data.error, sent: data.sent, unsent: data.unsent }
-  return res
-}
+import { Statistic, JobQueue } from '@core/models'
+import { CampaignStats } from '@core/interfaces'
 
 /**
  * Helper method to get precomputed number of errored , sent, and unsent from statistic table. 
- * @param model 
  * @param campaignId 
  */
 const getStatsFromArchive = async (campaignId: number): Promise<{ error: number; unsent: number; sent: number }> => {
   const stats = await Statistic.findByPk(campaignId)
   if (!stats) {
-    throw new Error('No stats found for project')
+    return {
+      error: 0,
+      sent: 0,
+      unsent: 0,
+    }
   }
   return {
     error: stats?.errored,
@@ -39,12 +22,67 @@ const getStatsFromArchive = async (campaignId: number): Promise<{ error: number;
   }
 }
 
+/**
+ * Helper method to get the count of errored messages, sent messages, and messages that remain unsent from ops table. 
+ * @param campaignId 
+ * @param model 
+ */
+const getStatsFromTable = async (campaignId: number, model: any): Promise<{ error: number; unsent: number; sent: number }> => {
+  // Retrieve stats from ops table
+  const [data] = await model.findAll({
+    raw: true,
+    where: { campaignId },
+    attributes:
+      [
+        [fn('count', col('error_code')), 'error'],
+        [fn('count', col('message_id')), 'sent'],
+        [fn('sum', cast({ 'delivered_at': null }, 'int')), 'unsent'],
+      ],
+  })
+  return {
+    error: +data.error,
+    sent: +data.sent,
+    unsent: +data.unsent,
+  }
+}
+
+/**
+ * Helper method to get current count of errored , sent, and unsent depending on job status
+ * @param campaignId 
+ * @param opsTable
+ */
+const getCurrentStats = async (campaignId: number, opsTable: any): Promise<CampaignStats> => {
+  // Get job from job_queue table
+  const job = await JobQueue.findOne({ where: { campaignId } })
+  if (job == null) throw new Error('Unable to find campaign in job queue table.')
+
+  // Get stats from statistics table
+  const archivedStats = await getStatsFromArchive(campaignId)
+
+  // If job is sending, sent or stopped, i.e. not logged back to message table,
+  // Retrieve current stats from ops table
+  if (['SENDING', 'SENT', 'STOPPED'].includes(job.status)) {
+    const opsStats = await getStatsFromTable(opsTable, campaignId)
+
+    // Sent count must be added to archived sent count since sent messages are 
+    // not enqueued to ops table if a project is stopped and resumed
+    return {
+      error: opsStats.error,
+      unsent: opsStats.unsent,
+      sent: opsStats.sent + archivedStats.sent,
+      status: job.status,
+    }
+  }
+  // else, return archived stats
+  return { ...archivedStats, status: job.status }
+}
+
+
 const getTotalSentCount = async (): Promise<number> => {
   return Statistic.sum('sent')
 }
 
 export const StatsService = {
-  getStatsFromTable,
-  getStatsFromArchive,
+  getCurrentStats,
   getTotalSentCount,
 }
--- backend/src/sms/services/sms-stats.service.ts
+++ backend/src/sms/services/sms-stats.service.ts
@@ -1,5 +1,4 @@
 
-import { JobQueue } from '@core/models'
 import { StatsService } from '@core/services'
 import { CampaignStats } from '@core/interfaces'
 
@@ -10,19 +9,8 @@ import { SmsOp } from '@sms/models'
  * @param campaignId 
  */
 const getStats = async (campaignId: number): Promise<CampaignStats> => {
-  const job = await JobQueue.findOne({ where: { campaignId } })
-  if (job === null) throw new Error('Unable to find campaign in job queue table.')
-
-  // Gets from email ops table if status is SENDING or SENT
-  if (job.status === 'SENDING' || job.status === 'SENT') {
-    const stats = await StatsService.getStatsFromTable(SmsOp, campaignId)
-    return { error: stats.error, unsent: stats.unsent, sent: stats.sent, status: job.status }
-  }
-
-  const stats = await StatsService.getStatsFromArchive(campaignId)
-  return { error: stats.error, unsent: stats.unsent, sent: stats.sent, status: job.status }
-}
-
+  return StatsService.getCurrentStats(campaignId, SmsOp)
+} 
 
 export const SmsStatsService = {
   getStats,
