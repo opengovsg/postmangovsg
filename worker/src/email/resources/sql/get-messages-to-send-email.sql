15ee30879fba231c4e7463f4eff9d30b0e1a9e13
Ian Chen <ian-@gmx.com>
2020-05-19 17:49:18 +0800

    fix: sql updating and returning more rows than limit

--- worker/src/email/resources/sql/get-messages-to-send-email.sql
+++ worker/src/email/resources/sql/get-messages-to-send-email.sql
@@ -4,19 +4,22 @@ LANGUAGE plpgsql AS $$
 BEGIN
 	RETURN QUERY
 	-- Set sent_at for messages belonging to a job that is in SENDING state only (it will not pick up any other states like STOPPED or LOGGED)
-	WITH messages AS (
-		UPDATE email_ops SET sent_at=clock_timestamp() WHERE 
-		id in (
-			SELECT id FROM email_ops WHERE
+	WITH
+    selected_ids AS (
+      SELECT id FROM email_ops WHERE
 			campaign_id = (SELECT q.campaign_id FROM job_queue q WHERE q.id = jid AND status = 'SENDING')
 			AND sent_at IS NULL
 			LIMIT lim
 			FOR UPDATE SKIP LOCKED
-		)
-		RETURNING id, recipient, params, campaign_id
-	) SELECT json_build_object('id', m.id, 'recipient', m.recipient, 'params', m.params, 'body', t.body, 'subject', t.subject, 'replyTo', t.reply_to)
-	 FROM messages m, email_templates t
-	 WHERE m.campaign_id = t.campaign_id;
+    ),
+    messages AS (
+      UPDATE email_ops SET sent_at=clock_timestamp() WHERE 
+      id in (SELECT * from selected_ids)
+      RETURNING id, recipient, params, campaign_id
+    )
+    SELECT json_build_object('id', m.id, 'recipient', m.recipient, 'params', m.params, 'body', t.body, 'subject', t.subject, 'replyTo', t.reply_to)
+    FROM messages m, email_templates t
+    WHERE m.campaign_id = t.campaign_id;
 		
 	-- If there are no messages found, we assume the job is done
 	-- This is only correct because enqueue and send are serialized. All messages are enqueued before sending occurs
