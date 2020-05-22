9df2c86a7d4f188aed2a9b20bb4d4a04dfe246b8
LoneRifle <alwyn.tan@gmail.com>
2020-05-22 04:33:14 +0000

    refactor(backend): allow aws endpoint override
    To allow dev work to be self-contained, we need to remove the hard
    dependency on hosted services like AWS at runtime. The first step to
    this requires us to accept a parameter - an explicit RESTful endpoint -
    that will be injected into Postman's AWS JavaScript clients, overriding
    `AWS_REGION` in the process.
    
    Doing so will allow developers to get Postman to connect to a specified
    mock AWS service when running on their own machines, and would also
    help with more invasive testing. This would also allow for external devs
    to contribute, since they no longer need to have an AWS account to work
    on Postman.
    
    Note that this does not allow overrides for workers, as frameworks that
    mock AWS services (eg localstack) do not have a freely available ECS API
    
    Prep work for #284
    
    - Add a new parameter in config - `aws.awsEndpoint`, env `AWS_ENDPOINT`
    - Create a new utility function - `configureEndpoint()` - that handles
      selection of either endpoint or region for AWS config purposes
    - Make use of `configureEndpoint()` in cloudwatch, S3 and SecretsManager
      - change the way we configure WinstonCloudwatch, by specifying
        `awsOptions` instead of `awsRegion`

---
+++ backend/src/core/utils/aws-endpoint.ts
@@ -0,0 +1,19 @@
+import convict from 'convict'
+
+interface AWSEndpointConfig { aws: { awsEndpoint: any, awsRegion: any } }
+
+/**
+ * Provide an Object containing either the endpoint to send AWS requests to,
+ * or if that is absent, the region from which to generate the endpoint. 
+ * This Object is to be injected into AWS JavaScript clients.
+ * 
+ * @param config - a convict object whose schema includes AWSEndpointConfig
+ * @return an object containing either the endpoint or region field
+ */
+const configureEndpoint = <T extends AWSEndpointConfig>(config: convict.Config<T>) => {
+  const endpoint = config.get('aws.awsEndpoint')
+  const region = config.get('aws.awsRegion')
+  return endpoint ? { endpoint } : { region }
+}
+
+export { configureEndpoint } 
