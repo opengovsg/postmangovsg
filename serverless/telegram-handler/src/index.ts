f2f93086d3270e32a71a512cf150ed0386f1c37a
Daryl Chan <dckj95@gmail.com>
2020-06-03 10:04:35 +0800

    chore: scaffold telegram handler

---
+++ serverless/telegram-handler/src/index.ts
@@ -0,0 +1,13 @@
+const handler = async (event: any): Promise<void> => {
+  console.log(event)
+
+  // TODO: Extract bot_id
+
+  // TODO: Attempt to find a match in stored credentials
+
+  // TODO: Instantiate Telegraf bot and attach handlers
+
+  return
+}
+
+export { handler }
