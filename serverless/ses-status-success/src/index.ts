6756bfb4f3d644a024418afd15c2b40da5e9114b
Shao Wei <lshaowei18@gmail.com>
2020-06-04 10:33:01 +0800

    chore: Connect to DB

--- serverless/ses-status-success/src/index.ts
+++ serverless/ses-status-success/src/index.ts
@@ -1,12 +1,23 @@
+import sequelizeLoader from './sequelize-loader'
+import { QueryTypes } from 'sequelize'
+
 exports.handler = async (event: any) => {
   try {
+    const sequelize = await sequelizeLoader()
+
     const message = JSON.parse(event.Records[0].Sns.Message)
 
     const messageId = message?.mail?.commonHeaders?.messageId
     console.log(messageId)
 
-    const timestamp = message?.delivery?.timestamp
-    console.log(timestamp)
+    const timeStamp = message?.delivery?.timestamp
+    console.log(timeStamp)
+
+    await sequelize.query(
+      `UPDATE email_messages SET received_at=timeStamp, updated_at = clock_timestamp() WHERE id=messageId`,
+      {
+        replacements: { timeStamp, messageId }, type: QueryTypes.UPDATE,
+      })
     return {
       statusCode: 200,
       body: 'Ok'
