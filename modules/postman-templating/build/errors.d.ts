1de4b51521c33d13210301e8aba0bb04d04889c5
Shao Wei <lshaowei18@gmail.com>
2020-06-08 09:43:38 +0800

    chore: Add type files and build package

---
+++ modules/postman-templating/build/errors.d.ts
@@ -0,0 +1,9 @@
+export declare class TemplateError extends Error {
+    constructor(message: string);
+}
+export declare class RecipientColumnMissing extends Error {
+    constructor();
+}
+export declare class UnexpectedDoubleQuoteError extends Error {
+    constructor();
+}
