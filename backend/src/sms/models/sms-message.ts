d00ba68e745180fb61a959f1a5e0d4ee78045b3d
Jean <33819199+jeantanzj@users.noreply.github.com>
2020-06-12 21:34:38 +0800

    feat: add status to sms_message

--- backend/src/sms/models/sms-message.ts
+++ backend/src/sms/models/sms-message.ts
@@ -7,6 +7,7 @@ import {
   BelongsTo,
 } from 'sequelize-typescript'
 import { Campaign } from '@core/models/campaign'
+import { MessageStatus } from '@core/constants'
 
 @Table({ tableName: 'sms_messages', underscored: true, timestamps: true })
 export class SmsMessage extends Model<SmsMessage> {
@@ -35,6 +36,12 @@ export class SmsMessage extends Model<SmsMessage> {
 
   @Column(DataType.STRING)
   errorCode?: string
+
+  @Column({
+    type: DataType.ENUM(...Object.values(MessageStatus)),
+    allowNull: true,
+  })
+  status?: MessageStatus
 
   @Column(DataType.DATE)
   dequeuedAt?: Date
