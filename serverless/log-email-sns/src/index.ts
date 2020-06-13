5f001abe925f4ceb343263b5bd97d7c405186c9a
Jean <33819199+jeantanzj@users.noreply.github.com>
2020-06-13 22:42:22 +0800

    feat: handle complaint notifications

--- serverless/log-email-sns/src/index.ts
+++ serverless/log-email-sns/src/index.ts
@@ -3,15 +3,46 @@ import { QueryTypes } from 'sequelize'
 import { Sequelize } from 'sequelize-typescript'
 let sequelize : Sequelize | null = null // Define the sequelize connection outside so that a warm lambda can reuse the connection
 
+
+/**
+ * Adds email to blacklist table if it does not exist
+ * @param recipientEmail 
+ */
+const addToBlacklist = (recipientEmail: string) => {
+  console.log(`Updating blacklist table with ${recipientEmail}`)
+  return sequelize?.query(
+    `INSERT INTO email_blacklist (recipient, created_at, updated_at) VALUES (:recipientEmail, clock_timestamp(), clock_timestamp()) 
+    ON CONFLICT DO NOTHING;`,
+    {
+      replacements: { recipientEmail }, type: QueryTypes.INSERT 
+    })
+}
+
+/**
+ * Updates email_messages with an error and error code 
+ *
+ */
+const updateMessageWithError = ({ errorCode, timestamp, messageId }: { errorCode: string, timestamp: string, messageId: string }) => {
+  console.log('Updating email_messages table.')
+  return sequelize?.query(
+    `UPDATE email_messages SET error_code=:errorCode, received_at=:timestamp, status='INVALID_RECIPIENT', updated_at = clock_timestamp()
+    WHERE message_id=:messageId 
+    AND (received_at IS NULL OR received_at < :timestamp);`,
+    {
+      replacements: { errorCode, timestamp, messageId }, type: QueryTypes.UPDATE 
+    })
+}
+
 /**
  *  Updates the email_messages table for successful delivery of an email.
  *  Delivery: Amazon SES successfully delivered the email to the recipient's mail server.
  *  @param message JSON object that contains the notification details
- *  @param dbConnection Sequelize connection
+ *  @param timestamp ISO string from notification timestamp
  */
 const updateSuccessfulDelivery = async (message: any, timestamp: string) => {
   const messageId = message?.mail?.commonHeaders?.messageId
 
+  console.log('Updating email_messages table.')
   // Since notifications for the same messageId can be interleaved, we only update that message if this notification is newer than the previous. 
   await sequelize?.query(
     `UPDATE email_messages SET received_at=:timestamp, updated_at = clock_timestamp(), status='SUCCESS' 
@@ -27,39 +58,36 @@ const updateSuccessfulDelivery = async (message: any, timestamp: string) => {
  *  Hard bounce: The recipient's mail server permanently rejected the email.
  *  Soft bounce: SES fails to deliver the email after retrying for a period of time.
  *  @param message JSON object that contains the notification details
- *  @param dbConnection Sequelize connection
+ *  @param timestamp ISO string from notification timestamp
  */
 const updateBouncedStatus = async (message: any, timestamp: string) => {
   const bounceType = message?.bounce?.bounceType
   const messageId = message?.mail?.commonHeaders?.messageId
-  const recipientEmail = message?.mail?.commonHeaders?.to[0]
-
   let errorCode
  
   if (bounceType === 'Permanent') {
     errorCode = "Hard bounce, the recipient's mail server permanently rejected the email."
-
-    console.log('Updating blacklist table.')
     // Add to black list
-    await sequelize?.query(
-      `INSERT INTO email_blacklist (recipient, created_at, updated_at) VALUES (:recipientEmail, clock_timestamp(), clock_timestamp()) 
-      ON CONFLICT DO NOTHING;`,
-      {
-        replacements: { recipientEmail }, type: QueryTypes.INSERT 
-      })
+    await Promise.all(message?.mail?.commonHeaders?.to.map(addToBlacklist))
+   
   }
   else {
     errorCode = "Soft bounce, Amazon SES fails to deliver the email after retrying for a period of time."
   }
   
-  console.log('Updating email_messages table.')
-  await sequelize?.query(
-    `UPDATE email_messages SET error_code=:errorCode, received_at=:timestamp, status='INVALID_RECIPIENT', updated_at = clock_timestamp()
-    WHERE message_id=:messageId 
-    AND (received_at IS NULL OR received_at < :timestamp);`,
-    {
-      replacements: { errorCode, timestamp, messageId }, type: QueryTypes.UPDATE 
-    })
+  await updateMessageWithError({ errorCode, timestamp, messageId })
+}
+
+/**
+ * Updates the error_code in the email_messages table for bounced delivery.
+ *  @param message JSON object that contains the notification details
+ *  @param timestamp ISO string from notification timestamp
+ */
+const updateComplaintStatus = async (message: any, timestamp: string) => {
+  const messageId = message?.mail?.commonHeaders?.messageId
+  const errorCode = message?.complaint?.complaintFeedbackType
+  await Promise.all(message?.mail?.commonHeaders?.to.map(addToBlacklist))
+  await updateMessageWithError({ errorCode, timestamp, messageId }) 
 }
 
 const handleMessage = async (record: any) => {
@@ -77,11 +105,14 @@ const handleMessage = async (record: any) => {
   console.log(`Updating messageId ${messageId} in respective tables, notificationType = ${notificationType}`)
   
   switch (notificationType) {
-    case "Delivery": 
+    case 'Delivery': 
       await updateSuccessfulDelivery(message, timestamp)
       break
-    case "Bounce":
+    case 'Bounce':
       await updateBouncedStatus(message, timestamp)
+      break
+    case 'Complaint':
+      await updateComplaintStatus(message, timestamp)
       break
     default:
       console.error(`Can't handle messages with this notification type. notificationType = ${notificationType}`)
@@ -99,7 +130,7 @@ exports.handler = async (event: any) => {
     if(sequelize === null) {
       sequelize = await sequelizeLoader()
     }
-    
+
     await Promise.all(event.Records.map(handleMessage))
     
     return {
