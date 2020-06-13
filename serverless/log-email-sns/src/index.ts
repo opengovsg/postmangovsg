0baf4c4d2170a2d37d4b9e5a3b6f4272ebb0d2a7
Jean <33819199+jeantanzj@users.noreply.github.com>
2020-06-13 22:27:29 +0800

    feat: add to blacklist and update based on timestamp

--- serverless/log-email-sns/src/index.ts
+++ serverless/log-email-sns/src/index.ts
@@ -1,6 +1,94 @@
 import sequelizeLoader from './sequelize-loader'
 import { QueryTypes } from 'sequelize'
 import { Sequelize } from 'sequelize-typescript'
+let sequelize : Sequelize | null = null // Define the sequelize connection outside so that a warm lambda can reuse the connection
+
+/**
+ *  Updates the email_messages table for successful delivery of an email.
+ *  Delivery: Amazon SES successfully delivered the email to the recipient's mail server.
+ *  @param message JSON object that contains the notification details
+ *  @param dbConnection Sequelize connection
+ */
+const updateSuccessfulDelivery = async (message: any, timestamp: string) => {
+  const messageId = message?.mail?.commonHeaders?.messageId
+
+  // Since notifications for the same messageId can be interleaved, we only update that message if this notification is newer than the previous. 
+  await sequelize?.query(
+    `UPDATE email_messages SET received_at=:timestamp, updated_at = clock_timestamp(), status='SUCCESS' 
+    WHERE message_id=:messageId 
+    AND (received_at IS NULL OR received_at < :timestamp);`,
+    {
+      replacements: { timestamp, messageId }, type: QueryTypes.UPDATE 
+    })
+}
+
+/**
+ *  Updates the error_code in the email_messages table for bounced delivery.
+ *  Hard bounce: The recipient's mail server permanently rejected the email.
+ *  Soft bounce: SES fails to deliver the email after retrying for a period of time.
+ *  @param message JSON object that contains the notification details
+ *  @param dbConnection Sequelize connection
+ */
+const updateBouncedStatus = async (message: any, timestamp: string) => {
+  const bounceType = message?.bounce?.bounceType
+  const messageId = message?.mail?.commonHeaders?.messageId
+  const recipientEmail = message?.mail?.commonHeaders?.to[0]
+
+  let errorCode
+ 
+  if (bounceType === 'Permanent') {
+    errorCode = "Hard bounce, the recipient's mail server permanently rejected the email."
+
+    console.log('Updating blacklist table.')
+    // Add to black list
+    await sequelize?.query(
+      `INSERT INTO email_blacklist (recipient, created_at, updated_at) VALUES (:recipientEmail, clock_timestamp(), clock_timestamp()) 
+      ON CONFLICT DO NOTHING;`,
+      {
+        replacements: { recipientEmail }, type: QueryTypes.INSERT 
+      })
+  }
+  else {
+    errorCode = "Soft bounce, Amazon SES fails to deliver the email after retrying for a period of time."
+  }
+  
+  console.log('Updating email_messages table.')
+  await sequelize?.query(
+    `UPDATE email_messages SET error_code=:errorCode, received_at=:timestamp, status='INVALID_RECIPIENT', updated_at = clock_timestamp()
+    WHERE message_id=:messageId 
+    AND (received_at IS NULL OR received_at < :timestamp);`,
+    {
+      replacements: { errorCode, timestamp, messageId }, type: QueryTypes.UPDATE 
+    })
+}
+
+const handleMessage = async (record: any) => {
+  const message = JSON.parse(record.Sns.Message)
+  const notificationType = message?.notificationType
+
+  const timestamp = record.Sns.Timestamp
+  const messageId = message?.mail?.commonHeaders?.messageId
+  
+  if(messageId === undefined){
+    console.error(`No messageId found for: ${JSON.stringify(message)}`)
+    return
+  }
+
+  console.log(`Updating messageId ${messageId} in respective tables, notificationType = ${notificationType}`)
+  
+  switch (notificationType) {
+    case "Delivery": 
+      await updateSuccessfulDelivery(message, timestamp)
+      break
+    case "Bounce":
+      await updateBouncedStatus(message, timestamp)
+      break
+    default:
+      console.error(`Can't handle messages with this notification type. notificationType = ${notificationType}`)
+      return
+  }    
+}
+
 /**
  *  Lambda to update the email delivery status.
  *  SNS triggers it whenever it receives a new notification from SES.
@@ -8,26 +96,11 @@ import { Sequelize } from 'sequelize-typescript'
  */
 exports.handler = async (event: any) => {
   try {
-    const sequelize = await sequelizeLoader()
-
-    const message = JSON.parse(event.Records[0].Sns.Message)
-
-    const notificationType = message?.notificationType
-
-    const messageId = message?.mail?.commonHeaders?.messageId
-
-    console.log(`Updating messageId ${messageId} in respective tables, notificationType = ${notificationType}`)
-
-    switch (notificationType) {
-      case "Delivery": 
-        await updateSuccessfulDelivery(message, sequelize)
-        break
-      case "Bounce":
-        await updateBouncedStatus(message, sequelize)
-        break
-      default:
-        throw new Error(`Can't handle messages with this notification type. notificationType = ${notificationType}`)
-    }    
+    if(sequelize === null) {
+      sequelize = await sequelizeLoader()
+    }
+    
+    await Promise.all(event.Records.map(handleMessage))
     
     return {
       statusCode: 200,
@@ -42,59 +115,4 @@ exports.handler = async (event: any) => {
       statusCode: 500
     }
   }
-}
-
-/**
- *  Updates the email_messages table for successful delivery of an email.
- *  Delivery: Amazon SES successfully delivered the email to the recipient's mail server.
- *  @param message JSON object that contains the notification details
- *  @param dbConnection Sequelize connection
- */
-const updateSuccessfulDelivery = async (message: any, dbConnection: Sequelize ) => {
-  const messageId = message?.mail?.commonHeaders?.messageId
-  const timeStamp = message?.delivery?.timestamp
-
-  await dbConnection.query(
-    `UPDATE email_messages SET received_at=:timeStamp, updated_at = clock_timestamp(), status='SUCCESS' WHERE message_id=:messageId`,
-    {
-      replacements: { timeStamp, messageId }, type: QueryTypes.UPDATE 
-    })
-}
-
-/**
- *  Updates the error_code in the email_messages table for bounced delivery.
- *  Hard bounce: The recipient's mail server permanently rejected the email.
- *  Soft bounce: SES fails to deliver the email after retrying for a period of time.
- *  @param message JSON object that contains the notification details
- *  @param dbConnection Sequelize connection
- */
-const updateBouncedStatus = async (message: any, dbConnection: Sequelize) => {
-  const bounceType = message?.bounce?.bounceType
-  const messageId = message?.mail?.commonHeaders?.messageId
-  const timeStamp = message?.delivery?.timestamp
-  const recipientEmail = message?.mail?.commonHeaders?.to[0]
-
-  let errorCode
- 
-  if (bounceType === 'Permanent') {
-    errorCode = "Hard bounce, the recipient's mail server permanently rejected the email."
-
-    console.log('Updating blacklist table.')
-    // Add to black list
-    await dbConnection.query(
-      `UPDATE email_blacklist SET recipient=:recipientEmail, updated_at=clock_timestamp() WHERE message_id=:messageId`,
-      {
-        replacements: { recipientEmail, messageId }, type: QueryTypes.UPDATE 
-      })
-  }
-  else {
-    errorCode = "Soft bounce, Amazon SES fails to deliver the email after retrying for a period of time."
-  }
-  
-  console.log('Updating email_messages table.')
-  await dbConnection.query(
-    `UPDATE email_messages SET error_code=:errorCode, received_at=:timeStamp, status='INVALID_RECIPIENT', updated_at = clock_timestamp() WHERE message_id=:messageId`,
-    {
-      replacements: { errorCode, timeStamp, messageId }, type: QueryTypes.UPDATE 
-    })
 }
