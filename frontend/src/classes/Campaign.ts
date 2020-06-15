73f663dd6f8581dfa18d4951c4bf38c6e70b1110
Lam Kee Wei <keewei.lam@gmail.com>
2020-06-15 11:09:10 +0800

    feat: Add Telegram to ChannelType enum and channel icons

--- frontend/src/classes/Campaign.ts
+++ frontend/src/classes/Campaign.ts
@@ -1,6 +1,7 @@
 export enum ChannelType {
   SMS = 'SMS',
   Email = 'EMAIL',
+  Telegram = 'TELEGRAM',
 }
 
 export enum Status {
@@ -12,6 +13,7 @@ export enum Status {
 export const channelIcons = {
   [ChannelType.SMS]: 'bx-message-detail',
   [ChannelType.Email]: 'bx-envelope-open',
+  [ChannelType.Telegram]: 'bxl-telegram',
 }
 
 export class Campaign {
