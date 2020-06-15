92b383acbaa161ddf1980cf72c26533c4bb29fd0
Shao Wei <lshaowei18@gmail.com>
2020-06-15 09:26:59 +0800

    chore: clean up template client class

--- backend/src/core/services/template-client.class.ts
+++ backend/src/core/services/template-client.class.ts
@@ -1,14 +1,9 @@
-import { difference, keys, mapKeys } from 'lodash'
+import { mapKeys } from 'lodash'
 import * as Sqrl from 'squirrelly'
 import { AstObject, TemplateObject } from 'squirrelly/dist/types/parse'
 
-import S3Client from '@core/services/s3-client.class'
 import logger from '@core/logger'
-import {
-  MissingTemplateKeysError,
-  TemplateError,
-} from '@core/errors/template.errors'
-import { isSuperSet } from '@core/utils'
+import { TemplateError } from '@core/errors/template.errors'
 
 import xss from 'xss'
 
@@ -160,71 +155,5 @@ export default class TemplateClient {
     // Remove extra '\' infront of single quotes and backslashes, added by Squirrelly when it escaped the csv
     const templated = parsed.tokens.join('').replace(/\\([\\'])/g, '$1')
     return xss.filterXSS(templated, this.xssOptions)
-  }
-
-  /**
-   * Ensures that the csv contains all the columns necessary to replace the attributes in the template
-   * @param csvRecord
-   * @param templateParams
-   */
-  private checkTemplateKeysMatch(
-    csvRecord: { [key: string]: string },
-    templateParams: Array<string>
-  ): void {
-    // if body exists, smsTemplate.params should also exist
-    if (!isSuperSet(keys(csvRecord), templateParams)) {
-      const missingKeys = difference(templateParams, keys(csvRecord))
-      throw new MissingTemplateKeysError(missingKeys)
-    }
-  }
-
-  /**
-   * Ensures that the csv contains all the columns necessary to replace the attributes in the template.
-   * Returns a message formed from the template and parameters specified in the csv.
-   * @param param0
-   */
-  async testHydration({
-    campaignId,
-    s3Key,
-    templateSubject,
-    templateBody,
-    templateParams,
-  }: {
-    campaignId: number
-    s3Key: string
-    templateSubject?: string
-    templateBody: string
-    templateParams: Array<string>
-  }): Promise<TestHydrationResult> {
-    const s3Client = new S3Client()
-    const downloadStream = s3Client.download(s3Key)
-    const fileContents = await s3Client.parseCsv(downloadStream)
-
-    const records: Array<MessageBulkInsertInterface> = fileContents.map(
-      (entry) => {
-        return {
-          campaignId,
-          recipient: entry['recipient'],
-          params: entry,
-        }
-      }
-    )
-
-    // attempt to hydrate
-    const firstRecord = fileContents[0]
-    this.checkTemplateKeysMatch(firstRecord, templateParams)
-
-    const hydratedRecord = {
-      body: this.template(templateBody, records[0].params),
-    } as { body: string; subject?: string }
-
-    if (templateSubject) {
-      hydratedRecord.subject = this.template(templateSubject, records[0].params)
-    }
-
-    return {
-      records,
-      hydratedRecord,
-    }
   }
 }
