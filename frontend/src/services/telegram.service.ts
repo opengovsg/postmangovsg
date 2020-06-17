43a5e7cd2c67c38081eaa58ce4cd2ffccc1bcb7d
Lam Kee Wei <keewei.lam@gmail.com>
2020-06-17 11:33:14 +0800

    feat: Add service methods for Telegram template and recipients

--- frontend/src/services/telegram.service.ts
+++ frontend/src/services/telegram.service.ts
@@ -1,4 +1,40 @@
 import axios, { AxiosError } from 'axios'
+import Papa from 'papaparse'
+
+interface PresignedUrlResponse {
+  presignedUrl: string
+  transactionId: string
+}
+
+interface UploadCompleteResponse {
+  template_body: string
+  num_recipients: number
+  hydrated_record: string
+}
+
+export async function saveTemplate(
+  campaignId: number,
+  body: string
+): Promise<{
+  numRecipients: number
+  updatedTemplate?: { body: string; params: Array<string> }
+}> {
+  try {
+    const response = await axios.put(
+      `/campaign/${campaignId}/telegram/template`,
+      {
+        body,
+      }
+    )
+    const {
+      num_recipients: numRecipients,
+      template: updatedTemplate,
+    } = response.data
+    return { numRecipients, updatedTemplate }
+  } catch (e) {
+    errorHandler(e, 'Error saving template.')
+  }
+}
 
 export async function storeCredentials({
   label,
@@ -31,6 +67,100 @@ export async function verifyUserCredentials({
     })
   } catch (e) {
     errorHandler(e, 'Error verifying credentials.')
+  }
+}
+
+export async function completeFileUpload({
+  campaignId,
+  transactionId,
+  filename,
+}: {
+  campaignId: number
+  transactionId: string
+  filename: string
+}): Promise<UploadCompleteResponse> {
+  try {
+    const response = await axios.post(
+      `/campaign/${campaignId}/telegram/upload/complete`,
+      {
+        transaction_id: transactionId,
+        filename,
+      }
+    )
+    return response.data
+  } catch (e) {
+    errorHandler(e, 'Error completing file upload')
+  }
+}
+
+export async function getPreviewMessage(
+  campaignId: number
+): Promise<{ body: string }> {
+  try {
+    const response = await axios.get(`/campaign/${campaignId}/telegram/preview`)
+    return response.data?.preview
+  } catch (e) {
+    errorHandler(e, 'Unable to get preview message')
+  }
+}
+
+export async function getPresignedUrl({
+  campaignId,
+  uploadedFile,
+}: {
+  campaignId: number
+  uploadedFile: File
+}): Promise<PresignedUrlResponse> {
+  let mimeType = uploadedFile.type
+  if (mimeType === '') {
+    const isValidCsv = await new Promise((resolve) => {
+      Papa.parse(uploadedFile, {
+        header: true,
+        delimiter: ',',
+        step: function (_, parser: Papa.Parser) {
+          // Checks first row only
+          parser.pause()
+          parser.abort()
+        },
+        complete: function (results) {
+          // results.data will contain 1 row of results because we aborted on the first step
+          const { delimiter, fields } = results.meta
+          resolve(
+            delimiter === ',' &&
+              // papaparse parses everything, including images, pdfs... This checks that at least one of the columns is sane
+              fields.some((field) => /^[a-zA-Z0-9\s-_'"/]+$/.test(field))
+          )
+        },
+        error: function () {
+          resolve(false)
+        },
+      })
+    })
+    if (isValidCsv) {
+      mimeType = 'text/csv'
+    } else {
+      throw new Error(
+        'Please make sure you are uploading a file in CSV format.'
+      )
+    }
+  }
+
+  try {
+    const response = await axios.get(
+      `/campaign/${campaignId}/telegram/upload/start`,
+      {
+        params: {
+          mime_type: mimeType,
+        },
+      }
+    )
+    const {
+      transaction_id: transactionId,
+      presigned_url: presignedUrl,
+    } = response.data
+    return { transactionId, presignedUrl } as PresignedUrlResponse
+  } catch (e) {
+    errorHandler(e, 'Error completing file upload')
   }
 }
 
