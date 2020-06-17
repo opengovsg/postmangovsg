6b3e1a10a734f81d2e106c9d64efdc463e14b4a8
Lam Kee Wei <keewei.lam@gmail.com>
2020-06-17 11:33:54 +0800

    feat: Add create, template and recipient stages for Telegram frontend

---
+++ frontend/src/components/dashboard/create/telegram/TelegramCreate.tsx
@@ -0,0 +1,76 @@
+import React, { useState } from 'react'
+import { cloneDeep } from 'lodash'
+
+import { TelegramCampaign, TelegramProgress, Status } from 'classes'
+import { ProgressPane } from 'components/common'
+import TelegramTemplate from './TelegramTemplate'
+import TelegramRecipients from './TelegramRecipients'
+
+import styles from '../Create.module.scss'
+
+const TELEGRAM_PROGRESS_STEPS = [
+  'Create Template',
+  'Upload Recipients',
+  'Insert Credentials',
+  'Send',
+]
+
+const CreateTelegram = ({
+  campaign: initialCampaign,
+}: {
+  campaign: TelegramCampaign
+}) => {
+  const [activeStep, setActiveStep] = useState(initialCampaign.progress)
+  const [campaign, setCampaign] = useState(initialCampaign)
+
+  // Modifies campaign object in state and navigates to next step
+  const onNext = (changes: any, next = true) => {
+    const updatedCampaign = Object.assign(
+      cloneDeep(campaign),
+      changes
+    ) as TelegramCampaign
+    updatedCampaign.setProgress()
+    setCampaign(updatedCampaign)
+    if (next) {
+      setActiveStep(activeStep + 1)
+    }
+  }
+
+  function renderStep() {
+    switch (activeStep) {
+      case TelegramProgress.CreateTemplate:
+        return <TelegramTemplate body={campaign.body} onNext={onNext} />
+      case TelegramProgress.UploadRecipients:
+        return (
+          <TelegramRecipients
+            params={campaign.params}
+            csvFilename={campaign.csvFilename}
+            numRecipients={campaign.numRecipients}
+            onNext={onNext}
+          />
+        )
+      default:
+        return <p>Invalid step</p>
+    }
+  }
+
+  return (
+    <div className={styles.createContainer}>
+      {campaign.status !== Status.Draft ? (
+        <div className={styles.stepContainer}></div>
+      ) : (
+        <>
+          <ProgressPane
+            steps={TELEGRAM_PROGRESS_STEPS}
+            activeStep={activeStep}
+            setActiveStep={setActiveStep}
+            progress={campaign.progress}
+          />
+          <div className={styles.stepContainer}>{renderStep()}</div>
+        </>
+      )}
+    </div>
+  )
+}
+
+export default CreateTelegram
---
+++ frontend/src/components/dashboard/create/telegram/TelegramRecipients.tsx
@@ -0,0 +1,168 @@
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+
+import {
+  FileInput,
+  InfoBlock,
+  ErrorBlock,
+  PreviewBlock,
+  PrimaryButton,
+  SampleCsv,
+} from 'components/common'
+import {
+  getPresignedUrl,
+  completeFileUpload,
+  getPreviewMessage,
+} from 'services/telegram.service'
+import { uploadFileWithPresignedUrl } from 'services/upload.service'
+import { sendTiming, sendException } from 'services/ga.service'
+
+import styles from '../Create.module.scss'
+
+const TelegramRecipients = ({
+  csvFilename: initialCsvFilename,
+  numRecipients: initialNumRecipients,
+  params,
+  onNext,
+}: {
+  csvFilename: string
+  numRecipients: number
+  params: Array<string>
+  onNext: (changes: any, next?: boolean) => void
+}) => {
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [csvFilename, setUploadedCsvFilename] = useState(initialCsvFilename)
+  const [numRecipients, setNumRecipients] = useState(initialNumRecipients)
+  const [isUploading, setIsUploading] = useState(false)
+  const [preview, setPreview] = useState({} as { body: string })
+
+  const { id: campaignId } = useParams()
+
+  async function loadPreview(campaignId: string) {
+    try {
+      const msgPreview = await getPreviewMessage(+campaignId)
+      if (msgPreview) {
+        setPreview(msgPreview)
+      }
+    } catch (err) {
+      setErrorMessage(err.message)
+    }
+  }
+
+  useEffect(() => {
+    if (!campaignId) return
+    loadPreview(campaignId)
+  }, [campaignId])
+
+  async function uploadFile(files: File[]) {
+    setIsUploading(true)
+    setErrorMessage(null)
+    const uploadTimeStart = performance.now()
+
+    try {
+      // user did not select a file
+      if (!files[0] || !campaignId) {
+        return
+      }
+      const uploadedFile = files[0]
+      // Get presigned url from postman server
+      const startUploadResponse = await getPresignedUrl({
+        campaignId: +campaignId,
+        uploadedFile,
+      })
+      // Upload to presigned url
+      await uploadFileWithPresignedUrl(
+        uploadedFile,
+        startUploadResponse.presignedUrl
+      )
+      const uploadResponse = await completeFileUpload({
+        campaignId: +campaignId,
+        transactionId: startUploadResponse.transactionId,
+        filename: uploadedFile.name,
+      })
+
+      // Set state
+      const uploadTimeEnd = performance.now()
+      sendTiming('Contacts file', 'upload', uploadTimeEnd - uploadTimeStart)
+      setUploadedCsvFilename(uploadedFile.name)
+      setNumRecipients(uploadResponse.num_recipients)
+
+      await loadPreview(campaignId)
+
+      onNext(
+        {
+          csvFilename: uploadedFile.name,
+          numRecipients: uploadResponse.num_recipients,
+        },
+        false
+      )
+    } catch (err) {
+      setErrorMessage(err.message)
+      const exceptionMsg = err.message.includes(
+        'not present in uploaded recipient list'
+      )
+        ? 'Attributes found in template not present in uploaded recipient list.'
+        : err.message
+      sendException(exceptionMsg)
+    } finally {
+      setIsUploading(false)
+    }
+  }
+
+  return (
+    <>
+      <sub>Step 2</sub>
+      <h2>Upload recipient list in CSV format</h2>
+      <p>
+        Only CSV format files are allowed. If you have an Excel file, please
+        convert it by going to File &gt; Save As &gt; CSV (Comma delimited).
+      </p>
+      <p>
+        CSV file must include a <b>recipient</b> column with recipients&apos;
+        mobile numbers
+      </p>
+      {numRecipients > 0 && (
+        <InfoBlock>
+          <li>
+            <i className="bx bx-user-check"></i>
+            <p>{numRecipients} recipients</p>
+          </li>
+          {csvFilename && (
+            <li>
+              <i className="bx bx-file"></i>
+              <p>{csvFilename}</p>
+            </li>
+          )}
+        </InfoBlock>
+      )}
+
+      <div className={styles.uploadActions}>
+        <FileInput isProcessing={isUploading} onFileSelected={uploadFile} />
+        <p>or</p>
+        <SampleCsv params={params} defaultRecipient="88888888" />
+      </div>
+
+      <ErrorBlock>{errorMessage}</ErrorBlock>
+
+      <div className="separator"></div>
+      {preview.body && (
+        <>
+          <p className={styles.greyText}>Message preview</p>
+          <PreviewBlock body={preview.body}></PreviewBlock>
+          <div className="separator"></div>
+        </>
+      )}
+
+      <div className="progress-button">
+        <PrimaryButton
+          disabled={!numRecipients || !csvFilename}
+          onClick={onNext}
+        >
+          Insert credentials →
+        </PrimaryButton>
+      </div>
+    </>
+  )
+}
+
+export default TelegramRecipients
