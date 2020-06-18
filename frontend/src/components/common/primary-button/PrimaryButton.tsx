c6bda7dd39993235cbaa5d4792322e05e3ea889f
Daryl Chan <dckj95@gmail.com>
2020-06-18 17:12:29 +0800

    feat: Disable button when executing async callback

--- frontend/src/components/common/primary-button/PrimaryButton.tsx
+++ frontend/src/components/common/primary-button/PrimaryButton.tsx
@@ -1,10 +1,42 @@
-import React from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import cx from 'classnames'
 
 import styles from './PrimaryButton.module.scss'
 
-const PrimaryButton = (props: any) => {
-  const { className, children, alignRight, ...otherProps } = props
+interface PrimaryButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  alignRight?: boolean
+  onClick?: (...args: any[]) => void | Promise<void>
+}
+
+const PrimaryButton: React.FunctionComponent<PrimaryButtonProps> = ({
+  alignRight,
+  className,
+  disabled,
+  onClick,
+  children,
+  ...otherProps
+}) => {
+  const [asyncDisabled, setAsyncDisabled] = useState(false)
+  const isMounted = useRef(true)
+
+  useEffect(() => {
+    return () => {
+      isMounted.current = false
+    }
+  }, [])
+
+  const asyncOnClick = onClick
+    ? async () => {
+        setAsyncDisabled(true)
+        await Promise.resolve(onClick())
+        // Only enable if button is still mounted
+        if (isMounted.current) {
+          setAsyncDisabled(false)
+        }
+      }
+    : undefined
+
   return (
     <button
       className={cx(
@@ -12,6 +44,8 @@ const PrimaryButton = (props: any) => {
         { [styles.alignRight]: alignRight },
         className
       )}
+      disabled={disabled || asyncDisabled}
+      onClick={asyncOnClick}
       {...otherProps}
     >
       {children}
