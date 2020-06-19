aa6b2d0cc1a16b885a42458fb2e9355470e9b479
Daryl Chan <dckj95@gmail.com>
2020-06-19 12:16:51 +0800

    feat: Disable text field when executing async callback

--- frontend/src/components/common/text-input-with-button/TextInputWithButton.tsx
+++ frontend/src/components/common/text-input-with-button/TextInputWithButton.tsx
@@ -1,56 +1,69 @@
-import React from 'react'
+import React, { useState, useRef, useEffect, MutableRefObject } from 'react'
 import cx from 'classnames'
 import styles from './TextInputWithButton.module.scss'
 import { PrimaryButton, TextInput } from '../'
 
-const TextInputWithButton = (props: {
-  value: string
-  onChange: Function
-  onClick: Function
-  children: React.ReactNode
-  type?: string
+interface TextInputWithButtonProps
+  extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'onChange'> {
+  onChange: (value: string) => void
+  onClick: () => void | Promise<void>
   buttonDisabled?: boolean
   inputDisabled?: boolean
-  placeholder?: string
-  className?: string
-  textRef?: any
-}) => {
-  const {
-    value,
-    onChange,
-    onClick,
-    children,
-    type,
-    buttonDisabled,
-    inputDisabled,
-    placeholder,
-    className,
-    textRef,
-  } = props
+  textRef?: MutableRefObject<HTMLInputElement | undefined>
+  buttonLabel?: React.ReactNode
+  loadingButtonLabel?: React.ReactNode
+}
 
-  function onFormSubmit(e: React.FormEvent) {
-    onClick()
-    // prevents page reload
+const TextInputWithButton: React.FunctionComponent<TextInputWithButtonProps> = ({
+  value,
+  onChange,
+  onClick,
+  type,
+  buttonDisabled,
+  inputDisabled,
+  placeholder,
+  className,
+  textRef,
+  buttonLabel,
+  loadingButtonLabel,
+}) => {
+  const [asyncLoading, setAsyncLoading] = useState(false)
+  const isMounted = useRef(true)
+
+  useEffect(() => {
+    return () => {
+      isMounted.current = false
+    }
+  }, [])
+
+  const asyncSubmit = async (e: React.FormEvent) => {
     e.preventDefault()
+
+    setAsyncLoading(true)
+    await Promise.resolve(onClick())
+    // Only enable if still mounted
+    if (isMounted.current) {
+      setAsyncLoading(false)
+    }
   }
 
   return (
-    <form className={styles.inputWithButton} onSubmit={onFormSubmit}>
+    <form className={styles.inputWithButton} onSubmit={asyncSubmit}>
       <TextInput
         className={styles.textInput}
         value={value}
         onChange={onChange}
         type={type}
-        disabled={inputDisabled}
+        disabled={inputDisabled || asyncLoading}
         placeholder={placeholder}
         ref={textRef}
       />
       <PrimaryButton
         className={cx(styles.inputButton, className)}
-        disabled={buttonDisabled}
+        disabled={buttonDisabled || asyncLoading}
         type="submit"
       >
-        {children}
+        {asyncLoading ? loadingButtonLabel : buttonLabel}
       </PrimaryButton>
     </form>
   )
