79e88942b5d30f58dd3003cbb6dbca714c54b9ee
Lam Kee Wei <keewei.lam@gmail.com>
2020-07-09 11:18:35 +0800

    feat: Add bot token help link in add credential modal

--- frontend/src/components/dashboard/create/telegram/TelegramCredentialsInput.tsx
+++ frontend/src/components/dashboard/create/telegram/TelegramCredentialsInput.tsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react'
+import { OutboundLink } from 'react-ga'
+import { GUIDE_TELEGRAM_CREDENTIALS_URL } from 'config'
 
 import { TextInput } from 'components/common'
+import styles from '../Create.module.scss'
 
 const TelegramCredentialsInput = ({
   onFilled,
@@ -19,7 +22,17 @@ const TelegramCredentialsInput = ({
 
   return (
     <>
-      <h5>Telegram Bot Token</h5>
+      <h5>
+        Telegram Bot Token
+        <OutboundLink
+          className={styles.inputLabelHelpLink}
+          eventLabel={GUIDE_TELEGRAM_CREDENTIALS_URL}
+          to={GUIDE_TELEGRAM_CREDENTIALS_URL}
+          target="_blank"
+        >
+          (What&apos;s this?)
+        </OutboundLink>
+      </h5>
       <TextInput
         placeholder="Enter Telegram Bot Token"
         value={telegramBotToken}
