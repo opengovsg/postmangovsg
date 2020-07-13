2b55247b9732d625502fbf186a752d45aacfca49
jeantanzj <33819199+jeantanzj@users.noreply.github.com>
2020-07-13 18:36:19 +0800

    feat: support Sendgrid callbacks (#496)
    * refactor: simplify interface
    
    * refactor: validate sns signature and create ses parser
    
    * feat: add sendgrid handler
    
    * chore: handle http call from sns
    
    chore: remove environment arg
    
    * chore: worker sends X-SMTPAPI header
    
    * chore: update travis
    
    * chore: add basic auth for http endpoint
    
    * chore: update swagger
    
    * chore: update worker
    
    * chore: amend check for cert url
    
    * chore: tighter check on event signature
    
    * chore: amend travis env var name

--- serverless/log-email-sns/src/index.ts
+++ serverless/log-email-sns/src/index.ts
@@ -1,106 +1,6 @@
-import {
-  init,
-  addToBlacklist,
-  updateMessageWithError,
-  updateMessageWithSuccess,
-  haltCampaignIfThresholdExceeded,
-} from './query'
-import { BounceMetadata, ComplaintMetadata } from './interfaces'
-
-const REFERENCE_ID_HEADER = 'X-Postman-ID' // Case sensitive
-
-/**
- * Parses the message to find the matching email_message id
- * @param message
- */
-const getReferenceId = (message: any): string | undefined => {
-  const headers: Array<{ name: string; value: string }> = message?.mail?.headers
-  const referenceId = headers.find(({ name }) => name === REFERENCE_ID_HEADER)
-    ?.value
-  return referenceId
-}
-
-/**
- *  Updates the error_code in the email_messages table for bounced delivery.
- *  Hard bounce: The recipient's mail server permanently rejected the email.
- *  Soft bounce: SES fails to deliver the email after retrying for a period of time.
- *  @param message JSON object that contains the notification details
- *  @param timestamp ISO string from notification timestamp
- */
-const updateBouncedStatus = async (metadata: BounceMetadata) => {
-  const bounceType = metadata.message?.bounce?.bounceType
-  const recipients = metadata?.message?.mail?.commonHeaders?.to
-  let errorCode
-
-  if (bounceType === 'Permanent') {
-    errorCode = 'Hard bounce'
-    // Add to black list
-    if (recipients) await Promise.all(recipients.map(addToBlacklist))
-  } else {
-    errorCode = 'Soft bounce'
-  }
-
-  const campaignId = await updateMessageWithError({
-    errorCode,
-    timestamp: metadata.timestamp,
-    id: metadata.id,
-  })
-  await haltCampaignIfThresholdExceeded(campaignId)
-}
-
-/**
- * Updates the error_code in the email_messages table for complaints.
- *  @param message JSON object that contains the notification details
- *  @param timestamp ISO string from notification timestamp
- */
-const updateComplaintStatus = async (metadata: ComplaintMetadata) => {
-  const errorCode = metadata.message?.complaint?.complaintFeedbackType
-  const recipients = metadata?.message?.mail?.commonHeaders?.to
-
-  if (errorCode && recipients) {
-    await Promise.all(recipients.map(addToBlacklist))
-    const campaignId = await updateMessageWithError({
-      errorCode,
-      timestamp: metadata.timestamp,
-      id: metadata.id,
-    })
-    await haltCampaignIfThresholdExceeded(campaignId)
-  }
-}
-
-const handleMessage = async (record: any) => {
-  const message = JSON.parse(record.Sns.Message)
-  const id = getReferenceId(message)
-  const messageId = message?.mail?.commonHeaders?.messageId
-  if (id === undefined) {
-    console.log(
-      `No ${REFERENCE_ID_HEADER} header found for messageId ${messageId}.`
-    )
-    return
-  }
-
-  const notificationType = message?.notificationType
-  const timestamp = record.Sns.Timestamp
-
-  console.log(`Update for notificationType = ${notificationType}`)
-  const metadata = { id, timestamp, messageId }
-  switch (notificationType) {
-    case 'Delivery':
-      await updateMessageWithSuccess(metadata)
-      break
-    case 'Bounce':
-      await updateBouncedStatus({ ...metadata, message })
-      break
-    case 'Complaint':
-      await updateComplaintStatus({ ...metadata, message })
-      break
-    default:
-      console.error(
-        `Can't handle messages with this notification type. notificationType = ${notificationType}`
-      )
-      return
-  }
-}
+import * as ses from './parsers/ses'
+import * as sendgrid from './parsers/sendgrid'
+import { init } from './query'
 
 /**
  *  Lambda to update the email delivery status.
@@ -110,18 +10,31 @@ const handleMessage = async (record: any) => {
 exports.handler = async (event: any) => {
   try {
     await init()
-    await Promise.all(event.Records.map(handleMessage))
 
-    return {
-      statusCode: 200,
-      body: 'Ok',
+    let records: Promise<void>[] = []
+    // Check the signatures of events to determine which parser to use
+    if (ses.isSnsEvent(event)) {
+      // Records is always an array
+      const sesSnsEvent = (event as unknown) as ses.SnsEvent
+      records = sesSnsEvent.Records.map(({ Sns }) => ses.parseRecord(Sns))
+    } else if (ses.isHttpEvent(event)) {
+      // body could be one record or an array of records, hence we concat
+      const body: ses.SesRecord[] = []
+      const sesHttpEvent = body.concat(JSON.parse(event.body))
+      records = sesHttpEvent.map(ses.parseRecord)
+    } else if (sendgrid.isHttpEvent(event)) {
+      // body is always an array
+      const sgEvent = JSON.parse(event.body)
+      records = sgEvent.map(sendgrid.parseRecord)
+    } else {
+      throw new Error('Unable to handle this event')
     }
+
+    await Promise.all(records)
   } catch (err) {
     console.error(`Unhandled server error  ${err.name}: ${err.message}`)
     console.error(`Event: ${JSON.stringify(event)}`)
-
-    return {
-      statusCode: 500,
-    }
   }
+  return
 }
+
