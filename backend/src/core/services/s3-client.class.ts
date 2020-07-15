33a6f9e98e3fb0c3fe8e90a72df806df65c6cd30
Shao Wei <lshaowei18@gmail.com>
2020-07-15 11:27:11 +0800

    chore: Retries when csv parsing fails because s3 couldn't find the key

--- backend/src/core/services/s3-client.class.ts
+++ backend/src/core/services/s3-client.class.ts
@@ -1,4 +1,5 @@
 import S3 from 'aws-sdk/clients/s3'
+import retry from 'async-retry'
 
 import config from '@core/config'
 import { configureEndpoint } from '@core/utils/aws-endpoint'
@@ -30,13 +31,31 @@ export default class S3Client {
   /**
    * Download CSV file from S3 and process it into message.
    * The messages are formed from the template and parameters specified in the csv.
-   *
+   * Retries when s3 can't find the s3Key. This could be due to s3's eventual consistency.
    * @param campaignId
    * @param s3Key
    */
   async getCsvFile(s3Key: string): Promise<Array<CSVParams>> {
-    const downloadStream = this.download(s3Key)
-    const fileContents = await ParseCsvService.parseCsv(downloadStream)
-    return fileContents
+    return await retry(
+      async (bail) => {
+        try {
+          const downloadStream = this.download(s3Key)
+          const fileContents = await ParseCsvService.parseCsv(downloadStream)
+          return fileContents
+        } catch (e) {
+          if (e.code !== 'NoSuchKey') {
+            bail(e)
+            return []
+          }
+          throw e
+        }
+      },
+      {
+        retries: 5,
+        minTimeout: 1000,
+        maxTimeout: 3 * 1000,
+        factor: 1,
+      }
+    )
   }
 }
