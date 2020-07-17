ffd37b572a542f760a640d4554f45f407da0f266
Ian Chen <ian-@gmx.com>
2020-07-17 18:36:54 +0800

    chore: added comments when changing styles

--- frontend/src/components/common/protected-preview/ProtectedPreview.tsx
+++ frontend/src/components/common/protected-preview/ProtectedPreview.tsx
@@ -3,6 +3,12 @@ import cx from 'classnames'
 
 import styles from './ProtectedPreview.module.scss'
 
+/**
+ * For breaking changes to styles, a new css class must be
+ * created in ProtectedPreview.module.scss and stored as a new
+ * column in protected_messages table in the db. This class version
+ * will be fetched together with the payload and dynamically rendered.
+ */
 const ProtectedPreview = ({
   html,
   style = 'style1',
