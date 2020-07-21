c0113ffaebaa526a034a4e2f416f7fd0b61f4a30
Ian Chen <ian-@gmx.com>
2020-07-22 01:06:34 +0800

    feat: checkbox component

---
+++ frontend/src/components/common/checkbox/Checkbox.tsx
@@ -0,0 +1,34 @@
+import React, { Dispatch, SetStateAction } from 'react'
+import cx from 'classnames'
+
+import styles from './Checkbox.module.scss'
+
+const Checkbox = ({
+  checked,
+  onChange,
+  className,
+  children,
+}: {
+  checked: boolean
+  onChange: Dispatch<SetStateAction<boolean>>
+  className?: string
+  children?: React.ReactNode
+}) => {
+  return (
+    <div
+      className={cx(styles.container, className)}
+      onClick={() => onChange((val) => !val)}
+    >
+      <i
+        className={cx(
+          'bx',
+          styles.icon,
+          checked ? 'bxs-checkbox-checked' : 'bx-checkbox'
+        )}
+      ></i>
+      {children}
+    </div>
+  )
+}
+
+export default Checkbox
