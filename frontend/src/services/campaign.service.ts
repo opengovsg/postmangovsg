b57394df3528c701dc60ec83317cdd706d23519d
Seah Chin Ying <scy.6char@gmail.com>
2020-07-24 16:45:56 +0800

    chore: Change frontend request for force update stats

--- frontend/src/services/campaign.service.ts
+++ frontend/src/services/campaign.service.ts
@@ -95,16 +95,27 @@ export async function getCampaignStats(
   campaignId: number,
   forceRefresh = false
 ): Promise<CampaignStats> {
-  return axios
-    .get(`/campaign/${campaignId}/stats?refresh=${forceRefresh}`)
-    .then((response) => {
-      const { status, updatedAt, ...counts } = response.data
-      return new CampaignStats({
-        ...counts,
-        status: parseStatus(status),
-        updatedAt,
+  if (forceRefresh) {
+    return axios
+      .post(`/campaign/${campaignId}/refresh-stats`)
+      .then((response) => {
+        const { status, updatedAt, ...counts } = response.data
+        return new CampaignStats({
+          ...counts,
+          status: parseStatus(status),
+          updatedAt,
+        })
       })
+  }
+
+  return axios.get(`/campaign/${campaignId}/stats`).then((response) => {
+    const { status, updatedAt, ...counts } = response.data
+    return new CampaignStats({
+      ...counts,
+      status: parseStatus(status),
+      updatedAt,
     })
+  })
 }
 
 export async function getCampaignDetails(
