51612843a06847e99a78d45eea91c06adbb64f14
Jean <33819199+jeantanzj@users.noreply.github.com>
2020-07-29 07:16:23 +0800

    chore: remove custom en

--- frontend/src/locales/index.tsx
+++ frontend/src/locales/index.tsx
@@ -1,10 +1,16 @@
 import { setupI18n } from '@lingui/core'
 import { Catalogs, I18n } from '@lingui/core/i18n'
-import catalogEnCustom from './en-custom/messages.js'
 
-const catalogs: Catalogs = { 'en-custom': catalogEnCustom }
+/**
+ * To create a new language or translation:
+ * Copy the `en` folder, and rename it to eg. `en-custom`.
+ * Then import it and setup i18n.
+ * Run `npm run extract && npm run compile`.
+ *  */
 
+import catalogEn from './en/messages.js'
+const catalogs: Catalogs = { en: catalogEn }
 export const i18n: I18n = setupI18n({
-  language: 'en-custom', // language to display for the site. Currently this is hard-coded and cannot be changed via UI.
+  language: 'en', // language to display for the site. Currently this is hard-coded and cannot be changed via UI.
   catalogs,
 })
