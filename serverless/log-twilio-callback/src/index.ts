daf3f2952477cb3e6563a7feb98c04e463bd4c2a
Jean <33819199+jeantanzj@users.noreply.github.com>
2020-08-21 08:30:25 +0800

    chore(deps): replace bcryptjs with bcrypt@^5.0.0

--- serverless/log-twilio-callback/src/index.ts
+++ serverless/log-twilio-callback/src/index.ts
@@ -1,5 +1,5 @@
 import querystring from 'querystring'
-import bcrypt from 'bcryptjs'
+import bcrypt from 'bcrypt'
 import { QueryTypes } from 'sequelize'
 import { Sequelize } from 'sequelize-typescript'
 
