90182bacea636a49b7d1b7fb33b8a0ec23e7622b
jeantanzj <33819199+jeantanzj@users.noreply.github.com>
2020-08-31 23:11:36 +0800

    chore: force sms to be delivered (#673)

--- backend/src/sms/services/twilio-client.class.ts
+++ backend/src/sms/services/twilio-client.class.ts
@@ -11,12 +11,17 @@ export default class TwilioClient {
     this.messagingServiceSid = messagingServiceSid
   }
 
-  public send(recipient: string, message: string): Promise<string | void> {
+  public send(
+    recipient: string,
+    message: string,
+    forceDelivery = false
+  ): Promise<string | void> {
     return this.client.messages
       .create({
         to: recipient,
         body: this.replaceNewLines(message),
         from: this.messagingServiceSid,
+        forceDelivery: forceDelivery,
       })
       .then((result: { [key: string]: string }) => {
         const { status, sid, error_code: errorCode, code } = result
@@ -29,6 +34,16 @@ export default class TwilioClient {
         } else {
           return Promise.reject(new Error(`${status};Unknown error`))
         }
+      })
+      .catch((error: Error) => {
+        // Handle +65802 errors by forcing delivery once
+        if (
+          /\+65802\d+ is not a valid phone number/.test(error.message) &&
+          !forceDelivery
+        ) {
+          return this.send(recipient, message, true)
+        }
+        return Promise.reject(new Error(error.message))
       })
   }
 
