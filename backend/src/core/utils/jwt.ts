d3f5f45e8e4284f9b4f2aa97370f74df502aefc5
Shao Wei <lshaowei18@gmail.com>
2020-08-31 13:46:57 +0800

    chore: fix jwt functions where jwt secret cannot be undefined

--- backend/src/core/utils/jwt.ts
+++ backend/src/core/utils/jwt.ts
@@ -2,11 +2,19 @@ import jwt from 'jsonwebtoken'
 
 import config from '@core/config'
 
-const JWT_SECRET = config.get('jwtSecret')
+const sign = (payload: any): string => {
+  const secret = config.get('jwtSecret')
+  if (secret === undefined) throw new Error('jwtSecret should not be undefined')
+  return jwt.sign(payload, secret, { algorithm: 'HS256' })
+}
+
+const verify = (token: any): string | object => {
+  const secret = config.get('jwtSecret')
+  if (secret === undefined) throw new Error('jwtSecret should not be undefined')
+  return jwt.verify(token, secret, { algorithms: ['HS256'] })
+}
 
 export const jwtUtils = {
-  sign: (payload: any): string =>
-    jwt.sign(payload, JWT_SECRET, { algorithm: 'HS256' }),
-  verify: (token: string): string | object =>
-    jwt.verify(token, JWT_SECRET, { algorithms: ['HS256'] }),
+  sign,
+  verify,
 }
