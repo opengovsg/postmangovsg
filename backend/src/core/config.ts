e3ad71745f8486132157b8c4f0f33ec70095e838
Lam Kee Wei <keewei.lam@gmail.com>
2020-08-31 11:15:54 +0800

    feat: Add support for IAM database in backend

--- backend/src/core/config.ts
+++ backend/src/core/config.ts
@@ -130,6 +130,11 @@ const config = convict({
         format: 'int',
       },
     },
+    useIam: {
+      doc: 'Whether to use IAM for authentication to database',
+      default: false,
+      env: 'DB_USE_IAM',
+    },
   },
   jwtSecret: {
     doc:
