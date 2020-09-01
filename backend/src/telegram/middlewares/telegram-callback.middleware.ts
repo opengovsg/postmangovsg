f9793a28a7a3453422a08a1023ce34476e6fc1e8
Jean <33819199+jeantanzj@users.noreply.github.com>
2020-09-01 08:08:47 +0800

    refactor(telegram): connect middleware

--- backend/src/telegram/middlewares/telegram-callback.middleware.ts
+++ backend/src/telegram/middlewares/telegram-callback.middleware.ts
@@ -1,20 +1,34 @@
 import { Request, Response, NextFunction } from 'express'
-const verifyBotIdRegistered = (
-  _req: Request,
+import { TelegramCallbackService } from '@telegram/services'
+const verifyBotIdRegistered = async (
+  req: Request,
   res: Response,
-  _next: NextFunction
-): Response => {
-  return res.sendStatus(403)
+  next: NextFunction
+): Promise<Response | void> => {
+  const { botToken } = req.params
+  const botId = botToken.split(':')[0]
+  if (!(botId && req.body)) {
+    throw new Error('botId, botToken and Telegram update must be specified.')
+  }
+  if (await TelegramCallbackService.verifyBotIdRegistered(botId)) {
+    res.locals.botId = botId
+    res.locals.botToken = botToken
+    return next()
+  }
+  return res.sendStatus(400)
 }
 
-const handleUpdate = (
-  _req: Request,
-  res: Response,
-  _next: NextFunction
-): Response => {
+const handleUpdate = async (
+  req: Request,
+  res: Response
+): Promise<Response | void> => {
+  await TelegramCallbackService.handleUpdate(
+    res.locals.botId,
+    res.locals.botToken,
+    req.body
+  )
   return res.sendStatus(200)
 }
-
 export const TelegramCallbackMiddleware = {
   verifyBotIdRegistered,
   handleUpdate,
