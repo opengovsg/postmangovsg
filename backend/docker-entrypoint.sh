ae80b76c25fd6c7cba5cf0a29511eb9e9bd8bfbe
Shao Wei <lshaowei18@gmail.com>
2020-09-14 12:18:01 +0800

    feat: Fetch secrets from Secrets Manager & export them

---
+++ backend/docker-entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# Get the secrets from aws secrets manager
+# transform them into KEY=value pairing, and store in temp file
+aws secretsmanager get-secret-value --secret-id '@SECRET_ID' --query SecretString --output text | jq -r 'to_entries|map("\(.key)=\(.value|tostring)")|.[]' > /tmp/secrets.env
+# Export all the key value pairings in the temp file
+eval $(cat /tmp/secrets.env | sed 's/^/export /')
+rm -f /tmp/secrets.env
+npm start
