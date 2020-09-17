371b63d284684ccc8993fbdcc16cbd21dedefd67
Lam Kee Wei <keewei.lam@gmail.com>
2020-09-17 18:17:08 +0800

    feat: Added support for decrypting using GCP KMS

--- serverless/database-backup/src/decrypt.ts
+++ serverless/database-backup/src/decrypt.ts
@@ -3,6 +3,11 @@ import fs from 'fs'
 import stream from 'stream'
 import util from 'util'
 
+import yargs from 'yargs'
+import { KeyManagementServiceClient } from '@google-cloud/kms'
+
+const pipeline = util.promisify(stream.pipeline)
+
 interface EncryptionParams {
   algorithm: crypto.CipherGCMTypes
   iv: string
@@ -10,32 +15,78 @@ interface EncryptionParams {
   encryptedKey: string
 }
 
-const pipeline = util.promisify(stream.pipeline)
+const argv = yargs
+  .option('input', {
+    type: 'string',
+    demandOption: true,
+    description: 'input file to be decrypted',
+  })
+  .option('params', {
+    type: 'string',
+    demandOption: true,
+    description: 'path to params file',
+  })
+  .option('output', {
+    type: 'string',
+    default: 'postman.decrypted.dump',
+    description: 'path to write decrypted file to ',
+  })
+  .option('privateKey', {
+    type: 'string',
+    default: '',
+    description: 'path to local private key to use for decryption',
+  })
+  .option('kmsKey', {
+    type: 'string',
+    default: '',
+    description: 'GCP resource ID of key to used for decryption',
+  }).argv
 
-const INPUT_FILE = process.env.INPUT_FILE || 'postman.dump'
-const OUTPUT_FILE = process.env.OUTPUT_FILE || 'postman.decrypted.dump'
-const PARAMS_FILE = process.env.PARAMS_FILE || 'postman.json'
-const PRIVATE_KEY = process.env.PRIVATE_KEY || 'key.pem'
+const client = new KeyManagementServiceClient()
+
+async function decryptKeyEncryptionKey(ciphertext: Buffer) {
+  if (argv.kmsKey) {
+    console.log('Decrypting using GCP KMS')
+    const [result] = await client.asymmetricDecrypt({
+      name: argv.kmsKey,
+      ciphertext,
+    })
+
+    return result.plaintext
+  }
+
+  console.log('Decrypting using local private key')
+  const key = crypto.createPrivateKey(fs.readFileSync(argv.privateKey))
+  const decrypted = crypto.privateDecrypt(
+    {
+      key,
+      oaepHash: 'sha256',
+      padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+    },
+    ciphertext
+  )
+  return decrypted
+}
 
 async function decrypt() {
   const params: EncryptionParams = JSON.parse(
-    fs.readFileSync(PARAMS_FILE, 'utf8')
+    fs.readFileSync(argv.params, 'utf8')
   )
 
-  const privateKey = crypto.createPrivateKey(fs.readFileSync(PRIVATE_KEY))
-  const decryptionKey = crypto.privateDecrypt(
-    privateKey,
-    Buffer.from(params.encryptedKey, 'base64')
-  )
+  const encryptedKey = Buffer.from(params.encryptedKey, 'base64')
+  const decryptionKey = await decryptKeyEncryptionKey(encryptedKey)
+  if (!decryptionKey) {
+    throw new Error('Error: Failed to decrypt key')
+  }
 
-  const inputFileStream = fs.createReadStream(INPUT_FILE)
-  const outputFileStream = fs.createWriteStream(OUTPUT_FILE)
+  const inputFileStream = fs.createReadStream(argv.input)
+  const outputFileStream = fs.createWriteStream(argv.output)
   const iv = Buffer.from(params.iv, 'base64')
   const decipher = crypto.createDecipheriv(params.algorithm, decryptionKey, iv)
   decipher.setAuthTag(Buffer.from(params.authTag, 'base64'))
 
   await pipeline(inputFileStream, decipher, outputFileStream)
-  console.log(`Success: ${INPUT_FILE} decrypted to ${OUTPUT_FILE}`)
+  console.log(`Success: ${argv.input} decrypted to ${argv.output}`)
 }
 
 decrypt().catch((err) => console.error(err))
