93e0edb0aabbb8c5d9c2526488f0fe9df39b9c6e
Jean <33819199+jeantanzj@users.noreply.github.com>
2020-09-29 00:19:20 +0800

    refactor(backend): update column type of email in email_from_address

--- backend/src/email/models/email-from-address.ts
+++ backend/src/email/models/email-from-address.ts
@@ -3,7 +3,7 @@ import { Column, DataType, Model, Table } from 'sequelize-typescript'
 @Table({ tableName: 'email_from_address', underscored: true, timestamps: true })
 export class EmailFromAddress extends Model<EmailFromAddress> {
   @Column({
-    type: DataType.STRING,
+    type: DataType.TEXT,
     primaryKey: true,
   })
   email!: string
