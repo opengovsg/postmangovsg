a068ab715c612d4894b8155cab8968c8cd3b915c
Azima M I <miazima@gmail.com>
2020-10-16 00:16:59 +0800

    Compute queue position and wait time for queued campaigns (#677)
    * feat: compute queue position and wait time for queued campaigns
    
    * feature: compute wait time from queued campaigns using job updated_at time
    
    * chore: check for smaller ids in the job queue to determine queue order
    
    * feat: show time on frontend
    
    * chore: reword
    
    * chore: fix lingui problem with plurals
    
    https://github.com/lingui/js-lingui/issues/437
    
    * chore: replace broken plural
    
    Co-authored-by: jeantanzj <33819199+jeantanzj@users.noreply.github.com>

--- backend/src/core/interfaces/campaign.interface.ts
+++ backend/src/core/interfaces/campaign.interface.ts
@@ -52,6 +52,7 @@ export interface CampaignStats extends CampaignStatsCount {
   status: string
   halted?: boolean
   status_updated_at?: Date
+  wait_time?: number
 }
 
 export interface CampaignStatsCount {
