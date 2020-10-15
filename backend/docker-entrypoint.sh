48826b8b32172f26b61958e797bcb415b6c9fc3b
jeantanzj <33819199+jeantanzj@users.noreply.github.com>
2020-10-15 17:09:43 +0800

    chore: use dotenv to pass in env vars

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 # Get the secrets from aws secrets manager
 # transform them into KEY=value pairing, and store in temp file
-aws secretsmanager get-secret-value --secret-id '@SECRET_ID' --query SecretString --output text | jq -r 'to_entries|map("\(.key)=\(.value|tostring)")|.[]' > /tmp/secrets.env
-# Export all the key value pairings in the temp file
-export "$(cat /tmp/secrets.env)"
-rm -f /tmp/secrets.env
+aws secretsmanager get-secret-value --secret-id '@SECRET_ID' --query SecretString --output text | jq -r 'to_entries|map("\(.key)=\(.value|tostring)")|.[]' > .env
+# rely on dotenv to add the environment variables to the node process
 npm start
