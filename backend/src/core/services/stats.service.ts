2f9f7997dc01ef5c4753ee26b437e7ba07f1c118
jeantanzj <33819199+jeantanzj@users.noreply.github.com>
2020-10-16 00:13:58 +0800

    chore: return recipients in sending state

--- backend/src/core/services/stats.service.ts
+++ backend/src/core/services/stats.service.ts
@@ -181,7 +181,7 @@ const getDeliveredRecipients = async (
     where: {
       campaignId,
       status: {
-        [Op.not]: MessageStatus.Sending,
+        [Op.ne]: null,
       },
     },
     attributes: ['recipient', 'status', 'error_code', 'updated_at'],
