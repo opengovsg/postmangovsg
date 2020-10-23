f2e1373e2135d57813f1f72d4045f44e8cdf731e
Azima M I <miazima@gmail.com>
2020-10-24 01:17:45 +0800

    chore: add service acc setup and ignore empty lines in env file

--- serverless/verify-backup/scripts/deploy.sh
+++ serverless/verify-backup/scripts/deploy.sh
@@ -7,17 +7,19 @@
 # https://cloud.google.com/docs/authentication/production
 # 3. Set GCLOUD_RUN_SERVICE_ACCOUNT env var to service account
 
+echo 'Setting up service account...'
+gcloud auth activate-service-account $GCLOUD_SERVICE_ACCOUNT --key-file=$GOOGLE_APPLICATION_CREDENTIALS
 
 echo 'Submitting build to Cloud Build...'
 gcloud builds submit --tag gcr.io/postmangovsg/verify-backup --gcs-log-dir gs://postmangovsg-cloudbuild-logs/logs
 
 echo 'Deploying build to Cloud Run...'
-# --set-env-vars `cat .env | awk -v ORS=, '{ print $1 }'`pulls env vars from .env file and formats as args
+# --set-env-vars `grep -v '^#' .env | awk -v ORS=, 'NF { print $1 }'`pulls env vars from .env file and formats as args
 gcloud run deploy verify-backup \
   --region asia-southeast1 \
   --no-allow-unauthenticated  \
   --image gcr.io/postmangovsg/verify-backup \
-  --service-account $GCLOUD_RUN_SERVICE_ACCOUNT \
+  --service-account $GCLOUD_SERVICE_ACCOUNT \
   --platform managed \
-  --set-env-vars `cat .env | awk -v ORS=, '{ print $1 }'` \
+  --set-env-vars `grep -v '^#' .env | awk -v ORS=, 'NF { print $1 }'` \
   --memory 4G
