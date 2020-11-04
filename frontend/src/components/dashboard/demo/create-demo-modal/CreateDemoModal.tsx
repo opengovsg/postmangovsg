761da9a6427227e7919aef3cc33a865244e07edc
jeantanzj <33819199+jeantanzj@users.noreply.github.com>
2020-11-05 00:05:48 +0800

    chore: rework spacing for CreateDemoModal

--- frontend/src/components/dashboard/demo/create-demo-modal/CreateDemoModal.tsx
+++ frontend/src/components/dashboard/demo/create-demo-modal/CreateDemoModal.tsx
@@ -2,7 +2,12 @@ import React, { useContext, useEffect, useState } from 'react'
 import cx from 'classnames'
 import styles from './CreateDemoModal.module.scss'
 import { Campaign, channelIcons, ChannelType } from 'classes/Campaign'
-import { ErrorBlock, PrimaryButton, TextInput } from 'components/common'
+import {
+  ErrorBlock,
+  PrimaryButton,
+  TextButton,
+  TextInput,
+} from 'components/common'
 import { ModalContext } from 'contexts/modal.context'
 import { useHistory } from 'react-router-dom'
 import { createCampaign } from 'services/campaign.service'
@@ -23,6 +28,7 @@ const CreateDemoModal = ({
   const [selectedName, setSelectedName] = useState('')
   const [demoCampaignMessage, setDemoCampaignMessage] = useState('')
   const [isCreateEnabled, setIsCreateEnabled] = useState(true)
+
   useEffect(() => {
     // Handle case where one of the channels does not have any demos left
     // Set the default selected channel to the channel that still has demos left
@@ -74,82 +80,78 @@ const CreateDemoModal = ({
 
   return (
     <>
-      <div className={styles.modalHeader}>{demoCampaignMessage}</div>
-      <div className={styles.section}>
-        <h5 className={styles.subtitle}>
-          The demo campaign has been named for you
-        </h5>
-        <TextInput
-          className={styles.input}
-          type="text"
-          value={selectedName}
-          disabled
-        ></TextInput>
-      </div>
-      <div className={cx(styles.section, styles.separator)}>
-        <h2 className={styles.title}>Choose the channel you want to send in</h2>
-        <h5 className={styles.subtitle}>
-          Demo campaigns are for SMS and Telegram channels only, as the Email
-          channel is already free. You may send messages to at most 20
-          recipients in each campaign. One try will be used up only after a demo
-          campaign has been sent, otherwise, it will stay as a draft.
-        </h5>
-      </div>
+      <div className={styles.content}>
+        <div className={styles.modalHeader}>{demoCampaignMessage}</div>
+        <div className={styles.section}>
+          <h5 className={styles.subtitle}>
+            The demo campaign has been named for you
+          </h5>
+          <TextInput
+            className={styles.input}
+            type="text"
+            value={selectedName}
+            disabled
+          ></TextInput>
+        </div>
+        <div className={cx(styles.section, styles.separator)}>
+          <h2 className={styles.title}>
+            Choose the channel you want to send in
+          </h2>
+          <h5 className={styles.subtitle}>
+            Demo campaigns are for SMS and Telegram channels only, as the Email
+            channel is already free. You may send messages to at most 20
+            recipients in each campaign. One try will be used up only after a
+            demo campaign has been sent, otherwise, it will stay as a draft.
+          </h5>
+        </div>
 
-      <div className={styles.channelTypes}>
-        <div className={styles.channelContainer}>
-          <PrimaryButton
-            className={cx(styles.button, {
-              [styles.active]: selectedChannel === ChannelType.SMS,
-            })}
-            onClick={() => setSelectedChannel(ChannelType.SMS)}
+        <div className={styles.channelTypes}>
+          <div className={styles.channelContainer}>
+            <PrimaryButton
+              className={cx(styles.button, {
+                [styles.active]: selectedChannel === ChannelType.SMS,
+              })}
+              onClick={() => setSelectedChannel(ChannelType.SMS)}
+            >
+              SMS
+              <i
+                className={cx('bx', styles.icon, channelIcons[ChannelType.SMS])}
+              ></i>
+            </PrimaryButton>
+          </div>
+          <div className={styles.channelContainer}>
+            <PrimaryButton
+              className={cx(styles.button, {
+                [styles.active]: selectedChannel === ChannelType.Telegram,
+              })}
+              onClick={() => setSelectedChannel(ChannelType.Telegram)}
+            >
+              Telegram
+              <i
+                className={cx(
+                  'bx',
+                  styles.icon,
+                  channelIcons[ChannelType.Telegram]
+                )}
+              ></i>
+            </PrimaryButton>
+          </div>
+        </div>
+        <div className="separator"></div>
+        <div className={styles.actions}>
+          <OutboundLink
+            eventLabel={i18n._(LINKS.guideDemoUrl)}
+            to={i18n._(LINKS.guideDemoUrl)}
+            target="_blank"
           >
-            SMS
-            <i
-              className={cx('bx', styles.icon, channelIcons[ChannelType.SMS])}
-            ></i>
+            <TextButton minButtonWidth> Learn more about demos </TextButton>
+          </OutboundLink>
+          <PrimaryButton onClick={createDemo} disabled={!isCreateEnabled}>
+            Create demo campaign
           </PrimaryButton>
         </div>
-        <div className={styles.channelContainer}>
-          <PrimaryButton
-            className={cx(styles.button, {
-              [styles.active]: selectedChannel === ChannelType.Telegram,
-            })}
-            onClick={() => setSelectedChannel(ChannelType.Telegram)}
-          >
-            Telegram
-            <i
-              className={cx(
-                'bx',
-                styles.icon,
-                channelIcons[ChannelType.Telegram]
-              )}
-            ></i>
-          </PrimaryButton>
-        </div>
+        <ErrorBlock>{errorMessage}</ErrorBlock>
       </div>
-      <div className="separator"></div>
-      <div className={styles.actions}>
-        <OutboundLink
-          eventLabel={i18n._(LINKS.guideDemoUrl)}
-          to={i18n._(LINKS.guideDemoUrl)}
-          target="_blank"
-        >
-          Learn more about demos
-        </OutboundLink>
-        {/* <TextButton
-          minButtonWidth
-          onClick={() => {
-            console.log('watch')
-          }}
-        >
-          Learn more about demos
-        </TextButton> */}
-        <PrimaryButton onClick={createDemo} disabled={!isCreateEnabled}>
-          Create demo campaign
-        </PrimaryButton>
-      </div>
-      <ErrorBlock>{errorMessage}</ErrorBlock>
     </>
   )
 }
