121a55bdc26bcfca0d62a736a3b0d40e475fa858
Lam Kee Wei <keewei.lam@gmail.com>
2020-11-17 11:12:42 +0800

    chore: Add logging and Sentry monitoring

--- serverless/redaction-digest/src/index.ts
+++ serverless/redaction-digest/src/index.ts
@@ -1,16 +1,34 @@
 import 'source-map-support/register'
+import * as Sentry from '@sentry/node'
 
+import config from './config'
 import { init, getUserRedactedCampaigns } from './redaction'
-import { createEmailBody } from './utils/generate-digest-mail'
 import { mailClient } from './mail-client.class'
+import { Logger } from './utils/logger'
+import { createEmailBody } from './utils/generate-digest-mail'
+
+const logger = new Logger('redaction-digest')
+
+Sentry.init({
+  dsn: config.get('sentryDsn'),
+  environment: config.get('env'),
+})
+Sentry.configureScope((scope) => {
+  const functionName =
+    process.env.AWS_LAMBDA_FUNCTION_NAME ||
+    `redaction-digest-${config.get('env')}`
+  scope.setTag('lambda-function-name', functionName)
+})
 
 /**
  * Lambda handler to send redaction digest
  */
 const handler = async (): Promise<{ statusCode: number }> => {
   await init()
+
   const userRedactedCampaigns = await getUserRedactedCampaigns()
 
+  const failedRecipients = []
   for (const userCampaigns of userRedactedCampaigns) {
     const { email, campaigns } = userCampaigns
 
@@ -24,10 +42,24 @@ const handler = async (): Promise<{ statusCode: number }> => {
         body: emailBody,
       })
 
-      console.log(`Reminder sent to ${email}`)
+      logger.log(`Redaction reminder sent to ${email}`)
     } catch (err) {
-      console.error(`Failed to send reminder to ${email}`)
+      logger.log(
+        `Failed to send redaction reminder to ${email}. Error: ${err.message}`
+      )
+
+      failedRecipients.push(email)
+      Sentry.captureException(err)
+      await Sentry.flush(2000)
     }
+  }
+
+  if (failedRecipients.length > 0) {
+    logger.log(
+      `Failed to send redaction reminder emails to ${failedRecipients.join(
+        ', '
+      )}`
+    )
   }
 
   return { statusCode: 200 }
