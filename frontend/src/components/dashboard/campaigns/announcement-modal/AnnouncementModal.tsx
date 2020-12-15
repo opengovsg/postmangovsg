b873b2b4de78ed4cd4c25c9d7ccecb8a6092c19d
themogget <y.kaiyi@outlook.com>
2020-12-15 13:58:09 +0800

    feat(announcement-modal): Create AnnouncementModal component

---
+++ frontend/src/components/dashboard/campaigns/announcement-modal/AnnouncementModal.tsx
@@ -0,0 +1,77 @@
+import React, { useContext, useState } from 'react'
+import cx from 'classnames'
+
+import { PrimaryButton, ErrorBlock } from 'components/common'
+import { ModalContext } from 'contexts/modal.context'
+
+import ConfirmImage from 'assets/img/confirm-modal.svg'
+import styles from './AnnouncementModal.module.scss'
+
+const AnnouncementModal = ({
+  title,
+  subtitle,
+  buttonText,
+  buttonIcon,
+  destructive,
+  onReadMore,
+  onClose,
+}: {
+  title: string
+  subtitle: string
+  buttonText: string
+  buttonIcon?: string
+  destructive?: boolean
+  onReadMore: () => Promise<any> | any
+  onClose: any
+}) => {
+  const modalContext = useContext(ModalContext)
+  const [errorMessage, setErrorMessage] = useState('')
+  const modalBackgroundId = 'modal-background'
+
+  async function handleClickBackground(event: any) {
+    if (event.target.id === modalBackgroundId) {
+      onClose()
+    }
+  }
+
+  async function onReadMoreClicked(): Promise<void> {
+    try {
+      await onReadMore()
+      // Closes the modal
+      modalContext.close()
+    } catch (err) {
+      setErrorMessage(err.message)
+    }
+  }
+
+  return (
+    <div
+      id={modalBackgroundId}
+      className={styles.modalBg}
+      onClick={handleClickBackground}
+    >
+      <div className={styles.modal}>
+        <div className={styles.modalImg}>
+          // Templating for linqui should go here?
+          <img src={ConfirmImage} alt="Modal graphic"></img>
+        </div>
+        <h2 className={styles.title}>{title}</h2>
+        <h4 className={styles.subtitle}>{subtitle}</h4>
+        <div className={styles.options}>
+          <PrimaryButton
+            className={destructive ? styles.redButton : styles.greenButton}
+            onClick={onReadMoreClicked}
+          >
+            {buttonText}
+            {buttonIcon && (
+              <i className={cx('bx', styles.icon, buttonIcon)}></i>
+            )}
+          </PrimaryButton>
+        </div>
+        <ErrorBlock>{errorMessage}</ErrorBlock>
+      </div>
+    </div>
+  )
+}
+
+export default AnnouncementModal
