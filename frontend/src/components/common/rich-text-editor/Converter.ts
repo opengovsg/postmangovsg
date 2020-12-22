74c563bb1d7d84293d8aa93d74c53c66602c91ab
Lam Kee Wei <keewei.lam@gmail.com>
2020-12-22 18:00:39 +0800

    feat(frontend): Add conversion from HTML to ContentBlocks

--- frontend/src/components/common/rich-text-editor/Converter.ts
+++ frontend/src/components/common/rich-text-editor/Converter.ts
@@ -1,10 +1,16 @@
 import {
+  ContentState,
+  ContentBlock,
+  CharacterMetadata,
+  DraftInlineStyle,
+  genKey,
   RawDraftContentState,
   RawDraftContentBlock,
   RawDraftInlineStyleRange,
   RawDraftEntityRange,
   RawDraftEntity,
 } from 'draft-js'
+import immutable from 'immutable'
 import { orderBy, findIndex } from 'lodash'
 
 import { HTMLTree } from './HTMLTree'
@@ -13,7 +19,18 @@ interface DraftEntityMap {
   [key: string]: RawDraftEntity<{ [key: string]: any }>
 }
 
-type TagType = 'strong' | 'em' | 'ins' | 'span' | 'a' | 'img' | 'ul' | 'ol'
+type TagType =
+  | 'strong'
+  | 'em'
+  | 'ins'
+  | 'span'
+  | 'a'
+  | 'img'
+  | 'ul'
+  | 'ol'
+  | 'b'
+  | 'i'
+  | 'u'
 
 interface HTMLTag {
   tag: TagType
@@ -36,9 +53,9 @@ const BLOCK_TYPE_MAPPPING: Record<string, string> = {
 }
 
 const INLINE_STYLE_MAPPING: Record<string, TagType> = {
-  BOLD: 'strong',
-  ITALIC: 'em',
-  UNDERLINE: 'ins',
+  BOLD: 'b',
+  ITALIC: 'i',
+  UNDERLINE: 'u',
 }
 
 const ENTITY_MAPPING: Record<string, TagType> = {
@@ -369,7 +386,7 @@ const handleTable = (
       tree.select(tr)
     } else {
       const { cols, row, col } = currBlock.data as Record<string, number>
-      const startRow = (row * (cols + 1) + col) % (cols + 1) === 0
+      const startRow = (row * cols + col) % cols === 0
       if (startRow) {
         const parent = tree.active?.parent
         if (parent) {
@@ -421,4 +438,319 @@ const convertToHTML = (contentState: RawDraftContentState): string => {
   return tree.toHTML()
 }
 
-export const Converter = { convertToHTML }
+const TAG_BLOCK_TYPE_MAPPING: Record<string, string> = {
+  p: 'unstyled',
+  h1: 'header-one',
+  h2: 'header-two',
+  h3: 'header-three',
+  h4: 'header-four',
+  h5: 'header-five',
+  h6: 'header-six',
+  div: 'atomic',
+  td: 'table-cell',
+  li: 'list-item',
+}
+
+const INLINE_STYLE_TAG_MAPPING: Record<string, string> = {
+  b: 'BOLD',
+  i: 'ITALIC',
+  u: 'UNDERLINE',
+}
+
+const getSafeHtmlBody = (htmlStr: string): HTMLElement => {
+  // Will not execute script. https://www.w3.org/TR/DOM-Parsing/#widl-DOMParser-parseFromString-Document-DOMString-str-SupportedType-type
+  const parser = new DOMParser()
+  const html = parser.parseFromString(htmlStr, 'text/html')
+  return html.body
+}
+
+class ContentBlocksBuilder {
+  currentBlockType = 'unstyled'
+  currentEntity: string | null = null
+  currentDepth = 0
+  currentText = ''
+  characterList = immutable.List<CharacterMetadata>()
+
+  wrapper?: string
+
+  contentBlocks: ContentBlock[] = []
+  contentState: ContentState = ContentState.createFromText('')
+
+  tableData?: immutable.Map<string, number> = undefined
+
+  getContentBlocks(): {
+    contentBlocks: ContentBlock[]
+    entityMap: immutable.OrderedMap<string, any>
+  } {
+    return {
+      contentBlocks: this.contentBlocks,
+      entityMap: this.contentState.getEntityMap(),
+    }
+  }
+
+  createContentBlocks(nodes: ChildNode[], style: DraftInlineStyle) {
+    for (let i = 0; i < nodes.length; i++) {
+      const node = nodes[i]
+      const nodeName = node.nodeName.toLowerCase()
+
+      if (nodeName === 'table') {
+        this.addTable(node)
+        continue
+      }
+
+      if (nodeName === 'td') {
+        this.addTableCell(node)
+        continue
+      }
+
+      if (nodeName === 'ul' || nodeName === 'ol') {
+        this.addList(node)
+        continue
+      }
+
+      if (nodeName === 'a') {
+        this.addLink(node, style)
+        continue
+      }
+
+      if (nodeName === 'img') {
+        this.addImage(node, style)
+        continue
+      }
+
+      if (nodeName === 'br') {
+        this.appendText('\n', style)
+        continue
+      }
+
+      if (nodeName === '#text') {
+        const text = node.textContent?.trim()
+        if (text) this.appendText(text, style)
+        continue
+      }
+
+      // Handle tag styles
+      let newStyle = this.setStylesFromCSS(node, style)
+      const inlineStyle = INLINE_STYLE_TAG_MAPPING[nodeName]
+      if (inlineStyle) {
+        newStyle = newStyle.add(inlineStyle)
+      }
+
+      const blockType = TAG_BLOCK_TYPE_MAPPING[nodeName]
+      if (blockType) {
+        // TODO: Prevent unstyled blocks from being entered when currentText is empty but yet allow empty blocks before and after for images and tables
+        this.flush()
+        this.currentBlockType = blockType
+      }
+
+      const children = Array.from(node.childNodes)
+      this.createContentBlocks(children, newStyle)
+    }
+
+    // Flush remaining data to a new block. table-cells and list-items are typically inserted here since
+    // there are no nested blocks within them.
+    this.flush()
+
+    return this
+  }
+
+  private clear() {
+    this.currentBlockType = 'unstyled'
+    this.currentEntity = null
+    this.currentText = ''
+    this.characterList = immutable.List<CharacterMetadata>()
+    this.contentState = ContentState.createFromText('')
+  }
+
+  private flush() {
+    // We always insert table-cell and list-item even if they have no content to preserve structure.
+    if (
+      this.currentText !== '' ||
+      ['table-cell', 'list-item'].includes(this.currentBlockType)
+    ) {
+      this.makeContentBlock()
+    }
+  }
+
+  private makeContentBlock(data = {}) {
+    const key = genKey()
+    let type = this.currentBlockType
+
+    // Set specific list type based on wrapper if it is a list-item
+    if (type === 'list-item') {
+      type = this.wrapper === 'ul' ? 'unordered-list-item' : 'ordered-list-item'
+    }
+
+    data = type === 'table-cell' && this.tableData ? this.tableData : data
+
+    const contentBlock = new ContentBlock({
+      key,
+      type,
+      text: this.currentText,
+      characterList: this.characterList,
+      data: immutable.Map(data),
+      depth: this.currentDepth,
+    })
+
+    this.contentBlocks.push(contentBlock)
+    this.clear()
+  }
+
+  private appendText(text: string, style: DraftInlineStyle) {
+    // Append text to current text
+    this.currentText += text
+    // Apply existing style for current text range
+    const characterMeta = CharacterMetadata.create({
+      style,
+      ...(this.currentEntity ? { entity: this.currentEntity } : {}),
+    })
+
+    this.characterList = this.characterList.push(
+      ...Array(text.length).fill(characterMeta)
+    )
+  }
+
+  private addLink(node: ChildNode, style: DraftInlineStyle) {
+    const link = node as HTMLAnchorElement
+    const { href, target, title } = link
+
+    // Add entity
+    this.contentState = this.contentState.createEntity('LINK', 'MUTABLE', {
+      url: href,
+      targetOption: target,
+      title,
+    })
+
+    // Process child nodes with entity set as inserted link
+    this.currentEntity = this.contentState.getLastCreatedEntityKey()
+    const children = Array.from(node.childNodes)
+    this.createContentBlocks(children, style)
+
+    // Reset entity
+    this.currentEntity = null
+  }
+
+  private addImage(node: ChildNode, style: DraftInlineStyle) {
+    const image = node as HTMLImageElement
+    const { src, height, width } = image
+
+    this.contentState = this.contentState.createEntity('IMAGE', 'MUTABLE', {
+      src,
+      height: height > 0 ? height : 'auto',
+      width: `${width}%`,
+    })
+
+    this.currentEntity = this.contentState.getLastCreatedEntityKey()
+    this.appendText(' ', style)
+    this.currentEntity = null
+  }
+
+  private addTable(table: ChildNode) {
+    // Close off any previous block with text
+    this.flush()
+
+    // Get the dimensions of the table
+    const tbody = table.childNodes[0]
+    if (table.childNodes.length !== 1 && tbody.nodeName !== 'tbody') {
+      throw new Error('Invalid table HTML')
+    }
+
+    const rows = Array.from(tbody.childNodes).filter(
+      (n) => n.nodeName.toLowerCase() === 'tr'
+    )
+
+    const numRows = rows.length
+    if (numRows < 1) return
+
+    const numCols = Array.from(rows[0].childNodes).filter(
+      (n) => n.nodeName.toLowerCase() === 'td'
+    ).length
+
+    this.tableData = immutable.Map({
+      rows: numRows,
+      cols: numCols,
+      row: 0,
+      col: 0,
+    })
+
+    rows.forEach((row, rowIdx) => {
+      if (this.tableData) {
+        this.tableData = this.tableData.set('row', rowIdx)
+      }
+      const tableCells = Array.from(row.childNodes)
+
+      tableCells.forEach((cell, colIdx) => {
+        if (this.tableData) {
+          this.tableData = this.tableData.set('col', colIdx)
+        }
+        this.createContentBlocks([cell], immutable.OrderedSet())
+      })
+    })
+
+    this.tableData = undefined
+  }
+
+  private addTableCell(cell: ChildNode) {
+    this.currentBlockType = 'table-cell'
+    const children = Array.from(cell.childNodes)
+    this.createContentBlocks(children, immutable.OrderedSet())
+    // this.makeContentBlock(this.tableData)
+  }
+
+  private addList(list: ChildNode) {
+    const prevWrapper = this.wrapper
+    const prevDepth = this.currentDepth
+    this.wrapper = list.nodeName.toLowerCase()
+
+    // Close off previous block first if there was text. This deals with the case when parsing nested list.
+    // For example for <li>text<ul>...</ul></li>, we will need to close off the list item holding "text"
+    // first before starting the nested list.
+
+    // TODO: How to deal with empty list item
+    this.flush()
+
+    if (prevWrapper === 'ul' || prevWrapper === 'ol') {
+      this.currentDepth++
+    }
+
+    list.childNodes.forEach((child) => {
+      this.createContentBlocks([child], immutable.OrderedSet())
+    })
+
+    this.wrapper = prevWrapper
+    if (prevWrapper === 'ul' || prevWrapper === 'ol') {
+      this.currentDepth = prevDepth
+    }
+  }
+
+  private setStylesFromCSS(
+    node: ChildNode,
+    style: DraftInlineStyle
+  ): DraftInlineStyle {
+    const { style: cssStyle } = node as HTMLElement
+    if (cssStyle) {
+      const { color, background } = cssStyle
+      // TODO: Set style to style set
+      if (color) console.log('color', color)
+      if (background) console.log('background', background)
+    }
+
+    return style
+  }
+}
+
+const convertFromHTML = (
+  html: string
+): {
+  contentBlocks: ContentBlock[]
+  entityMap: immutable.OrderedMap<string, any>
+} => {
+  const root = getSafeHtmlBody(html)
+  const builder = new ContentBlocksBuilder()
+
+  return builder
+    .createContentBlocks([root], immutable.OrderedSet())
+    .getContentBlocks()
+}
+
+export const Converter = { convertToHTML, convertFromHTML }
