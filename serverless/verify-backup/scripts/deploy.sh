cf7b0930b7e1d15133d28874583210fe7ed531ce
Azima M I <miazima@gmail.com>
2021-02-09 14:55:13 +0800

    chore: remove gcloud sdk path

--- serverless/verify-backup/scripts/deploy.sh
+++ serverless/verify-backup/scripts/deploy.sh
@@ -6,14 +6,14 @@
 # 2. Ensure instructions in Pre-requisites to run scripts section in readme are fulfilled
 
 echo 'Setting up service account...'
-./google-cloud-sdk/bin/gcloud auth activate-service-account $GCLOUD_SERVICE_ACCOUNT --key-file=$GOOGLE_APPLICATION_CREDENTIALS
+gcloud auth activate-service-account $GCLOUD_SERVICE_ACCOUNT --key-file=$GOOGLE_APPLICATION_CREDENTIALS
 
 echo 'Submitting build to Cloud Build...'
-./google-cloud-sdk/bin/gcloud builds submit --tag gcr.io/postmangovsg/verify-backup --gcs-log-dir gs://postmangovsg-cloudbuild-logs/logs
+gcloud builds submit --tag gcr.io/postmangovsg/verify-backup --gcs-log-dir gs://postmangovsg-cloudbuild-logs/logs
 
 echo 'Deploying build to Cloud Run...'
 # --set-env-vars `grep -v '^#' .env | awk -v ORS=, 'NF { print $1 }'`pulls env vars from .env file and formats as args
-./google-cloud-sdk/bin/gcloud run deploy $GCLOUD_RUN_SERVICE_NAME \
+gcloud run deploy $GCLOUD_RUN_SERVICE_NAME \
   --region asia-southeast1 \
   --no-allow-unauthenticated \
   --image gcr.io/postmangovsg/verify-backup \
