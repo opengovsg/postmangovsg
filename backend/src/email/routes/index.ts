69fa9d6fee5eac8ee9d47b75b582da423e2ccd9e
Zhao Wei Liew <zhaoweiliew@gmail.com>
2021-03-01 15:47:05 +0800

    fix: Fix typo

--- backend/src/email/routes/index.ts
+++ backend/src/email/routes/index.ts
@@ -1,4 +1,4 @@
 export { default as emailCampaignRoutes } from './email-campaign.routes'
 export { default as emailSettingsRoutes } from './email-settings.routes'
 export { default as emailCallbackRoutes } from './email-callback.routes'
-export { default as emailTransactional } from './email-transactional.routes'
+export { default as emailTransactionalRoutes } from './email-transactional.routes'
