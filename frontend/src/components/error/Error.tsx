ddb62868c0f379da521240dabeba7993a23c6ba8
Zhao Wei Liew <zhaoweiliew@gmail.com>
2021-03-02 15:58:23 +0800

    feat(Error): Add navigation buttons; improve layout
    - Align text to center
    - Add navigation buttons to previous page and to dashboard

--- frontend/src/components/error/Error.tsx
+++ frontend/src/components/error/Error.tsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 import styles from './Error.module.scss'
 import appLogo from 'assets/img/brand/app-logo.svg'
+import { TextButton } from 'components/common'
+import { useHistory } from 'react-router-dom'
+
 const Error = () => {
+  const history = useHistory()
+
   return (
     <React.Fragment>
       <div className={styles.topContainer}>
@@ -16,6 +21,20 @@ const Error = () => {
               ></img>
             </a>
             <h1>Page Not Found!</h1>
+            <p>
+              The link you followed may be broken, or the page may not exist.
+            </p>
+            <span className={styles.actions}>
+              <TextButton className={styles.action} onClick={history.goBack}>
+                Go back to previous page
+              </TextButton>
+              <TextButton
+                className={styles.action}
+                onClick={() => history.push('/campaigns')}
+              >
+                Go to dashboard
+              </TextButton>
+            </span>
           </div>
         </div>
       </div>
