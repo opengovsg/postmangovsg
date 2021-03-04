a43e479d69246b6a5feb901bee4b405c18857ef8
Zhao Wei Liew <zhaoweiliew@gmail.com>
2021-03-04 14:26:17 +0800

    refactor(twilio-client): Rewrite send() as an async function

--- backend/src/sms/services/twilio-client.class.ts
+++ backend/src/sms/services/twilio-client.class.ts
@@ -11,40 +11,43 @@ export default class TwilioClient {
     this.messagingServiceSid = messagingServiceSid
   }
 
-  public send(
+  async send(
     recipient: string,
     message: string,
     forceDelivery = false
   ): Promise<string | void> {
-    return this.client.messages
-      .create({
+    try {
+      const {
+        status,
+        sid,
+        error_code: errorCode,
+        code,
+      } = await this.client.messages.create({
         to: recipient,
         body: this.replaceNewLines(message),
         from: this.messagingServiceSid,
-        forceDelivery: forceDelivery,
+        forceDelivery,
       })
-      .then((result: { [key: string]: string }) => {
-        const { status, sid, error_code: errorCode, code } = result
-        if (sid) {
-          if (errorCode || code) {
-            return Promise.reject(new Error(`${sid};${errorCode};${code}`))
-          } else {
-            return sid
-          }
-        } else {
-          return Promise.reject(new Error(`${status};Unknown error`))
-        }
-      })
-      .catch((error: Error) => {
-        // Handle +65802 errors by forcing delivery once
-        if (
-          /\+65802\d+ is not a valid phone number/.test(error.message) &&
-          !forceDelivery
-        ) {
-          return this.send(recipient, message, true)
-        }
-        return Promise.reject(new Error(error.message))
-      })
+
+      if (!sid) {
+        throw new Error(`${status};Unknown error`)
+      }
+
+      if (errorCode || code) {
+        throw new Error(`${sid};${errorCode};${code}`)
+      }
+
+      return sid
+    } catch (error) {
+      // Handle +65802 errors by forcing delivery once
+      if (
+        /\+65802\d+ is not a valid phone number/.test(error.message) &&
+        !forceDelivery
+      ) {
+        return this.send(recipient, message, true)
+      }
+      throw error
+    }
   }
 
   /**
