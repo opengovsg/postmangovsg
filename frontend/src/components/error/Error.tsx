69974617257cf71f71b332cfaf80821b5fa6f95e
Zhao Wei Liew <zhaoweiliew@gmail.com>
2021-03-09 16:32:33 +0800

    Remove graphic and change TextButtons to PrimaryButtons

--- frontend/src/components/error/Error.tsx
+++ frontend/src/components/error/Error.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
 
 import styles from './Error.module.scss'
-import appLogo from 'assets/img/brand/app-logo.svg'
-import { TextButton } from 'components/common'
+import { PrimaryButton } from 'components/common'
 import { useHistory } from 'react-router-dom'
 
 const Error = () => {
@@ -13,27 +12,20 @@ const Error = () => {
       <div className={styles.topContainer}>
         <div className={styles.innerContainer}>
           <div className={styles.textContainer}>
-            <a href="/">
-              <img
-                className={styles.appLogo}
-                src={appLogo}
-                alt="Postman logo"
-              ></img>
-            </a>
             <h1>Page Not Found!</h1>
             <p>
               The link you followed may be broken, or the page may not exist.
             </p>
             <span className={styles.actions}>
-              <TextButton className={styles.action} onClick={history.goBack}>
+              <PrimaryButton className={styles.action} onClick={history.goBack}>
                 Go back to previous page
-              </TextButton>
-              <TextButton
+              </PrimaryButton>
+              <PrimaryButton
                 className={styles.action}
                 onClick={() => history.push('/campaigns')}
               >
                 Go to dashboard
-              </TextButton>
+              </PrimaryButton>
             </span>
           </div>
         </div>
