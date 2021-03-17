cc05b4d2bdae793609b08fd701a5f1ae5a47b57d
Zhao Wei Liew <zhaoweiliew@gmail.com>
2021-03-17 12:07:07 +0800

    fix: adjust layout to match the unsubscribe page

--- frontend/src/components/error/Error.tsx
+++ frontend/src/components/error/Error.tsx
@@ -12,20 +12,20 @@ const Error = () => {
       <div className={styles.topContainer}>
         <div className={styles.innerContainer}>
           <div className={styles.textContainer}>
-            <h1>Page Not Found!</h1>
-            <p>
+            <h2 className={styles.title}>Page Not Found!</h2>
+            <p className={styles.description}>
               The link you followed may be broken, or the page may not exist.
             </p>
             <span className={styles.actions}>
+              <PrimaryButton className={styles.action} onClick={history.goBack}>
+                Go back to previous page
+              </PrimaryButton>
               <TextButton
                 className={styles.action}
                 onClick={() => history.push('/campaigns')}
               >
                 Go to dashboard
               </TextButton>
-              <PrimaryButton className={styles.action} onClick={history.goBack}>
-                Go back to previous page
-              </PrimaryButton>
             </span>
           </div>
         </div>
