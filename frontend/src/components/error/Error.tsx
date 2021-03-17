32dd51f05b29b40293a0f0ae4c354a14d996d062
Zhao Wei Liew <zhaoweiliew@gmail.com>
2021-03-17 14:05:18 +0800

    chore: remove redundant Fragment

--- frontend/src/components/error/Error.tsx
+++ frontend/src/components/error/Error.tsx
@@ -8,29 +8,27 @@ const Error = () => {
   const history = useHistory()
 
   return (
-    <React.Fragment>
-      <div className={styles.topContainer}>
-        <div className={styles.innerContainer}>
-          <div className={styles.textContainer}>
-            <h2 className={styles.title}>Page Not Found!</h2>
-            <p className={styles.description}>
-              The link you followed may be broken, or the page may not exist.
-            </p>
-            <span className={styles.actions}>
-              <PrimaryButton className={styles.action} onClick={history.goBack}>
-                Go back to previous page
-              </PrimaryButton>
-              <TextButton
-                className={styles.action}
-                onClick={() => history.push('/campaigns')}
-              >
-                Go to dashboard
-              </TextButton>
-            </span>
-          </div>
+    <div className={styles.topContainer}>
+      <div className={styles.innerContainer}>
+        <div className={styles.textContainer}>
+          <h2 className={styles.title}>Page Not Found!</h2>
+          <p className={styles.description}>
+            The link you followed may be broken, or the page may not exist.
+          </p>
+          <span className={styles.actions}>
+            <PrimaryButton className={styles.action} onClick={history.goBack}>
+              Go back to previous page
+            </PrimaryButton>
+            <TextButton
+              className={styles.action}
+              onClick={() => history.push('/campaigns')}
+            >
+              Go to dashboard
+            </TextButton>
+          </span>
         </div>
       </div>
-    </React.Fragment>
+    </div>
   )
 }
 
