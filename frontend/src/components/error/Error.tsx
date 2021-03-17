bfc49694fcc32e8344fefc4e116e2c78faf586d4
Zhao Wei Liew <zhaoweiliew@gmail.com>
2021-03-17 11:56:49 +0800

    Convert dashboard button to a text button

--- frontend/src/components/error/Error.tsx
+++ frontend/src/components/error/Error.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import styles from './Error.module.scss'
-import { PrimaryButton } from 'components/common'
+import { PrimaryButton, TextButton } from 'components/common'
 import { useHistory } from 'react-router-dom'
 
 const Error = () => {
@@ -17,14 +17,14 @@ const Error = () => {
               The link you followed may be broken, or the page may not exist.
             </p>
             <span className={styles.actions}>
-              <PrimaryButton className={styles.action} onClick={history.goBack}>
-                Go back to previous page
-              </PrimaryButton>
-              <PrimaryButton
+              <TextButton
                 className={styles.action}
                 onClick={() => history.push('/campaigns')}
               >
                 Go to dashboard
+              </TextButton>
+              <PrimaryButton className={styles.action} onClick={history.goBack}>
+                Go back to previous page
               </PrimaryButton>
             </span>
           </div>
