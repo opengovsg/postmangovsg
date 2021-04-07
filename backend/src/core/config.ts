46c7c752664a6cb651197a16f68e05815e74e007
Lam Kee Wei <keewei.lam@gmail.com>
2021-04-07 16:53:55 +0800

    refactor: change default sender ID to Postman

--- backend/src/core/config.ts
+++ backend/src/core/config.ts
@@ -439,7 +439,7 @@ const config = convict({
     },
     senderId: {
       doc: 'Sender ID to use for all SNS SMS',
-      default: 'GovSg',
+      default: 'Postman',
       env: 'SMS_FALLBACK_SENDER_ID',
     },
   },
