a779181eca3a3fcba043f040766f481b0f19bdf0
Zhao Wei Liew <zhaoweiliew@gmail.com>
2021-04-13 00:14:35 +0800

    fix(moment): disable periodic updates for react-moment components (#1127)
    By default, the Moment component sets a timer on a 60s interval to
    update the time shown. We don't need this feature since all our
    components simply display a static time.
    
    Furthermore, there are instances when periodic updates causes errors
    due to calling `setState` when the component is unmounted.
    
    Since we don't need this functionality, disable it. This should also
    slightly improve efficiency since app will do less work.
    
    Ref: https://github.com/headzoo/react-moment#interval

--- frontend/src/components/dashboard/campaigns/Campaigns.tsx
+++ frontend/src/components/dashboard/campaigns/Campaigns.tsx
@@ -142,7 +142,9 @@ const Campaigns = () => {
     {
       name: 'Created At',
       render: (campaign: Campaign) => (
-        <Moment format="MMM DD YYYY, HH:mm">{campaign.createdAt}</Moment>
+        <Moment format="MMM DD YYYY, HH:mm" interval={0}>
+          {campaign.createdAt}
+        </Moment>
       ),
       width: 'md',
     },
@@ -150,7 +152,9 @@ const Campaigns = () => {
       name: 'Sent At',
       render: (campaign: Campaign) =>
         campaign.sentAt ? (
-          <Moment format="MMM DD YYYY, HH:mm">{campaign.sentAt}</Moment>
+          <Moment format="MMM DD YYYY, HH:mm" interval={0}>
+            {campaign.sentAt}
+          </Moment>
         ) : (
           <span></span>
         ),
