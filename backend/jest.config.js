aaaaa73bdbfd88e7c0a1e16b30acda4332dbbfce
Azima M I <miazima@gmail.com>
2021-05-04 11:05:38 +0800

    chore: setup scaffolding for backend tests (#940)
    * chore: setup scaffolding for backend tests
    
    * chore: add tests for campaign routes
    
    * refactor: rename model mocks
    
    * chore: update tests to use sequelize db test data
    
    * chore: start postgres instance in travis for backend tests
    
    * chore: add more tests to campaign routes test
    
    * chore: update package-lock.json
    
    * chore: add test setup files
    
    * chore: remove redundant node modules
    
    * refactor: change folder structure of tests
    
    * chore: add redis service on travis
    
    * refactor: shift creation of dbs from travis.yml to globalSetup
    
    * fix: create dbs from index 1

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,14 +1,20 @@
 module.exports = {
   roots: ['<rootDir>'],
-  testMatch: ['**/__tests__/**/*.(spec|test).+(ts|tsx|js)'],
+  testMatch: ['**/tests/**/*.(spec|test).+(ts|tsx|js)'],
+  testPathIgnorePatterns: ['<rootDir>/build/', '<rootDir>/node_modules/'],
   moduleNameMapper: {
     '@core/(.*)': '<rootDir>/src/core/$1',
     '@sms/(.*)': '<rootDir>/src/sms/$1',
     '@email/(.*)': '<rootDir>/src/email/$1',
+    '@telegram/(.*)': '<rootDir>/src/telegram/$1',
+    '@test-utils/(.*)': '<rootDir>/src/test-utils/$1',
   },
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
   },
   testEnvironment: 'node',
-  setupFilesAfterEnv: ['<rootDir>/__tests__/setup.js'],
+  globalSetup: '<rootDir>/src/test-utils/global-setup.ts',
+  globalTeardown: '<rootDir>/src/test-utils/global-teardown.ts',
+  setupFiles: ['<rootDir>/src/test-utils/test-env.ts'],
+  setupFilesAfterEnv: ['<rootDir>/src/test-utils/setup.ts'],
 }
