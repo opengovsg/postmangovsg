4d79446286cfb1236d13f098c53231252e5bda55
Zhao Wei Liew <zhaoweiliew@gmail.com>
2021-05-05 21:54:02 +0800

    chore: define redis env vars for test environment

--- backend/src/test-utils/test-env.ts
+++ backend/src/test-utils/test-env.ts
@@ -1,5 +1,7 @@
 process.env.REDIS_OTP_URI = 'redis://localhost:6379/3'
 process.env.REDIS_SESSION_URI = 'redis://localhost:6379/4'
+process.env.REDIS_RATE_LIMIT_URI = 'redis://localhost:6379/5'
+process.env.REDIS_CREDENTIAL_URI = 'redis://localhost:6379/6'
 process.env.SENDGRID_PUBLIC_KEY =
   'MFYwEAYHKoZIzj0CAQYFK4EEAAoDQgAEKWFCI/58CSJe4uz9WX7VZZBIoeb3c1UEJ+pe3HL0ywyGA6c3Bq92+1YVKv0HHxf5mjm+t47P672gcaYarlp2LA=='
 process.env.SESSION_SECRET = 'SESSIONSECRET'
