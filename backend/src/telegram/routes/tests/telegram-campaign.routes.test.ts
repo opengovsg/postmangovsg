10a5c50813a555d55362d9cb4a412c81000ae2b4
Gui Ming Jiang <mjgui2000@hotmail.com>
2021-05-27 19:18:57 +0800

    test: add tests for telegram /new-credentials

--- backend/src/telegram/routes/tests/telegram-campaign.routes.test.ts
+++ backend/src/telegram/routes/tests/telegram-campaign.routes.test.ts
@@ -127,6 +127,100 @@ describe('POST /campaign/{campaignId}/telegram/credentials', () => {
   })
 })
 
+describe('POST /campaign/{campaignId}/telegram/new-credentials', () => {
+  beforeAll(async () => {
+    // Mock telegram to always accept credential
+    mockTelegram.setWebhook.mockResolvedValue(true)
+    mockTelegram.setMyCommands.mockResolvedValue(true)
+  })
+
+  afterAll(async () => {
+    mockTelegram.setWebhook.mockReset()
+    mockTelegram.setMyCommands.mockReset()
+  })
+
+  afterEach(async () => {
+    // Reset number of calls for mocked functions
+    jest.clearAllMocks()
+  })
+
+  test('Demo Campaign should not be able to create custom credential', async () => {
+    const demoCampaign = await createCampaign({ isDemo: true })
+
+    const FAKE_API_TOKEN = 'Some API Token'
+
+    const res = await request(app)
+      .post(`/campaign/${demoCampaign.id}/telegram/new-credentials`)
+      .send({
+        telegram_bot_token: FAKE_API_TOKEN,
+      })
+
+    expect(res.status).toBe(400)
+    expect(res.body).toEqual({
+      message: `Action disabled for demo campaign`,
+    })
+
+    expect(mockSecretsManager.createSecret).not.toHaveBeenCalled()
+  })
+
+  test('User should not be able to add custom credential using invalid Telegram API key', async () => {
+    const nonDemoCampaign = await createCampaign({ isDemo: false })
+
+    const INVALID_API_TOKEN = 'Some Invalid API Token'
+
+    // Mock Telegram API to return 404 error (invalid token)
+    const TELEGRAM_ERROR_STRING = '404: Not Found'
+    mockTelegram.getMe.mockRejectedValue(new Error(TELEGRAM_ERROR_STRING))
+
+    const res = await request(app)
+      .post(`/campaign/${nonDemoCampaign.id}/telegram/new-credentials`)
+      .send({
+        telegram_bot_token: INVALID_API_TOKEN,
+      })
+
+    expect(res.status).toBe(400)
+    expect(res.body).toEqual({
+      message: `Error: Invalid token. ${TELEGRAM_ERROR_STRING}`,
+    })
+
+    expect(mockSecretsManager.createSecret).not.toHaveBeenCalled()
+    mockTelegram.getMe.mockReset()
+  })
+
+  test('User should be able to add custom credential using valid Telegram API key', async () => {
+    const nonDemoCampaign = await createCampaign({ isDemo: false })
+
+    const VALID_API_TOKEN = '12345:Some Valid API Token'
+
+    // Mock Telegram API to return a bot with user id 12345
+    mockTelegram.getMe.mockResolvedValue({ id: 12345 })
+
+    const res = await request(app)
+      .post(`/campaign/${nonDemoCampaign.id}/telegram/new-credentials`)
+      .send({
+        telegram_bot_token: VALID_API_TOKEN,
+      })
+
+    expect(res.status).toBe(200)
+
+    expect(mockSecretsManager.createSecret).toHaveBeenCalledWith(
+      expect.objectContaining({
+        Name: '12345',
+        SecretString: VALID_API_TOKEN,
+      })
+    )
+
+    // Ensure credential was added into DB
+    const dbCredential = await Credential.findOne({
+      where: {
+        name: '12345',
+      },
+    })
+    expect(dbCredential).not.toBe(null)
+    mockTelegram.getMe.mockReset()
+  })
+})
+
 describe('PUT /campaign/{campaignId}/telegram/template', () => {
   test('Template with only invalid HTML tags is not accepted', async () => {
     const testBody = await request(app)
