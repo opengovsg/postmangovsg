204bb5c52c3f46adfde26d268f7edc15811dadd7
Lam Kee Wei <keewei.lam@gmail.com>
2021-05-31 19:32:11 +0800

    feat(frontend): add support for badges in TextInput

--- frontend/src/components/common/text-input/TextInput.tsx
+++ frontend/src/components/common/text-input/TextInput.tsx
@@ -6,15 +6,19 @@ import type { ReactNode, ChangeEvent } from 'react'
 import styles from './TextInput.module.scss'
 
 const TextInput = forwardRef((props: any, ref: ReactNode) => {
-  const { onChange, className, ...otherProps } = props
+  const { onChange, className, badge, ...otherProps } = props
 
   return (
-    <input
-      ref={ref}
-      className={cx(styles.textInput, className)}
-      onChange={(e: ChangeEvent<HTMLInputElement>) => onChange(e.target.value)}
-      {...otherProps}
-    />
+    <div className={cx(styles.textInput, className)}>
+      <input
+        ref={ref}
+        onChange={(e: ChangeEvent<HTMLInputElement>) =>
+          onChange(e.target.value)
+        }
+        {...otherProps}
+      />
+      {badge && <span className={styles.badge}>{badge}</span>}
+    </div>
   )
 })
 
