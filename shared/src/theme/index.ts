1844e62276f1511f8dc4e7fe8f43bb4a54b023bd
Gui Ming Jiang <mjgui2000@hotmail.com>
2021-06-08 13:39:43 +0800

    feat: add preheader field to template

--- shared/src/theme/index.ts
+++ shared/src/theme/index.ts
@@ -1,4 +1,5 @@
 import mustache from 'mustache'
+import xss from 'xss'
 import fs from 'fs'
 import path from 'path'
 
@@ -12,7 +13,26 @@ const generateThemedHTMLEmail = async (emailThemeFields: {
     path.resolve(__dirname, './email-theme.mustache'),
     'utf-8'
   )
-  return mustache.render(htmlTemplate, emailThemeFields)
+
+  // Take first 200 characters of the body (without html tags) to use as preheader (preview text of email content)
+  // Append whitespace after closing tags so paragraphs are spaced out in preheader
+  const { body } = emailThemeFields
+  const preheader = xss
+    .filterXSS(body, {
+      whiteList: {},                  // whitelist for tags is empty, i.e. strip all HTML tags 
+      stripIgnoreTagBody: ['script'], // remove contents of script tags entirely
+      onIgnoreTag: (_tag, _html, options) => {
+        // @ts-ignore types are missing, upgrade to js-xss 1.0.9 which fixes missing types
+        if(options.isClosing) return ' '
+        else return ''
+      }
+    })
+    .substring(0,200)
+
+  return mustache.render(htmlTemplate, {
+    ...emailThemeFields,
+    preheader,
+  })
 }
 
 export {
