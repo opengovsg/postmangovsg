c219df5f1c127a39b65413ba4a25fb28d7015177
Azima M I <miazima@gmail.com>
2021-06-09 11:45:15 +0800

    Improvements to decrease memory usage in verify backup service (#1215)
    * chore: improvements to decrease memory usage
    
    * revert unintended changes to file

--- serverless/verify-backup/scripts/run.sh
+++ serverless/verify-backup/scripts/run.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-# Initialise and start postgres
+# Initialise postgres
 mkdir -p /run/postgresql/
 chown -R postgres:postgres /run/postgresql/
 su - postgres -c "initdb /var/lib/postgresql/data"
-su - postgres -c "pg_ctl start -D /var/lib/postgresql/data -l /var/lib/postgresql/log.log"
 
 # Start server to listen for pubsub messages
 npm run start
