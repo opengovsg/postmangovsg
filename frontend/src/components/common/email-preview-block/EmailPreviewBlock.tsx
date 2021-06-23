ec49e252c2a628b774386badb57243d73aef2095
mjgui <mjgui2000@hotmail.com>
2021-06-23 19:05:48 +0800

    Email template (#1242)
    * feat: implement themed email generator
    implement html theme in mustache
    
    * feat: add copy-assets script to shared
    trigger shared postbuild manually within backend and worker dockerfile
    
    * feat: update sample templated mail to use theme
    
    * feat: update worker to use themed email
    
    * feat: show email theme preview on frontend
    
    * feat: update rich text editor to use email theme styles
    
    * fix: allow top margin to collapse in email theme
    
    * fix: use test unsub url instead of empty string
    
    * chore: upgrade js-xss to 1.0.9
    
    * fix: remove unneeded !important directives
    
    * fix: edit h4 font weight to 500
    
    * fix: reset site-wide table styles for preview
    
    * refactor: move shared email styles to new file
    
    * style: run eslint on shared folder
    
    * style: inline typedef for msg with sender email
    
    * feat: add new sql func to get messages w sender
    
    * fix(frontend): update field descriptions
    
    * fix: remove unused fallback for body preview
    
    * feat: add extra spacing to preheader
    
    * fix: prefer helvetica over arial for email theme
    
    * fix: remove dark mode meta tags
    
    * fix: update frontend mock apis to add themedBody
    
    Co-authored-by: Lam Kee Wei <keewei.lam@gmail.com>

--- frontend/src/components/common/email-preview-block/EmailPreviewBlock.tsx
+++ frontend/src/components/common/email-preview-block/EmailPreviewBlock.tsx
@@ -2,12 +2,12 @@ import cx from 'classnames'
 import type { FC } from 'react'
 
 import DetailBlock from '../detail-block'
-import RichTextEditor from '../rich-text-editor'
 
 import styles from './EmailPreviewBlock.module.scss'
 
 interface EmailPreviewBlockProps {
   body: string
+  themedBody?: string
   subject?: string
   replyTo?: string | null
   from?: string
@@ -16,11 +16,11 @@ interface EmailPreviewBlockProps {
 
 const EmailPreviewBlock: FC<EmailPreviewBlockProps> = ({
   body,
+  themedBody,
   subject,
   replyTo,
   from,
   className,
-  ...otherProps
 }) => {
   if (!body && !subject) {
     return (
@@ -34,19 +34,23 @@ const EmailPreviewBlock: FC<EmailPreviewBlockProps> = ({
   }
 
   return (
-    <div className={cx(styles.preview, className)}>
-      <h5>From</h5>
-      <p>{from}</p>
+    <>
+      <div className={cx(styles.previewInfo, className)}>
+        <h5>From</h5>
+        <p>{from}</p>
 
-      <h5>Subject</h5>
-      <p>{subject}</p>
+        <h5>Subject</h5>
+        <p>{subject}</p>
 
-      <h5>Body</h5>
-      <RichTextEditor value={body} preview {...otherProps} />
+        <h5>Replies</h5>
+        <p>{replyTo}</p>
+      </div>
 
-      <h5>Replies</h5>
-      <p>{replyTo}</p>
-    </div>
+      <div
+        className={styles.previewBody}
+        dangerouslySetInnerHTML={{ __html: themedBody ?? '' }}
+      ></div>
+    </>
   )
 }
 
