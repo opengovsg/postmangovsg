2572f41e670a3b57bce5ff88edcd17f29d26a857
jeantanzj <33819199+jeantanzj@users.noreply.github.com>
2021-07-01 14:44:33 +0800

    chore: backfill error_sub_type

---
+++ backend/src/database/migrations/20210701054329-add-email-error-subtype.js
@@ -0,0 +1,45 @@
+'use strict'
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.addColumn(
+      'email_messages', // name of Source model
+      'error_sub_type', // name of column we're adding
+      {
+        type: Sequelize.STRING(255),
+        allowNull: true,
+      }
+    )
+    await queryInterface.addColumn(
+      'email_ops', // name of Source model
+      'error_sub_type', // name of column we're adding
+      {
+        type: Sequelize.STRING(255),
+        allowNull: true,
+      }
+    )
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.removeColumn(
+      'email_messages', // name of Source Model
+      'error_sub_type' // name of column we want to remove
+    )
+    await queryInterface.removeColumn(
+      'email_ops', // name of Source Model
+      'error_sub_type' // name of column we want to remove
+    )
+  },
+}
