bf9f4ee3996702af13c20f030568c082647f2601
Lam Kee Wei <keewei.lam@gmail.com>
2021-08-12 12:42:45 +0800

    refactor: remove redundant infoblock for send test email

--- frontend/src/components/dashboard/create/email/EmailCredentials.tsx
+++ frontend/src/components/dashboard/create/email/EmailCredentials.tsx
@@ -5,15 +5,7 @@ import { useParams } from 'react-router-dom'
 import EmailValidationInput from './EmailValidationInput'
 
 import { EmailProgress } from 'classes'
-import {
-  NextButton,
-  DetailBlock,
-  ErrorBlock,
-  ButtonGroup,
-  TextButton,
-  StepHeader,
-  StepSection,
-} from 'components/common'
+import { ErrorBlock, StepHeader } from 'components/common'
 import { CampaignContext } from 'contexts/campaign.context'
 
 import { sendPreviewMessage } from 'services/email.service'
@@ -24,8 +16,7 @@ const EmailCredentials = ({
   setActiveStep: Dispatch<SetStateAction<EmailProgress>>
 }) => {
   const { campaign, updateCampaign } = useContext(CampaignContext)
-  const { hasCredential: initialHasCredential, protect } = campaign
-  const [hasCredential, setHasCredential] = useState(initialHasCredential)
+  const { protect } = campaign
   const [errorMsg, setErrorMsg] = useState(null)
   const { id: campaignId } = useParams<{ id: string }>()
 
@@ -39,9 +30,9 @@ const EmailCredentials = ({
         campaignId: +campaignId,
         recipient,
       })
-      setHasCredential(true)
       // Saves hasCredential property but do not advance to next step
       updateCampaign({ hasCredential: true })
+      setActiveStep((s) => s + 1)
     } catch (err) {
       setErrorMsg(err.message)
     }
@@ -51,52 +42,28 @@ const EmailCredentials = ({
     <>
       {
         <>
-          <StepSection>
-            <StepHeader title="Send a test email" subtitle="Step 3">
+          <StepHeader title="Send a test email" subtitle="Step 3">
+            <p>
+              <label htmlFor="validateEmail">
+                You can preview your message by sending an email to yourself.
+              </label>
+            </p>
+            {protect && (
               <p>
                 <label htmlFor="validateEmail">
-                  You can preview your message by sending an email to yourself.
+                  You will receive an email from postman.gov.sg showing the
+                  email that the recipient would receive once you click send
+                  campaign. You can click on the unique link and unlock the
+                  password protected page using the corresponding recipient
+                  password in your uploaded csv.
                 </label>
               </p>
-              {protect && (
-                <p>
-                  <label htmlFor="validateEmail">
-                    You will receive an email from postman.gov.sg showing the
-                    email that the recipient would receive once you click send
-                    campaign. You can click on the unique link and unlock the
-                    password protected page using the corresponding recipient
-                    password in your uploaded csv.
-                  </label>
-                </p>
-              )}
-            </StepHeader>
-            <div>
-              <EmailValidationInput onClick={handleTestSend} />
-            </div>
-            <ErrorBlock>{errorMsg}</ErrorBlock>
-
-            {hasCredential && (
-              <DetailBlock>
-                <li>
-                  <i className="bx bx-check-circle"></i>
-                  <span>
-                    Email credentials have been validated but you may continue
-                    to send test messages.
-                  </span>
-                </li>
-              </DetailBlock>
             )}
-          </StepSection>
-
-          <ButtonGroup>
-            <NextButton
-              disabled={!hasCredential}
-              onClick={() => setActiveStep((s) => s + 1)}
-            />
-            <TextButton onClick={() => setActiveStep((s) => s - 1)}>
-              Previous
-            </TextButton>
-          </ButtonGroup>
+          </StepHeader>
+          <div>
+            <EmailValidationInput onClick={handleTestSend} />
+          </div>
+          <ErrorBlock>{errorMsg}</ErrorBlock>
         </>
       }
     </>
