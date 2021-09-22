1405caeb616669266624f96db9c16c1303711ab6
jeantanzj <33819199+jeantanzj@users.noreply.github.com>
2021-09-22 09:44:36 +0800

    Add sequelize retry (#1329)
    * feat: add sequelize retry
    
    * fix: remove unsupported backoffBase property

--- worker/src/core/loaders/message-worker/index.ts
+++ worker/src/core/loaders/message-worker/index.ts
@@ -136,6 +136,20 @@ const createConnection = (): Sequelize => {
     logging: false,
     pool: config.get('database.poolOptions'),
     dialectOptions,
+    retry: {
+      max: 5,
+      match: [
+        /ConnectionError/,
+        /SequelizeConnectionError/,
+        /SequelizeConnectionRefusedError/,
+        /SequelizeHostNotFoundError/,
+        /SequelizeHostNotReachableError/,
+        /SequelizeInvalidConnectionError/,
+        /SequelizeConnectionTimedOutError/,
+        /SequelizeConnectionAcquireTimeoutError/,
+        /Connection terminated unexpectedly/,
+      ],
+    },
     hooks: {
       beforeConnect: async (dbConfig: MutableConfig): Promise<void> => {
         if (config.get('database.useIam')) {
