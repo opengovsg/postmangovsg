ba70bd3268978b2ac0cbadff12f7384e556fce9d
Rae Wong <yingyee0111@gmail.com>
2022-07-27 17:58:56 +0800

    fix: update swagger for transactional/email/send

--- backend/src/email/routes/email-transactional.routes.ts
+++ backend/src/email/routes/email-transactional.routes.ts
@@ -55,23 +55,103 @@ export const InitEmailTransactionalRoute = (
    *                 body:
    *                   type: string
    *                 recipient:
-   *                   type: string
+   *                   type: email
    *                 from:
-   *                   type: string
+   *                   type: custom domain
    *                 reply_to:
-   *                   type: string
+   *                   type: email
    *                   nullable: true
    *       responses:
    *         "202":
    *           description: Accepted. The message is being sent.
+   *           content:
+   *              text/plain:
+   *                type: string
+   *                example: Accepted
    *         "400":
    *           description: Bad Request
+   *           content:
+   *              text/plain:
+   *                type: string
+   *                examples:
+   *                  Blacklist:
+   *                    value: Recipient email is blacklisted
+   *                  InvalidMessage:
+   *                    value: Message is invalid as the subject or body only contains invalid HTML tags.
+   *              application/json:
+   *                schema:
+   *                  oneOf:
+   *                    - $ref: '#/components/schemas/ValidationError'
+   *                    - $ref: '#/components/schemas/FromError'
+   *
    *         "401":
-   *           description: Unauthorized
+   *           description: Unauthorized.
+   *           content:
+   *              text/plain:
+   *                type: string
+   *                example: Unauthorized
    *         "429":
-   *           description: Too many requests. Please try again later.
+   *           description: Rate limit exceeded
+   *           content:
+   *              text/plain:
+   *                type: string
+   *                example: Too many requests, please try again later.
+   *              application/json:
+   *                schema:
+   *                  $ref: '#/components/schemas/RateLimit'
    *         "500":
-   *           description: Internal Server Error
+   *           description: Internal Server Error (Eg. custom domain passed email validation but is incorrect)
+   *           content:
+   *              text/plain:
+   *                type: string
+   *                example: Internal Server Error
+   *
+   * components:
+   *  schemas:
+   *    RateLimit:
+   *      description: rate limit exceeded
+   *      type: object
+   *      properties:
+   *        status:
+   *          type: integer
+   *          example: 429
+   *        message:
+   *          type: string
+   *          example: Too many requests. Please try again later.
+   *
+   *    ValidationError:
+   *      description: any one of the params failed validation
+   *      type: object
+   *      properties:
+   *        statusCode:
+   *          type: integer
+   *          example: 400
+   *        error:
+   *          type: string
+   *          example: Bad Request
+   *        message:
+   *          type: string
+   *          example: |-
+   *            "from" must be a valid email
+   *        validation:
+   *          type: object
+   *          properties:
+   *            source:
+   *              type: string
+   *              example: body
+   *            keys:
+   *              type: array
+   *              items:
+   *                type: string
+   *                example: from
+   *
+   *    FromError:
+   *      description: from address passed email validation but is neither the users email nor the default app email
+   *      type: object
+   *      properties:
+   *        message:
+   *          type: string
+   *          example: Invalid 'from' email address.
    */
   router.use(
     '/send',
