699b2f934a6d9cec69b89d252730e75c88e9904f
Alexis <gweiying@gmail.com>
2022-08-01 15:44:52 +0200

    chore: move file extension checker into separate service

--- backend/src/core/services/file-attachment.service.ts
+++ backend/src/core/services/file-attachment.service.ts
@@ -3,8 +3,8 @@ import config from '@core/config'
 import { Promise as BluebirdPromise } from 'bluebird'
 import _ from 'lodash'
 import { MailAttachment } from '@shared/clients/mail-client.class'
-import FileType from 'file-type'
 import { MaliciousFileError, UnsupportedFileTypeError } from '@core/errors'
+import { FileExtensionService } from '@core/services'
 
 if (!config.get('file.cloudmersiveKey')) {
   throw new Error('fileScanner: cloudmersiveKey not found')
@@ -12,23 +12,11 @@ if (!config.get('file.cloudmersiveKey')) {
 
 const client = new CloudmersiveClient(config.get('file.cloudmersiveKey'))
 
-const hasAllowedType = async (file: {
-  data: Buffer
-  name: string
-}): Promise<boolean> => {
-  const { data, name } = file
-  const allowedFileExtensions = config.get('file.supportedExtensions')
-
-  const fileType = await FileType.fromBuffer(data)
-  const extension = fileType?.ext || `${`${name}`.split('.').pop()}`
-  return allowedFileExtensions.includes(extension)
-}
-
-const checkType = async (
+const checkExtensions = async (
   files: { data: Buffer; name: string }[]
 ): Promise<boolean> => {
   const isAllowed = await BluebirdPromise.map(files, (file) =>
-    hasAllowedType(file)
+    FileExtensionService.hasAllowedExtensions(file)
   )
   return _.every(isAllowed)
 }
@@ -54,7 +42,7 @@ const parseFiles = async (
 const sanitizeFiles = async (
   files: { data: Buffer; name: string }[]
 ): Promise<MailAttachment[]> => {
-  const isAcceptedType = await checkType(files)
+  const isAcceptedType = await checkExtensions(files)
   if (!isAcceptedType) {
     throw new UnsupportedFileTypeError()
   }
