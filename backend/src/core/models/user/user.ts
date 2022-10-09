d8126616c5a3cc390f3958f3ec11dfbd9b721325
zx <67887489+tan-zx@users.noreply.github.com>
2022-10-09 16:42:40 +0800

    refactor: remove comment from user.ts

--- backend/src/core/models/user/user.ts
+++ backend/src/core/models/user/user.ts
@@ -37,8 +37,6 @@ export class User extends Model<User> {
   })
   email!: string
 
-  // to be accurate, this should be api_key_ash
-  // should I rename this for clarity?
   @Column(DataType.STRING)
   apiKey?: string
 
