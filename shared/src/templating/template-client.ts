473686c68529bf699cd03530cc7e44ff3b072796
Richard Yak <74128885+richard-yak@users.noreply.github.com>
2022-11-08 11:03:30 +0800

    feat(backend): rendering hyperlinks (#1863)
    * feat(backend): added url templating
    
    * feat(backend): use html instead of md
    
    * feat(backend): Added test cases for new templating rules

--- shared/src/templating/template-client.ts
+++ shared/src/templating/template-client.ts
@@ -1,6 +1,6 @@
 import cheerio from 'cheerio'
 import { mapKeys } from 'lodash'
-import { IFilterXSSOptions, filterXSS } from 'xss'
+import { filterXSS, IFilterXSSOptions } from 'xss'
 import { TemplateError } from './errors'
 import { TemplatingConfig, TemplatingConfigDefault } from './interfaces'
 import { filterImageSources } from './xss-options'
@@ -94,7 +94,17 @@ export class TemplateClient {
           if (!params) continue
 
           if (dict[key]) {
-            const templated = dict[key]
+            let templated = dict[key]
+            // if there is the string "url" or "link" in the provided template key
+            // transform the template key into hyperlink
+            if (key.endsWith('_url') || key.endsWith('_link')) {
+              templated =
+                '<a href="' +
+                templated +
+                '" target="_blank">' +
+                templated +
+                '</a>'
+            }
             tokens.push(templated)
             continue
           }
