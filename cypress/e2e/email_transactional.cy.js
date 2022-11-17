c8dce8b47af0f24186eed4f28217cee2ad302880
Stanley Nguyen <hung.ngn.the@gmail.com>
2022-11-17 12:17:35 +0800

    test(cypress): add env vars for api tests

--- cypress/e2e/email_transactional.cy.js
+++ cypress/e2e/email_transactional.cy.js
@@ -16,49 +16,36 @@ describe('Transactional Email Test', () => {
     const apiUrl = Cypress.env('API_BASE_URL');
     const apiKey = Cypress.env('API_KEY');
 
-    let messageId;
+    const requestUrl = new URL(apiUrl);
+    requestUrl.pathname = '/v1/transactional/email/send';
 
-    // /transaction/email/send
-    {
-      const requestUrl = new URL(apiUrl);
-      requestUrl.pathname = '/v1/transactional/email/send';
+    // Note: Can't use txt/plain or txt/html with gmail-tester until this is fixed
+    // https://github.com/levz0r/gmail-tester/pull/110
+    // const attachmentFilename = 'example_attachment.txt';
+    // cy.writeFile(
+    //   `cypress/fixtures/${attachmentFilename}`,
+    //   'example content',
+    // );
+    const attachmentFilename = 'example.pdf';
+    const exampleAttachment = await cy.fixture(attachmentFilename, 'binary');
+    const blob = Cypress.Blob.binaryStringToBlob(exampleAttachment, 'text/pdf');
+    const formData = new FormData();
+    formData.append('attachments', blob, attachmentFilename);
+    formData.append('subject', messageSubject);
+    formData.append('body', messageContent);
+    formData.append('recipient', recipient);
 
-      // Note: Can't use txt/plain or txt/html with gmail-tester until this is fixed
-      // https://github.com/levz0r/gmail-tester/pull/110
-      // const attachmentFilename = 'example_attachment.txt';
-      // cy.writeFile(
-      //   `cypress/fixtures/${attachmentFilename}`,
-      //   'example content',
-      // );
-      const attachmentFilename = 'example.pdf';
-      const exampleAttachment = await cy.fixture(attachmentFilename, 'binary');
-      const blob = Cypress.Blob.binaryStringToBlob(
-        exampleAttachment,
-        'text/pdf',
-      );
-      const formData = new FormData();
-      formData.append('attachments', blob, attachmentFilename);
-      formData.append('subject', messageSubject);
-      formData.append('body', messageContent);
-      formData.append('recipient', recipient);
-
-      // This request is being sent from the browser instance, it's working because
-      // we have CORs settings to allow our frontend URLs to send requests to APIs
-      // Note: Somehow cy.task is not a true Promise so need to wrap it around with
-      // a Promise constructor for it to work
-      const response = await new Promise((r) =>
-        cy
-          .request({
-            method: 'POST',
-            url: requestUrl.toString(),
-            body: formData,
-            headers: {
-              Authorization: `Bearer ${apiKey}`,
-              'Content-Type': 'multipart/form-data',
-            },
-          })
-          .then(r),
-      );
+    // This request is being sent from the browser instance, it's working because
+    // we have CORs settings to allow our frontend URLs to send requests to APIs
+    cy.request({
+      method: 'POST',
+      url: requestUrl.toString(),
+      body: formData,
+      headers: {
+        Authorization: `Bearer ${apiKey}`,
+        'Content-Type': 'multipart/form-data',
+      },
+    }).then((response) => {
       assert.equal(
         response.status,
         201,
@@ -67,67 +54,52 @@ describe('Transactional Email Test', () => {
 
       // Somehow if we send requests with formData body the response body won't
       // be automatically parsed as a JSON object
-      const { id } = JSON.parse(
+      const { id: messageId } = JSON.parse(
         String.fromCharCode.apply(null, new Uint8Array(response.body)),
       );
-      messageId = id;
 
-      // Note: Somehow cy.task is not a true Promise so need to wrap it around with
-      // a Promise constructor for it to work
-      const email = await new Promise((r) =>
-        cy
-          .task('gmail:check', {
-            from: mailSender,
-            to: recipient,
-            subject: messageSubject,
-          })
-          .then(r),
-      );
-      assert.equal(
-        email.length,
-        1,
-        `Expected 1 matching email in the inbox, got ${email.length}`,
-      );
-    }
+      cy.task('gmail:check', {
+        from: mailSender,
+        to: recipient,
+        subject: messageSubject,
+      }).then((email) => {
+        assert.equal(
+          email.length,
+          1,
+          `Expected 1 matching email in the inbox, got ${email.length}`,
+        );
+        const requestUrl = new URL(apiUrl);
+        requestUrl.pathname = path.join('/v1/transactional/email', messageId);
 
-    // /transaction/email/{emailId}
-    {
-      const requestUrl = new URL(apiUrl);
-      requestUrl.pathname = path.join('/v1/transactional/email', messageId);
-
-      // This request is being sent from the browser instance, it's working because
-      // we have CORs settings to allow our frontend URLs to send requests to APIs
-      // Note: Somehow cy.task is not a true Promise so need to wrap it around with
-      // a Promise constructor for it to work
-      const response = await new Promise((r) =>
-        cy
-          .request({
-            method: 'GET',
-            url: requestUrl.toString(),
-            headers: {
-              Authorization: `Bearer ${apiKey}`,
-              'Content-Type': 'application/json',
-            },
-          })
-          .then(r),
-      );
-      assert.equal(
-        response.status,
-        200,
-        `Expected: 200, Got: ${response.status}`,
-      );
-      const { body } = response;
-      assert.equal(
-        body.id,
-        messageId,
-        `Expected message ID ${messageId}, got ${body.id}`,
-      );
-      assert.isNotNull(body.attachments_metadata);
-      assert.equal(
-        body.attachments_metadata.length,
-        1,
-        `Expected 1 attachment metadata, got ${body.attachments_metadata.length}`,
-      );
-    }
+        // This request is being sent from the browser instance, it's working because
+        // we have CORs settings to allow our frontend URLs to send requests to APIs
+        cy.request({
+          method: 'GET',
+          url: requestUrl.toString(),
+          headers: {
+            Authorization: `Bearer ${apiKey}`,
+            'Content-Type': 'application/json',
+          },
+        }).then((response) => {
+          assert.equal(
+            response.status,
+            200,
+            `Expected: 200, Got: ${response.status}`,
+          );
+          const { body } = response;
+          assert.equal(
+            body.id,
+            messageId,
+            `Expected message ID ${messageId}, got ${body.id}`,
+          );
+          assert.isNotNull(body.attachments_metadata);
+          assert.equal(
+            body.attachments_metadata.length,
+            1,
+            `Expected 1 attachment metadata, got ${body.attachments_metadata.length}`,
+          );
+        });
+      });
+    });
   });
 });
