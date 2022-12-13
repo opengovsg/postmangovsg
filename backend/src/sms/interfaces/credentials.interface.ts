1ed3e978caedf612e850c5d5a44f3902d7ebab15
zx <67887489+tan-zx@users.noreply.github.com>
2022-12-13 16:01:43 +0800

    docs: set up openapi.yaml (#1886)
    * docs: set up openapi.yml and create move campaign routes docs
    
    * docs: make errors go away
    
    * docs: campaign email API
    
    * docs: transactional email API routes
    
    * docs: sms API routes
    
    * docs: telegram API routes
    
    * fix: clean up
    
    * build(deps): remove unused dependencies
    
    * fix: move openapi.yml file to backend and fix Dockerfile
    
    * style: use single quotes
    
    * feat: add Authorize header

--- backend/src/sms/interfaces/credentials.interface.ts
+++ backend/src/sms/interfaces/credentials.interface.ts
@@ -4,19 +4,3 @@ export interface TwilioCredentials {
   apiSecret: string
   messagingServiceSid: string
 }
-/**
- * @swagger
- * components:
- *   schemas:
- *     TwilioCredentials:
- *       type: object
- *       properties:
- *         twilio_account_sid:
- *           type: string
- *         twilio_api_key:
- *           type: string
- *         twilio_api_secret:
- *           type: string
- *         twilio_messaging_service_sid:
- *           type: string
- */
