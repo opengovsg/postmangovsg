1692780743c3440ef9bbca7e89abdf9d5cead628
Stanley Nguyen <hung.ngn.the@gmail.com>
2023-03-14 21:43:28 +0800

    ci(e2e): update e2e workflow to playwright

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -22,7 +22,7 @@ const config: PlaywrightTestConfig = {
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  // globalSetup: require.resolve('./global-setup'),
+  globalSetup: require.resolve('./global-setup'),
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
