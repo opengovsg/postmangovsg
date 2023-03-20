55a8b59a849e27c85f989db7a0120ae28798e69b
Richard Yak <richardyak@open.gov.sg>
2023-03-20 11:48:16 +0800

    chore: removed try catch statement

--- backend/src/core/middlewares/api-key.middleware.ts
+++ backend/src/core/middlewares/api-key.middleware.ts
@@ -1,38 +1,33 @@
 import { loggerWithLabel } from '@core/logger'
-import { NextFunction, Request, Response } from 'express'
+import { Request, Response } from 'express'
 import { ApiKeyService } from '@core/services'
 
 const logger = loggerWithLabel(module)
 
 const deleteApiKey = async (
   req: Request,
-  res: Response,
-  next: NextFunction
+  res: Response
 ): Promise<Response | void> => {
-  try {
-    const { apiKeyId } = req.params
-    const userId = req.session?.user?.id
+  const { apiKeyId } = req.params
+  const userId = req.session?.user?.id
 
-    const deletedRows = await ApiKeyService.deleteApiKey(
-      userId.toString(),
-      +apiKeyId
-    )
-    if (deletedRows <= 0) {
-      return res.status(404).json({
-        code: 'not_found',
-        message: `Could not find API key to delete`,
-      })
-    }
-    logger.info({
-      message: 'Successfully deleted api key',
-      action: 'deleteApiKey',
+  const deletedRows = await ApiKeyService.deleteApiKey(
+    userId.toString(),
+    +apiKeyId
+  )
+  if (deletedRows <= 0) {
+    return res.status(404).json({
+      code: 'not_found',
+      message: `Could not find API key to delete`,
     })
-    return res.status(200).json({
-      api_key_id: apiKeyId,
-    })
-  } catch (err) {
-    return next(err)
   }
+  logger.info({
+    message: 'Successfully deleted api key',
+    action: 'deleteApiKey',
+  })
+  return res.status(200).json({
+    api_key_id: apiKeyId,
+  })
 }
 
 export const ApiKeyMiddleware = {
