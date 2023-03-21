fbb588145e1b333064ba923dcab9e8ea1382d413
Richard Yak <richardyak@open.gov.sg>
2023-03-21 12:00:59 +0800

    feat: added endpoint to get api key list

--- backend/src/core/routes/tests/api-key.routes.test.ts
+++ backend/src/core/routes/tests/api-key.routes.test.ts
@@ -49,3 +49,47 @@ describe('DELETE /api-key/:apiKeyId', () => {
     expect(res.body.api_key_id).toBe('1')
   })
 })
+
+describe('GET /api-key/', () => {
+  test('Attempting to get list without valid cookie', async () => {
+    const res = await request(app).get('/api-key')
+    expect(res.status).toBe(401)
+  })
+  test('Getting api key list when there are no api keys', async () => {
+    await User.create({ id: 1, email: 'user@agency.gov.sg' } as User)
+    const res = await request(appWithUserSession).get('/api-key')
+    expect(res.status).toBe(200)
+    expect(res.body).toHaveLength(0)
+  })
+  test('Getting api key list with a few api keys', async () => {
+    await User.create({ id: 1, email: 'user@agency.gov.sg' } as User)
+    await ApiKey.create({
+      id: 1,
+      userId: '1',
+      hash: 'hash',
+      label: 'label',
+      lastFive: '12345',
+    } as ApiKey)
+    await ApiKey.create({
+      id: 2,
+      userId: '1',
+      hash: 'hash1',
+      label: 'label1',
+      lastFive: '22345',
+    } as ApiKey)
+    await ApiKey.create({
+      id: 3,
+      userId: '1',
+      hash: 'hash2',
+      label: 'label2',
+      lastFive: '32345',
+    } as ApiKey)
+    const res = await request(appWithUserSession).get('/api-key')
+    expect(res.status).toBe(200)
+    expect(res.body).toHaveLength(3)
+    // should be arranged according to what was created most recently
+    expect(res.body[0].id).toBe(3)
+    expect(res.body[1].id).toBe(2)
+    expect(res.body[2].id).toBe(1)
+  })
+})
