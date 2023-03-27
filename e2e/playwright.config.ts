9bd2d61df5f5cfbfb73005be0b5e734c73c43a9e
Stanley Nguyen <hung.ngn.the@gmail.com>
2023-03-27 12:43:01 +0800

    test(e2e): fix scheduled test filling in timing

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -1,9 +1,9 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
 
-require("dotenv").config();
+require('dotenv').config();
 
 const config: PlaywrightTestConfig = {
-  testDir: "./tests",
+  testDir: './tests',
   /* Maximum time one test can run for. */
   timeout: 120 * 1000,
   expect: {
@@ -20,9 +20,9 @@ const config: PlaywrightTestConfig = {
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: "html",
+  reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  globalSetup: require.resolve("./global-setup"),
+  globalSetup: require.resolve('./global-setup'),
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
@@ -30,7 +30,7 @@ const config: PlaywrightTestConfig = {
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on-first-retry",
+    trace: 'on-first-retry',
   },
 };
 
