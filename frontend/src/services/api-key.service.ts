6e516d982f4c0a94816275150285ec29848a4b66
Stanley Nguyen <hung.ngn.the@gmail.com>
2023-04-11 19:31:56 +0800

    feat: display API key expiry date and notification addresses

--- frontend/src/services/api-key.service.ts
+++ frontend/src/services/api-key.service.ts
@@ -5,6 +5,8 @@ export interface ApiKey {
   label: string
   last_five: string
   plain_text_key?: string
+  valid_until: string
+  notification_addresses: string[]
 }
 
 export async function listApiKeys(): Promise<ApiKey[]> {
@@ -14,10 +16,28 @@ export async function listApiKeys(): Promise<ApiKey[]> {
 
 export async function generateApiKey({
   label,
+  notificationAddresses,
 }: {
   label: string
+  notificationAddresses: string[]
 }): Promise<ApiKey> {
-  const { data } = await axios.post('/api-key', { label })
+  const { data } = await axios.post('/api-key', {
+    label,
+    notification_addresses: notificationAddresses,
+  })
+  return data as ApiKey
+}
+
+export async function updateApiKey({
+  id,
+  notificationAddresses,
+}: {
+  id: string
+  notificationAddresses: string[]
+}): Promise<ApiKey> {
+  const { data } = await axios.put(`/api-key/${id}`, {
+    notification_addresses: notificationAddresses,
+  })
   return data as ApiKey
 }
 
