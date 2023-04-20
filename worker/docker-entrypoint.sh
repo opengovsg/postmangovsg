82301a4ba9ebf026f04023254d8fa66260115bef
zx <67887489+tan-zx@users.noreply.github.com>
2023-04-20 15:08:58 +0800

    build(dotenv): move to devDeps and update docker-entrypoint.sh to get rid of .env

--- worker/docker-entrypoint.sh
+++ worker/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Get the secrets from aws secrets manager
 # transform them into KEY=value pairing, and store in temp file
-aws secretsmanager get-secret-value --secret-id '@SECRET_ID' --query SecretString --output text | jq -r 'to_entries|map("\(.key)=\(.value|tostring)")|.[]' > .env
+aws secretsmanager get-secret-value --secret-id '@SECRET_ID' --query SecretString --output text | export "$(jq -r 'to_entries|map("\(.key)=\(.value|tostring)")|.[]')"
 # Rely on dotenv to add the environment variables to the node process
 # Use exec to replace shell process with npm start so that it can properly handle SIGTERM
 
