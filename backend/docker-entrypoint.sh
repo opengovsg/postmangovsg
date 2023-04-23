a6a7adcc3bb83b20c3b21785ef9b9eef4ad09c87
zx <67887489+tan-zx@users.noreply.github.com>
2023-04-23 15:08:07 +0800

    fix: try Stanley's fix

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 # Get the secrets from aws secrets manager
 # transform them into KEY=value pairing, and store in temp file
 # transform them into KEY=value pairing, and store in temp file
-aws secretsmanager get-secret-value --secret-id '@SECRET_ID' --query SecretString --output text | export "$(jq -r 'to_entries|map("\(.key)=\(.value|tostring)")|.[]')"
+eval "export $(echo "$(aws secretsmanager get-secret-value --secret-id '@SECRET_ID' --query SecretString --output text)" | jq -r 'to_entries | map("\(.key)=\(.value)") | @sh')"
 # rely on dotenv to add the environment variables to the node process
 
 # export DD_SERVICE here as the backend and worker share the same secret from secrets manager for env
