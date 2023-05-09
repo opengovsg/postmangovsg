5b63e123d138135761638bd53913b280e161f8cb
Richard Yak <richardyak@open.gov.sg>
2023-05-09 19:59:06 +0800

    chore: prettier format for e2e

--- e2e/tests/email-campaign.test.ts
+++ e2e/tests/email-campaign.test.ts
@@ -1,73 +1,73 @@
-import test, {expect} from '@playwright/test';
-import {Page} from 'playwright';
-import path from 'path';
-import {readFileSync, writeFileSync} from 'fs';
+import test, { expect } from "@playwright/test";
+import { Page } from "playwright";
+import path from "path";
+import { readFileSync, writeFileSync } from "fs";
 
-import {DASHBOARD_URL, MAILBOX, POSTMAN_FROM} from '../config';
-import {checkGmail} from '../gmail-check';
-import moment from 'moment';
+import { DASHBOARD_URL, MAILBOX, POSTMAN_FROM } from "../config";
+import { checkGmail } from "../gmail-check";
+import moment from "moment";
 
 let page: Page;
 test.beforeAll(async ({ browser }) => {
   page = await browser.newPage({
     baseURL: DASHBOARD_URL,
-    storageState: path.resolve(__dirname, '../cookie-auth-state.json'),
+    storageState: path.resolve(__dirname, "../cookie-auth-state.json"),
   });
 });
 
-test.describe.serial('Email campaign', () => {
-  const dateTime = moment().format('DD/MM/YYYY@HH:mm:ss');
+test.describe.serial("Email campaign", () => {
+  const dateTime = moment().format("DD/MM/YYYY@HH:mm:ss");
   const campaignName = `email_${dateTime}`;
 
-  const randomString = '_'.concat(
-    Math.floor(Math.random() * 1000000 + 1).toString(),
+  const randomString = "_".concat(
+    Math.floor(Math.random() * 1000000 + 1).toString()
   );
-  const subjectLine = 'sub_'.concat(dateTime).concat(randomString);
+  const subjectLine = "sub_".concat(dateTime).concat(randomString);
   const messageContent = `Dear {{ name }} ${randomString}`;
   const messageToVerify = `Dear postman ${randomString}`;
 
-  test('should be successfully created', async () => {
-    await page.goto('/');
-    await page.getByRole('button', { name: 'Create', exact: true }).click();
+  test("should be successfully created", async () => {
+    await page.goto("/");
+    await page.getByRole("button", { name: "Create", exact: true }).click();
     await page.locator('input[id="nameCampaign"]').fill(campaignName);
-    await page.getByRole('radio', { name: /^email$/i }).click();
-    await page.getByRole('button', { name: 'Create campaign' }).click();
+    await page.getByRole("radio", { name: /^email$/i }).click();
+    await page.getByRole("button", { name: "Create campaign" }).click();
     await expect(page.getByText(/Step 1/)).toBeVisible();
   });
 
-  test('should be successfully filled with message details', async () => {
+  test("should be successfully filled with message details", async () => {
     await page.locator('textarea[id="subject"]').fill(subjectLine);
     await page.locator('div[aria-label="rdw-editor"]').fill(messageContent);
-    await page.getByRole('button', { name: 'Next' }).click();
+    await page.getByRole("button", { name: "Next" }).click();
     await expect(page.getByText(/Step 2/)).toBeVisible();
   });
 
-  test('should have messages generated from uploaded recipient list', async () => {
+  test("should have messages generated from uploaded recipient list", async () => {
     writeFileSync(
-      './email-recipients.csv',
-      `recipient,name\n${MAILBOX},postman`,
+      "./email-recipients.csv",
+      `recipient,name\n${MAILBOX},postman`
     );
     await page
       .locator('input[type="file"]')
-      .setInputFiles('./email-recipients.csv');
-    await expect(page.getByText('Message preview')).toBeVisible();
-    await expect(page.getByText('email-recipients.csv')).toBeVisible();
-    await expect(page.getByText('1 recipients')).toBeVisible();
-    await page.getByRole('button', { name: 'Next' }).click();
+      .setInputFiles("./email-recipients.csv");
+    await expect(page.getByText("Message preview")).toBeVisible();
+    await expect(page.getByText("email-recipients.csv")).toBeVisible();
+    await expect(page.getByText("1 recipients")).toBeVisible();
+    await page.getByRole("button", { name: "Next" }).click();
     await expect(page.getByText(/Step 3/)).toBeVisible();
   });
 
-  test('should be able to validate email credentials', async () => {
+  test("should be able to validate email credentials", async () => {
     await page.locator('input[type="email"]').fill(MAILBOX);
     await page.locator('button[type="submit"]').click();
     await expect(page.getByText(/validated/)).toBeVisible();
-    await page.getByRole('button', { name: 'Next' }).click();
+    await page.getByRole("button", { name: "Next" }).click();
     await expect(page.getByText(/Step 4/)).toBeVisible();
   });
 
-  test('should be able to send out', async () => {
-    await page.getByRole('button', { name: /Send/ }).click();
-    await page.getByRole('button', { name: /Confirm/ }).click();
+  test("should be able to send out", async () => {
+    await page.getByRole("button", { name: /Send/ }).click();
+    await page.getByRole("button", { name: /Confirm/ }).click();
 
     const emails = await checkGmail({
       from: POSTMAN_FROM,
@@ -79,7 +79,7 @@ test.describe.serial('Email campaign', () => {
     await expect(page.getByText(/Sending completed/)).toBeVisible();
     // wait for the email to be successfully sent
     await expect(
-      page.getByRole('row', { name: /Sent Sent to recipient 1/ }),
+      page.getByRole("row", { name: /Sent Sent to recipient 1/ })
     ).toBeVisible();
 
     expect(emails).toBeTruthy();
@@ -90,12 +90,12 @@ test.describe.serial('Email campaign', () => {
     expect(emailContent).toContain(messageToVerify);
   });
 
-  test('should be able to have a report generated', async () => {
+  test("should be able to have a report generated", async () => {
     await expect(
-      page.getByRole('button', { name: 'Report' }).locator('i'),
+      page.getByRole("button", { name: "Report" }).locator("i")
     ).toHaveClass(/bx-download/, { timeout: 60000 });
-    const downloadPromise = page.waitForEvent('download');
-    await page.getByRole('button', { name: 'Report' }).click();
+    const downloadPromise = page.waitForEvent("download");
+    await page.getByRole("button", { name: "Report" }).click();
     const download = await downloadPromise;
     const content = readFileSync((await download.path()) as string);
     expect(content.toString()).toContain(MAILBOX);
--- e2e/tests/email-scheduled.test.ts
+++ e2e/tests/email-scheduled.test.ts
@@ -1,78 +1,78 @@
-import test, {expect} from '@playwright/test';
-import {Page} from 'playwright';
-import path from 'path';
-import {writeFileSync} from 'fs';
+import test, { expect } from "@playwright/test";
+import { Page } from "playwright";
+import path from "path";
+import { writeFileSync } from "fs";
 
-import {DASHBOARD_URL, MAILBOX} from '../config';
-import moment from 'moment';
+import { DASHBOARD_URL, MAILBOX } from "../config";
+import moment from "moment";
 
 let page: Page;
 test.beforeAll(async ({ browser }) => {
   page = await browser.newPage({
     baseURL: DASHBOARD_URL,
-    storageState: path.resolve(__dirname, '../cookie-auth-state.json'),
+    storageState: path.resolve(__dirname, "../cookie-auth-state.json"),
   });
 });
 
-test.describe.serial('Scheduled email campaign', () => {
-  const dateTime = moment().format('DD/MM/YYYY@HH:mm:ss');
+test.describe.serial("Scheduled email campaign", () => {
+  const dateTime = moment().format("DD/MM/YYYY@HH:mm:ss");
   const campaignName = `emailscheduled_${dateTime}`;
 
-  const randomString = '_'.concat(
-    Math.floor(Math.random() * 1000000 + 1).toString(),
+  const randomString = "_".concat(
+    Math.floor(Math.random() * 1000000 + 1).toString()
   );
-  const subjectLine = 'subscheduled_'.concat(dateTime).concat(randomString);
+  const subjectLine = "subscheduled_".concat(dateTime).concat(randomString);
   const messageContent = `Dear {{ name }} ${randomString}`;
 
-  test('should be successfully created', async () => {
-    await page.goto('/');
-    await page.getByRole('button', { name: 'Create', exact: true }).click();
+  test("should be successfully created", async () => {
+    await page.goto("/");
+    await page.getByRole("button", { name: "Create", exact: true }).click();
     await page.locator('input[id="nameCampaign"]').fill(campaignName);
-    await page.getByRole('radio', { name: /^email$/i }).click();
-    await page.getByRole('button', { name: 'Create campaign' }).click();
+    await page.getByRole("radio", { name: /^email$/i }).click();
+    await page.getByRole("button", { name: "Create campaign" }).click();
     await expect(page.getByText(/Step 1/)).toBeVisible();
   });
 
-  test('should be successfully filled with message details', async () => {
+  test("should be successfully filled with message details", async () => {
     await page.locator('textarea[id="subject"]').fill(subjectLine);
     await page.locator('div[aria-label="rdw-editor"]').fill(messageContent);
-    await page.getByRole('button', { name: 'Next' }).click();
+    await page.getByRole("button", { name: "Next" }).click();
     await expect(page.getByText(/Step 2/)).toBeVisible();
   });
 
-  test('should have messages generated from uploaded recipient list', async () => {
+  test("should have messages generated from uploaded recipient list", async () => {
     writeFileSync(
-      './email-recipients.csv',
-      `recipient,name\n${MAILBOX},postman`,
+      "./email-recipients.csv",
+      `recipient,name\n${MAILBOX},postman`
     );
     await page
       .locator('input[type="file"]')
-      .setInputFiles('./email-recipients.csv');
-    await expect(page.getByText('Message preview')).toBeVisible();
-    await expect(page.getByText('email-recipients.csv')).toBeVisible();
-    await expect(page.getByText('1 recipients')).toBeVisible();
-    await page.getByRole('button', { name: 'Next' }).click();
+      .setInputFiles("./email-recipients.csv");
+    await expect(page.getByText("Message preview")).toBeVisible();
+    await expect(page.getByText("email-recipients.csv")).toBeVisible();
+    await expect(page.getByText("1 recipients")).toBeVisible();
+    await page.getByRole("button", { name: "Next" }).click();
     await expect(page.getByText(/Step 3/)).toBeVisible();
   });
 
-  test('should be able to validate email credentials', async () => {
+  test("should be able to validate email credentials", async () => {
     await page.locator('input[type="email"]').fill(MAILBOX);
     await page.locator('button[type="submit"]').click();
     await expect(page.getByText(/validated/)).toBeVisible();
-    await page.getByRole('button', { name: 'Next' }).click();
+    await page.getByRole("button", { name: "Next" }).click();
     await expect(page.getByText(/Step 4/)).toBeVisible();
   });
 
-  test('should be able to be scheduled', async () => {
-    const scheduledDate = moment().add(1, 'hour');
-    await page.getByRole('button', { name: 'Schedule for later' }).click();
+  test("should be able to be scheduled", async () => {
+    const scheduledDate = moment().add(1, "hour");
+    await page.getByRole("button", { name: "Schedule for later" }).click();
     await page
       .locator('input[type="date"]')
-      .fill(scheduledDate.format('yyyy-MM-DD'));
+      .fill(scheduledDate.format("yyyy-MM-DD"));
     await page
       .locator('input[type="time"]')
-      .fill(scheduledDate.format('HH:mm'));
-    await page.getByRole('button', { name: 'Schedule campaign' }).click();
-    await expect(page.getByRole('row', { name: /1 Scheduled/ })).toBeVisible();
+      .fill(scheduledDate.format("HH:mm"));
+    await page.getByRole("button", { name: "Schedule campaign" }).click();
+    await expect(page.getByRole("row", { name: /1 Scheduled/ })).toBeVisible();
   });
 });
