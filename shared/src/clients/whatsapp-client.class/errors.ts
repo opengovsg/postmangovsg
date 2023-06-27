f0e50b529ef47f4e7bf5321406f1425be43b4e40
zx <67887489+tan-zx@users.noreply.github.com>
2023-06-27 15:49:08 +0800

    fix: changes made in response to PR review

---
+++ shared/src/clients/whatsapp-client.class/errors.ts
@@ -0,0 +1,11 @@
+export class AuthenticationError extends Error {
+  constructor(message: string) {
+    super(message)
+  }
+}
+
+export class RateLimitError extends Error {
+  constructor(message = 'Too Many Requests') {
+    super(message)
+  }
+}
