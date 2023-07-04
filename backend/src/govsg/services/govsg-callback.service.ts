093a15077197066cb7b6034f654516826f58980f
zx <67887489+tan-zx@users.noreply.github.com>
2023-07-04 15:32:21 +0800

    feat: update infobot text

--- backend/src/govsg/services/govsg-callback.service.ts
+++ backend/src/govsg/services/govsg-callback.service.ts
@@ -298,7 +298,7 @@ async function sendAutoReply(
   const textMessageToSend: WhatsAppTextMessageToSend = {
     recipient: whatsappId,
     apiClient: clientId,
-    body: 'The COVID-19 infobot is temporarily unavailable due to maintenance works. For more COVID-19 related information, please visit the Ministry of Health’s website at moh.gov.sg. Thank you.',
+    body: 'If you are inquiring about COVID-19 updates, the COVID-19 infobot is temporarily unavailable due to maintenance work. For more COVID-19 related information, please visit the Ministry of Health’s website at moh.gov.sg. Thank you',
   }
   // can substitute this with template message if we get such a template approved
   await WhatsAppService.whatsappClient.sendTextMessage(textMessageToSend)
