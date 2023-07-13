5106af28dccf2787ba5930dd106f765d999656df
zx <67887489+tan-zx@users.noreply.github.com>
2023-07-13 23:01:31 +0800

    feat: connect to db

---
+++ sst/README.md
@@ -0,0 +1,8 @@
+# SST for Postmangovsg
+
+## Developing against local
+
+## Developing against staging
+
+- Need to manually set VPC of Lambda function to staging VPC
+- Turn on OGP VPN + run tunnel to jumphost on backend
--- sst/packages/functions/src/cron.ts
+++ sst/packages/functions/src/cron.ts
@@ -3,6 +3,7 @@ import { apiKeys } from "../../core/src/models/api-key";
 
 export async function handler() {
   console.log("Running my cron job");
+  console.log({ hi: process.env.IS_LOCAL });
   try {
     const apiKey = await db.select().from(apiKeys).limit(1);
     console.log(apiKey);
