5521548aa19c195218cf3b2c30efdbe06d987b29
zx <67887489+tan-zx@users.noreply.github.com>
2023-07-13 21:30:57 +0800

    feat: sst wip

--- sst/packages/functions/src/cron.ts
+++ sst/packages/functions/src/cron.ts
@@ -1,6 +1,12 @@
-import { Config } from "sst/node/config";
+import { db } from "../../core/src/dbClient";
+import { apiKeys } from "../../core/src/models/api-key";
 
 export async function handler() {
   console.log("Running my cron job");
-  console.log(Config.TEST_ENV_VAR);
+  try {
+    const apiKey = await db.select().from(apiKeys).limit(1);
+    console.log(apiKey);
+  } catch (e) {
+    console.log(e);
+  }
 }
