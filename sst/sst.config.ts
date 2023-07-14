92e744bcbbacd3f8b8428d6205c5a4a079e20a9d
zx <67887489+tan-zx@users.noreply.github.com>
2023-07-15 01:10:37 +0800

    style(eslint+prettier): apply rules throughout

--- sst/sst.config.ts
+++ sst/sst.config.ts
@@ -1,14 +1,15 @@
-import { SSTConfig } from "sst";
-import { MyStack } from "./stacks/MyStack";
+import { SSTConfig } from 'sst'
+
+import { MyStack } from './stacks/MyStack'
 
 export default {
   config(_input) {
     return {
-      name: "postmangovsg-sst",
-      region: "ap-southeast-1",
-    };
+      name: 'postmangovsg-sst',
+      region: 'ap-southeast-1',
+    }
   },
   stacks(app) {
-    app.stack(MyStack);
-  }
-} satisfies SSTConfig;
+    app.stack(MyStack)
+  },
+} satisfies SSTConfig
--- sst/stacks/MyStack.ts
+++ sst/stacks/MyStack.ts
@@ -1,26 +1,26 @@
-import { StackContext, Config, Cron, Stack } from "sst/constructs";
+import { Config, Cron, Stack, StackContext } from 'sst/constructs'
 
 export function MyStack({ stack }: StackContext) {
-  const apiKeyExpiryCron = new Cron(stack, "cron", {
+  const apiKeyExpiryCron = new Cron(stack, 'cron', {
     // runs every day at 12AM UTC, i.e. 8AM SGT
-    schedule: "cron(0 0 */1 * ? *)",
-    job: "packages/functions/src/api-key-expiry-cron.handler",
-  });
-  apiKeyExpiryCron.attachPermissions(getPermissions());
-  apiKeyExpiryCron.bind(getSecrets(stack));
+    schedule: 'cron(0 0 */1 * ? *)',
+    job: 'packages/functions/src/api-key-expiry-cron.handler',
+  })
+  apiKeyExpiryCron.attachPermissions(getPermissions())
+  apiKeyExpiryCron.bind(getSecrets(stack))
 }
 
 function getSecrets(stack: Stack) {
-  return [new Config.Secret(stack, "POSTMAN_DB_URI")];
+  return [new Config.Secret(stack, 'POSTMAN_DB_URI')]
 }
 
 function getPermissions() {
   return [
     // necessary to change VPC of Lambda to VPC of RDS
-    "ec2:DescribeNetworkInterfaces",
-    "ec2:CreateNetworkInterface",
-    "ec2:DeleteNetworkInterface",
-    "ec2:DescribeInstances",
-    "ec2:AttachNetworkInterface",
-  ];
+    'ec2:DescribeNetworkInterfaces',
+    'ec2:CreateNetworkInterface',
+    'ec2:DeleteNetworkInterface',
+    'ec2:DescribeInstances',
+    'ec2:AttachNetworkInterface',
+  ]
 }
