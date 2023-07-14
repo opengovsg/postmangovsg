e6caa22fbfcdaabfee67403bd57abd9111234ce0
zx <67887489+tan-zx@users.noreply.github.com>
2023-07-14 10:15:32 +0800

    refactor: no regret changes

--- sst/packages/core/src/dbClient.ts
+++ sst/packages/core/src/dbClient.ts
@@ -2,6 +2,7 @@ import postgres from "postgres";
 import { drizzle } from "drizzle-orm/postgres-js";
 import { Config } from "sst/node/config";
 
+// turn this into a class to be initialized
 const client = postgres(
   process.env.IS_LOCAL
     ? "postgres://postgres:postgres@localhost:5432/postmangovsg_dev" // pragma: allowlist secret
