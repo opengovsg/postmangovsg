9ccbee716443579785e1e1c293ec3c283becdac4
zx <67887489+tan-zx@users.noreply.github.com>
2023-07-14 10:15:32 +0800

    refactor: no regret changes

--- sst/packages/functions/src/cron.ts
+++ sst/packages/functions/src/cron.ts
@@ -1,13 +1,10 @@
-import { db } from "../../core/src/dbClient";
-import { apiKeys } from "../../core/src/models/api-key";
+import { db } from "@postmangovsg-sst/core/src/dbClient";
+import { apiKeys } from "@postmangovsg-sst/core/src/models";
 
 export async function handler() {
   console.log("Running my cron job");
-  console.log({ hi: process.env.IS_LOCAL });
-  try {
-    const apiKey = await db.select().from(apiKeys).limit(1);
-    console.log(apiKey);
-  } catch (e) {
-    console.log(e);
-  }
+  console.log(process.env.IS_LOCAL);
+  console.log(process.env.LOCAL_DB_URI);
+  const apiKey = await db.select().from(apiKeys).limit(1);
+  console.log(apiKey);
 }
