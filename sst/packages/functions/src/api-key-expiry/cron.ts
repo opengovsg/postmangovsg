d576a6a7aa104de4380d9410e055fb9792fe6f53
zx <67887489+tan-zx@users.noreply.github.com>
2023-07-17 02:39:41 +0800

    feat: configure VPC, subnets, SG using CDK

--- sst/packages/functions/src/api-key-expiry/cron.ts
+++ sst/packages/functions/src/api-key-expiry/cron.ts
@@ -1,16 +1,8 @@
-import { eq, sql } from 'drizzle-orm'
+// to replace with fetch when upgraded to Node 18
+import axios from 'axios'
 import { Config } from 'sst/node/config'
 
-import PostmanDbClient from '@/core/database/client'
-import { apiKeys } from '@/core/models'
-import { users } from '@/core/models/user'
-import { getFutureUTCDate } from '@/core/util/date'
-import { sendEmail } from '@/core/util/email'
-
-import { IS_LOCAL, LOCAL_DB_URI } from '../env'
-
-import { reminderEmailMapper } from './helper'
-
+import { IS_LOCAL } from '../env'
 /*
 - This cron job runs every day at 12AM UTC, i.e. 8AM SGT
 - It picks up API keys that are expiring at exactly 28 days, 14 days, 3 days and 1 day from now
@@ -19,75 +11,10 @@ import { reminderEmailMapper } from './helper'
 */
 
 export async function handler() {
-  const dbUri = IS_LOCAL ? LOCAL_DB_URI : Config.POSTMAN_DB_URI
-  const db = new PostmanDbClient(dbUri).getClient()
-  const fourWeeks = getFutureUTCDate(28)
-  const twoWeeks = getFutureUTCDate(14)
-  const threeDays = getFutureUTCDate(3)
-  const oneDay = getFutureUTCDate(1)
-
-  const [fourWeeksKeys, twoWeeksKeys, threeDaysKeys, oneDayKeys] =
-    await Promise.all([
-      await db
-        .select({
-          userEmail: users.email,
-          apiKeyLabel: apiKeys.label,
-          validUntil: apiKeys.validUntil,
-          apiKeyLastFiveChars: apiKeys.lastFive,
-          notificationContacts: apiKeys.notificationContacts,
-        })
-        .from(apiKeys)
-        .innerJoin(users, eq(users.id, apiKeys.userId))
-        .where(
-          sql`DATE_TRUNC('day', ${apiKeys.validUntil}) = DATE(${fourWeeks})`,
-        ),
-      await db
-        .select({
-          userEmail: users.email,
-          apiKeyLabel: apiKeys.label,
-          validUntil: apiKeys.validUntil,
-          apiKeyLastFiveChars: apiKeys.lastFive,
-          notificationContacts: apiKeys.notificationContacts,
-        })
-        .from(apiKeys)
-        .innerJoin(users, eq(users.id, apiKeys.userId))
-        .where(
-          sql`DATE_TRUNC('day', ${apiKeys.validUntil}) = DATE(${twoWeeks})`,
-        ),
-      await db
-        .select({
-          userEmail: users.email,
-          apiKeyLabel: apiKeys.label,
-          validUntil: apiKeys.validUntil,
-          apiKeyLastFiveChars: apiKeys.lastFive,
-          notificationContacts: apiKeys.notificationContacts,
-        })
-        .from(apiKeys)
-        .innerJoin(users, eq(users.id, apiKeys.userId))
-        .where(
-          sql`DATE_TRUNC('day', ${apiKeys.validUntil}) = DATE(${threeDays})`,
-        ),
-      await db
-        .select({
-          userEmail: users.email,
-          apiKeyLabel: apiKeys.label,
-          validUntil: apiKeys.validUntil,
-          apiKeyLastFiveChars: apiKeys.lastFive,
-          notificationContacts: apiKeys.notificationContacts,
-        })
-        .from(apiKeys)
-        .innerJoin(users, eq(users.id, apiKeys.userId))
-        .where(sql`DATE_TRUNC('day', ${apiKeys.validUntil}) = DATE(${oneDay})`),
-    ])
-  const fourWeeksEmails = reminderEmailMapper(fourWeeksKeys, 'four weeks')
-  const twoWeeksEmails = reminderEmailMapper(twoWeeksKeys, 'two weeks')
-  const threeDaysEmails = reminderEmailMapper(threeDaysKeys, 'three days')
-  const oneDayEmails = reminderEmailMapper(oneDayKeys, 'one day')
-  await Promise.all([
-    ...fourWeeksEmails.map((email) => sendEmail(email)),
-    ...twoWeeksEmails.map((email) => sendEmail(email)),
-    ...threeDaysEmails.map((email) => sendEmail(email)),
-    ...oneDayEmails.map((email) => sendEmail(email)),
-  ])
-  // })
+  const apiEndpoint = IS_LOCAL ? Config.API_ENDPOINT : 'todolater'
+  console.log('apiEndpoint', apiEndpoint)
+  await axios.get(`${apiEndpoint}/api-key-expiry`).catch((err) => {
+    console.error(err)
+    throw err
+  })
 }
--- sst/stacks/MyStack.ts
+++ sst/stacks/MyStack.ts
@@ -1,29 +1,79 @@
-import { Config, Cron, Stack, StackContext } from 'sst/constructs'
+import * as ec2 from 'aws-cdk-lib/aws-ec2'
+import {
+  Api,
+  Config,
+  Cron,
+  Function,
+  Stack,
+  StackContext,
+} from 'sst/constructs'
 
-export function MyStack({ stack }: StackContext) {
-  const apiKeyExpiryCron = new Cron(stack, 'cron', {
+export function MyStack({ app, stack }: StackContext) {
+  const { vpcName, lookupOptions, sgName, sgId } =
+    app.stage === 'prod'
+      ? {
+          vpcName: 'postmangovsg-production-vpc',
+          lookupOptions: {
+            vpcId: 'vpc-0e71dfc6b3c022b7a',
+            isDefault: false,
+          },
+          sgName: ' postmangovsg-production-serverless',
+          sgId: 'sg-034854389076df5e8',
+        }
+      : // use staging VPC for stg and dev
+        {
+          vpcName: 'postmangovsg-staging-vpc',
+          lookupOptions: {
+            vpcId: 'vpc-006dc4e0a97146e40',
+            isDefault: false,
+          },
+          sgName: 'postmangovsg-staging-serverless',
+          sgId: 'sg-057180ddecca97846',
+        }
+
+  const existingVpc = ec2.Vpc.fromLookup(stack, vpcName, lookupOptions)
+  const existingSg = ec2.SecurityGroup.fromSecurityGroupId(stack, sgName, sgId)
+  app.setDefaultFunctionProps({
+    vpc: existingVpc,
+    securityGroups: [existingSg],
+    vpcSubnets: {
+      subnets: existingVpc.privateSubnets,
+    },
+  })
+
+  const sendExpiryEmail = new Function(stack, 'api-key-expiry', {
+    handler: 'packages/functions/src/api-key-expiry/main.handler',
+    vpc: existingVpc,
+    securityGroups: [existingSg],
+    vpcSubnets: {
+      subnets: existingVpc.privateSubnets,
+    },
+  })
+  sendExpiryEmail.bind(getSecrets(stack))
+
+  const api = new Api(stack, 'Api', {
+    routes: {
+      'GET    /api-key-expiry': {
+        function: sendExpiryEmail,
+        // add authorization
+      },
+    },
+  })
+  const cron = new Cron(stack, 'cron', {
     // runs every day at 12AM UTC, i.e. 8AM SGT
     schedule: 'cron(0 0 */1 * ? *)',
     job: 'packages/functions/src/api-key-expiry/cron.handler',
   })
-  apiKeyExpiryCron.attachPermissions(getPermissions())
-  apiKeyExpiryCron.bind(getSecrets(stack))
+  cron.bind([
+    new Config.Parameter(stack, 'API_ENDPOINT', {
+      value: api.url,
+    }),
+  ])
 }
 
 function getSecrets(stack: Stack) {
   return [
     new Config.Secret(stack, 'POSTMAN_DB_URI'),
     new Config.Secret(stack, 'POSTMAN_API_KEY'),
-  ]
-}
-
-function getPermissions() {
-  return [
-    // necessary to change VPC of Lambda to VPC of RDS
-    'ec2:DescribeNetworkInterfaces',
-    'ec2:CreateNetworkInterface',
-    'ec2:DeleteNetworkInterface',
-    'ec2:DescribeInstances',
-    'ec2:AttachNetworkInterface',
   ]
 }
