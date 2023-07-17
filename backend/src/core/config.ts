26bed12bc6feb5440d0618606424c276accd33b4
Stanley Nguyen <stanleynguyen@users.noreply.github.com>
2023-07-18 02:08:30 +0800

    fix(backend): tighten default cors regex check (#2089)

--- backend/src/core/config.ts
+++ backend/src/core/config.ts
@@ -314,7 +314,7 @@ const config: Config<ConfigSchema> = convict({
   },
   frontendUrl: {
     doc: 'CORS: accept requests from this origin. Can be a string, or regex',
-    default: 'https://postman.gov.sg', // prod only
+    default: '/^https:\\/\\/([A-z0-9-]+\\.)?(postman\\.gov\\.sg)$/',
     env: 'FRONTEND_URL',
   },
   protectedUrl: {
@@ -819,7 +819,6 @@ config.set('mailFrom', config.get('mailFrom') || defaultMailFrom)
 switch (config.get('env')) {
   case 'staging':
     config.load({
-      frontendUrl: '/^https:\\/\\/([A-z0-9-]+\\.)?(postman\\.gov\\.sg)$/', // all subdomains
       protectedUrl: 'https://staging.postman.gov.sg/p',
       unsubscribeUrl: 'https://staging.postman.gov.sg/unsubscribe',
       aws: {
