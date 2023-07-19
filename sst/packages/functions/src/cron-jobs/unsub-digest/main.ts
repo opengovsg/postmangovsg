bf7a660c551002454014a41e7178b89a74b32bf5
zx <67887489+tan-zx@users.noreply.github.com>
2023-07-19 15:05:55 +0800

    refactor: wrap in try catch

--- sst/packages/functions/src/cron-jobs/unsub-digest/main.ts
+++ sst/packages/functions/src/cron-jobs/unsub-digest/main.ts
@@ -7,13 +7,18 @@ import { IS_LOCAL, LOCAL_DB_URI } from '../../env'
 import { getUnsubscribeList, sendEmailAndUpdate } from './helper'
 
 export async function handler() {
-  const dbUri = IS_LOCAL ? LOCAL_DB_URI : Config.POSTMAN_DB_URI
-  const sequelize = getSequelize(dbUri)
-  // retrieve unsubscribed recipients grouped by campaigns and users
-  const unsubscribeDigests = await getUnsubscribeList(sequelize)
+  try {
+    const dbUri = IS_LOCAL ? LOCAL_DB_URI : Config.POSTMAN_DB_URI
+    const sequelize = getSequelize(dbUri)
+    // retrieve unsubscribed recipients grouped by campaigns and users
+    const unsubscribeDigests = await getUnsubscribeList(sequelize)
 
-  // generate email digest and send email to each user
-  for (const userDigest of unsubscribeDigests) {
-    await sendEmailAndUpdate(userDigest, sequelize)
+    // generate email digest and send email to each user
+    for (const userDigest of unsubscribeDigests) {
+      await sendEmailAndUpdate(userDigest, sequelize)
+    }
+  } catch (err) {
+    console.error(err)
+    throw err
   }
 }
