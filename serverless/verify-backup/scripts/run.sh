7b4c848c06dccf545d3278ab25801b1958ea4b13
zx <67887489+tan-zx@users.noreply.github.com>
2023-07-24 21:11:06 +0800

    refactor: delete backup-related lambdas

--- serverless/verify-backup/scripts/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Initialise postgres
-mkdir -p /run/postgresql/
-chown -R postgres:postgres /run/postgresql/
-su - postgres -c "initdb /var/lib/postgresql/data"
-
-# Start server to listen for pubsub messages
-npm run start
