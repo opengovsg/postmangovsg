f195d09c67b6c4cbea987b2368fe781346e39e26
Jiayee Lim <me@jiayee.net>
2023-08-11 15:42:48 +0800

    chore: improve logging

--- backend/src/govsg/services/govsg-callback.service.ts
+++ backend/src/govsg/services/govsg-callback.service.ts
@@ -236,6 +236,14 @@ const parseTemplateMessageWebhook = async (
       return
     }
     case WhatsAppMessageStatus.sent: {
+      logger.info({
+        message: 'WhatsApp message Status: Sent',
+        meta: {
+          govsgMessage,
+          govsgMessageTransactional,
+          govsgOp,
+        },
+      })
       const fieldOpts = {
         status: shouldUpdateStatus(statusIfUpdated, prevStatus)
           ? statusIfUpdated
@@ -248,6 +256,14 @@ const parseTemplateMessageWebhook = async (
       return
     }
     case WhatsAppMessageStatus.delivered: {
+      logger.info({
+        message: 'WhatsApp message Status: Delivered',
+        meta: {
+          govsgMessage,
+          govsgMessageTransactional,
+          govsgOp,
+        },
+      })
       const fieldOpts = {
         status: shouldUpdateStatus(statusIfUpdated, prevStatus)
           ? statusIfUpdated
