cdb9bc8472a83dba7caa645381b96b779ed81d9d
Jiayee Lim <me@jiayee.net>
2023-08-14 11:50:24 +0800

    chore: update WA webhook default text (#2171)

--- backend/src/govsg/services/govsg-callback.service.ts
+++ backend/src/govsg/services/govsg-callback.service.ts
@@ -482,7 +482,7 @@ async function sendAutoReply(
   const textMessageToSend: WhatsAppTextMessageToSend = {
     recipient: whatsappId,
     apiClient: clientId,
-    body: 'We are unable to receive replies at the moment.\n\nIf you are inquiring about COVID-19 updates, the COVID-19 infobot is temporarily unavailable due to maintenance work. For more COVID-19 related information, please visit the Ministry of Health’s website at moh.gov.sg. Thank you',
+    body: 'We are unable to receive replies at the moment.',
   }
   // can substitute this with template message if we get such a template approved
   await whatsappService.whatsappClient.sendTextMessage(textMessageToSend)
