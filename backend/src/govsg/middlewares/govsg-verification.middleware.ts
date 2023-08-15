026ee32bc6964984cf1a88e107ef98b728a44d1c
Jiayee Lim <me@jiayee.net>
2023-08-15 09:15:46 +0800

    feat: hide passcode until MOP clicks (#2175)

--- backend/src/govsg/middlewares/govsg-verification.middleware.ts
+++ backend/src/govsg/middlewares/govsg-verification.middleware.ts
@@ -45,7 +45,7 @@ export const listMessages = async (
     include: [
       {
         model: GovsgVerification,
-        attributes: ['passcode'],
+        attributes: ['passcode', 'userClickedAt'],
       },
     ],
   })
@@ -65,7 +65,9 @@ export const listMessages = async (
       name: recipient_name,
       mobile: recipient,
       data: JSON.stringify(remainingParams),
-      passcode: row.govsgVerification?.passcode ?? '',
+      passcode: row.govsgVerification?.userClickedAt
+        ? row.govsgVerification.passcode
+        : '',
       sent: row.sentAt,
       officer: officer_name,
     }
