16faa9edf73d4de6bb4d73cf1ea69edc78115baf
Jiayee Lim <me@jiayee.net>
2023-08-18 11:18:32 +0800

    feat: implement click to reveal passcode

--- frontend/src/components/common/StyledText/PasscodeBadge.tsx
+++ frontend/src/components/common/StyledText/PasscodeBadge.tsx
@@ -8,9 +8,16 @@ interface PasscodeBadgeProps {
 }
 
 export const PasscodeBadge = ({ label, placeholder }: PasscodeBadgeProps) => {
+  const logOnClick = () => {
+    // TODO: Log click
+  }
+
   return (
-    <span className={label ? cx(styles.positive) : cx(styles.negative)}>
-      {label ? label : placeholder}
-    </span>
+    <details>
+      <summary onClick={logOnClick}>Click to reveal</summary>
+      <span className={label ? cx(styles.positive) : cx(styles.negative)}>
+        {label ? label : placeholder}
+      </span>
+    </details>
   )
 }
