38b7a2e461e6461dd6d6d4ccae82c74d0ab51dc4
Jiayee Lim <me@jiayee.net>
2023-08-21 13:35:45 +0800

    feat: remove passcode button on delivered

--- backend/src/govsg/services/govsg-verification-service.ts
+++ backend/src/govsg/services/govsg-verification-service.ts
@@ -1,13 +1,8 @@
 import config from '@core/config'
 import { whatsappService } from '@core/services'
-import { GovsgMessage, GovsgVerification } from '@govsg/models'
-import { createPasscode } from '@govsg/utils/passcode'
 import {
-  WhatsAppId,
-  WhatsAppApiClient,
   MessageId,
   WhatsAppTemplateMessageToSend,
-  WhatsAppLanguages,
 } from '@shared/clients/whatsapp-client.class/types'
 
 export const sendMessage = async (
@@ -19,82 +14,4 @@ export const sendMessage = async (
     isLocal
   )
   return wamid
-}
-
-// TODO: Remove
-export const sendPasscodeCreationMessage = async (
-  whatsappId: WhatsAppId,
-  clientId: WhatsAppApiClient
-): Promise<MessageId> => {
-  const templateMessageToSend: WhatsAppTemplateMessageToSend = {
-    recipient: whatsappId,
-    apiClient: clientId,
-    templateName: 'sgc_passcode_generation', // TODO: Un-hardcode this
-    params: [],
-    language: WhatsAppLanguages.english,
-  }
-  const isLocal = config.get('env') === 'development'
-  const passcodeCreationWamid =
-    await whatsappService.whatsappClient.sendTemplateMessage(
-      templateMessageToSend,
-      isLocal
-    )
-  return passcodeCreationWamid
-}
-
-// TODO: Remove
-export const storePrecreatedPasscode = async (
-  govsgMessageId: GovsgMessage['id'],
-  passcodeCreationWamid: MessageId
-): Promise<GovsgVerification> => {
-  const govsgVerification = await GovsgVerification.findOne({
-    where: { govsgMessageId },
-  })
-  if (!govsgVerification) {
-    return await GovsgVerification.create({
-      govsgMessageId,
-      passcodeCreationWamid,
-      passcode: createPasscode(),
-    } as GovsgVerification)
-  }
-  return await govsgVerification.update({
-    passcodeCreationWamid,
-  })
-}
-
-// TODO: Remove
-export const sendPasscodeMessage = async (
-  whatsappId: WhatsAppId,
-  clientId: WhatsAppApiClient,
-  officerName: string,
-  officerAgency: string,
-  passcode: string
-): Promise<MessageId> => {
-  const templateMessageToSend: WhatsAppTemplateMessageToSend = {
-    recipient: whatsappId,
-    apiClient: clientId,
-    templateName: 'sgc_send_passcode', // TODO: Un-hardcode this
-    params: [
-      {
-        type: 'text',
-        text: officerName,
-      },
-      {
-        type: 'text',
-        text: officerAgency,
-      },
-      {
-        type: 'text',
-        text: passcode,
-      },
-    ],
-    language: WhatsAppLanguages.english,
-  }
-  const isLocal = config.get('env') === 'development'
-  const passcodeCreationWamid =
-    await whatsappService.whatsappClient.sendTemplateMessage(
-      templateMessageToSend,
-      isLocal
-    )
-  return passcodeCreationWamid
 }
