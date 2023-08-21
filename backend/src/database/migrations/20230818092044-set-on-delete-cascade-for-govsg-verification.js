6dc4639223b253fe25413854692aec0582206fc9
Jiayee Lim <me@jiayee.net>
2023-08-21 10:28:20 +0800

    fix: set on delete cascade for govsg_verification_govsg_message_id_fkey constraint

--- backend/src/database/migrations/20230818092044-set-on-delete-cascade-for-govsg-verification.js
+++ backend/src/database/migrations/20230818092044-set-on-delete-cascade-for-govsg-verification.js
@@ -2,27 +2,31 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.changeColumn('govsg_verification', 'govsg_message_id', {
-      type: Sequelize.DataTypes.BIGINT,
-      allowNull: false,
-      references: {
-        model: 'govsg_messages',
-        key: 'id',
-      },
-      onUpdate: 'CASCADE',
-      onDelete: 'CASCADE',
-    })
+    const transaction = await queryInterface.sequelize.transaction()
+    try {
+      await queryInterface.sequelize.query('ALTER TABLE govsg_verification DROP CONSTRAINT govsg_verification_govsg_message_id_fkey', { transaction })
+      await queryInterface.sequelize.query(`ALTER TABLE govsg_verification ADD CONSTRAINT govsg_verification_govsg_message_id_fkey
+        FOREIGN KEY(govsg_message_id) REFERENCES govsg_messages (id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE`, { transaction })
+      transaction.commit()
+    } catch (e) {
+      transaction.rollback()
+      throw e
+    }
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.changeColumn('govsg_verification', 'govsg_message_id', {
-      type: Sequelize.DataTypes.BIGINT,
-      allowNull: false,
-      references: {
-        model: 'govsg_messages',
-        key: 'id',
-      },
-      onUpdate: 'CASCADE',
-    })
-  }
+    const transaction = await queryInterface.sequelize.transaction()
+    try {
+      await queryInterface.sequelize.query('ALTER TABLE govsg_verification DROP CONSTRAINT govsg_verification_govsg_message_id_fkey', { transaction })
+      await queryInterface.sequelize.query(`ALTER TABLE govsg_verification ADD CONSTRAINT govsg_verification_govsg_message_id_fkey
+        FOREIGN KEY(govsg_message_id) REFERENCES govsg_messages (id)
+        ON UPDATE CASCADE`, { transaction })
+      transaction.commit()
+    } catch (e) {
+      transaction.rollback()
+      throw e
+    }
+  },
 };
