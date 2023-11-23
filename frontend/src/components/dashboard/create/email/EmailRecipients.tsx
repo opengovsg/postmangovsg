dd3522a7a5ac84f91ca5c8e5a1125abfd9b00a9a
Kishen Kumar <kishen1198@gmail.com>
2023-11-23 10:51:50 +0800

    Fix/UI bug (#2237)
    * fix: ui bug
    
    * fix: ui bug
    
    ---------
    
    Co-authored-by: KishenKumarrrrr <kishen@open.gov.sg>

--- frontend/src/components/dashboard/create/email/EmailRecipients.tsx
+++ frontend/src/components/dashboard/create/email/EmailRecipients.tsx
@@ -160,7 +160,7 @@ const EmailRecipients = ({
   return (
     <>
       <StepSection>
-        <StepHeader title="Upload CSV File" subtitle="Step 2">
+        <StepHeader title="Upload CSV File" subtitle={protect ? '' : 'Step 2'}>
           <p>
             Only CSV format files are allowed. If you have an Excel file, please
             convert it by going to File &gt; Save As &gt; CSV (Comma delimited).
