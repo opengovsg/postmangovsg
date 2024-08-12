e818c33bff37510f00dd77576070867dcda0205a
KishenKumarrrrr <kishen@open.gov.sg>
2024-08-12 13:15:58 +0800

    refactor: make indx composite of user_id and tag

--- backend/src/database/migrations/20240812063204-add-user-id-index-to-email-messages-transactional.js
+++ backend/src/database/migrations/20240812063204-add-user-id-index-to-email-messages-transactional.js
@@ -3,12 +3,12 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.addIndex('email_messages_transactional', {
-      name: 'email_messages_transactional_user_id_idx',
-      fields: ['user_id']
+      name: 'email_messages_transactional_user_id_tag_idx',
+      fields: ['user_id', 'tag']
     })
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.removeIndex('email_messages_transactional', 'email_messages_transactional_user_id_idx')
+    await queryInterface.removeIndex('email_messages_transactional', 'email_messages_transactional_user_id_tag_idx')
   }
 };
