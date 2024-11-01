33224ed14ca3839831d911ce0af53699335620ca
KishenKumarrrrr <kishen@open.gov.sg>
2024-11-01 13:45:10 +0800

    feat: enable profiling for 20 percent of instances

--- shared/src/tracing.ts
+++ shared/src/tracing.ts
@@ -2,11 +2,14 @@ import tracer from 'dd-trace'
 import { ClientRequest, IncomingMessage } from 'http'
 import { Span } from 'opentracing'
 
+const RANDOM_NUMBER_MAX_100 = Math.floor(Math.random() * 100) + 1 // Number from 1 - 100
+const ENABLE_PROFILING_FOR_20_PER_CENT = RANDOM_NUMBER_MAX_100 <= 20 // 20% chance of enabling profiling
+
 // Make it an init function so the code won't be evaluated and flagged as invalid
 // on frontend linting-in-background during development
 export function init() {
   tracer.init({
-    profiling: true,
+    profiling: ENABLE_PROFILING_FOR_20_PER_CENT,
   })
   tracer.use('http', {
     client: {
