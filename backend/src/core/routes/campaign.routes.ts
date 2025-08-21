9cf00f2d3f10301892bafc34fefad8e0a9eb8291
KishenKumarrrrr <kishen@open.gov.sg>
2025-08-21 20:26:18 +0800

    feat: prevent campaign from being a demo campaign

--- backend/src/core/routes/campaign.routes.ts
+++ backend/src/core/routes/campaign.routes.ts
@@ -30,7 +30,7 @@ const createCampaignValidator = {
       .required(),
     name: Joi.string().max(255).trim().required(),
     protect: Joi.boolean().default(false),
-    demo_message_limit: Joi.number().default(null).min(1).max(20),
+    demo_message_limit: Joi.number().default(null).min(0).max(0),
   }),
 }
 
